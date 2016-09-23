b9e5b3aa4a179b46743d8576d177bc7810ceeaba
Alexandre Segura <mex.zktk@gmail.com>
2016-09-23 02:47:20 +0200

    Add android files.

--- android/app/src/main/java/com/coursiersapp/MainApplication.java
+++ android/app/src/main/java/com/coursiersapp/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new FBSDKPackage(),
+            new MapsPackage()
       );
     }
   };
