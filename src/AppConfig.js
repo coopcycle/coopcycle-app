77881761921e6da882f7a85aa144b16a0c29b264
Alexandre Segura <mex.zktk@gmail.com>
2016-11-29 14:42:33 +0100

    Change domain name.

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,6 +1,6 @@
 class AppConfig {
     // static API_DOMAIN = 'coursiers-velo.xyz';
-    static API_DOMAIN = 'coursiers2.dev';
+    static API_DOMAIN = 'coopcycle.dev';
     static API_BASEURL = 'http://' + AppConfig.API_DOMAIN;
     static WEBSOCKET_BASEURL = 'ws://' + AppConfig.API_DOMAIN;
 }
