5c06e00b84fd1652681a5d969416df8bbcf690de
Alexandre Segura <mex.zktk@gmail.com>
2018-02-18 17:46:12 +0100

    Introduce Registry class.

---
+++ src/Registry.js
@@ -0,0 +1,30 @@
+import WebSocketClient from './WebSocketClient'
+import { Settings, events } from './Settings'
+
+let webSocketClient
+
+class Registry {
+
+  static initWebSocketClient(client) {
+    webSocketClient = new WebSocketClient(client, '/dispatch', {
+      onConnect: () => { events.emit('websocket:connect')},
+      onDisconnect: () => { events.emit('websocket:disconnect')},
+      onReconnect: () => { events.emit('websocket:reconnect')},
+      onMessage: event => { events.emit('websocket:message', event)},
+    })
+    webSocketClient.connect()
+  }
+
+  static getWebSocketClient() {
+    return webSocketClient
+  }
+
+  static clearWebSocketClient() {
+    if (webSocketClient) {
+      webSocketClient.disconnect()
+    }
+  }
+
+}
+
+module.exports = { Registry }
