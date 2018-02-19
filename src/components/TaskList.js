dabebc45b2f97339e39b825521248c799754cafd
Alexandre Segura <mex.zktk@gmail.com>
2018-02-20 00:43:45 +0100

    Style items depending on status.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,16 +4,40 @@ import { Icon, Text } from 'native-base'
 import { Col, Row, Grid } from 'react-native-easy-grid'
 import moment from 'moment/min/moment-with-locales'
 
-import { whiteColor, lightGreyColor } from "../styles/common"
+import { whiteColor, lightGreyColor, redColor } from "../styles/common"
 
 moment.locale('fr')
 
 const styles = StyleSheet.create({
+  itemLeftRight: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 5,
+  },
+  itemBody: {
+    padding: 10
+  },
   item: {
     paddingVertical: 10,
     borderBottomColor: lightGreyColor,
     borderBottomWidth: StyleSheet.hairlineWidth
   },
+  disabled: {
+    opacity: 0.4
+  },
+  text: {
+    fontSize: 14
+  },
+  textDanger: {
+    color: redColor
+  },
+  icon: {
+    fontSize: 14
+  },
+  iconDanger: {
+    color: redColor
+  }
 })
 
 export default class TaskList extends Component {
@@ -71,31 +95,40 @@ export default class TaskList extends Component {
     const taskTypeIcon = task.type === 'PICKUP' ? 'arrow-up' : 'arrow-down'
 
     let taskStatusIcon = 'list'
+    let itemLeftStyle = [ styles.itemLeftRight ]
+    let itemBodyStyle = [ styles.itemBody ]
+    let textStyle = [ styles.text ]
+    let iconStyle = [ styles.icon ]
+
     if (task.status === 'DONE') {
       taskStatusIcon = 'checkmark'
+      itemLeftStyle.push(styles.disabled)
+      itemBodyStyle.push(styles.disabled)
     }
     if (task.status === 'FAILED') {
       taskStatusIcon = 'warning'
+      textStyle.push(styles.textDanger)
+      iconStyle.push(styles.iconDanger)
+      itemLeftStyle.push(styles.disabled)
+      itemBodyStyle.push(styles.disabled)
     }
 
-    let style = [ styles.item ]
-
     return (
-      <TouchableOpacity onPress={ () => onTaskClick(task) } style={ style }>
+      <TouchableOpacity onPress={ () => onTaskClick(task) } style={ styles.item }>
         <Grid>
-          <Col size={ 1 } style={{ paddingHorizontal: 5, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Col size={ 1 } style={ itemLeftStyle }>
             <Row style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Icon style={{ fontSize: 14 }} name={ taskTypeIcon } />
+              <Icon style={ iconStyle } name={ taskTypeIcon } />
             </Row>
             <Row style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Icon style={{ fontSize: 14 }} name={ taskStatusIcon } />
+              <Icon style={ iconStyle } name={ taskStatusIcon } />
             </Row>
           </Col>
-          <Col size={ 10 } style={{ padding: 10 }}>
-            <Text style={{ fontSize: 14 }}>{ task.address.streetAddress }</Text>
-            <Text style={{ fontSize: 14 }}>{ moment(task.doneAfter).format('LT') } - { moment(task.doneBefore).format('LT') }</Text>
+          <Col size={ 10 } style={ itemBodyStyle }>
+            <Text style={ textStyle }>{ task.address.streetAddress }</Text>
+            <Text style={ textStyle }>{ moment(task.doneAfter).format('LT') } - { moment(task.doneBefore).format('LT') }</Text>
           </Col>
-          <Col size={ 1 } style={{ paddingHorizontal: 5, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Col size={ 1 } style={ styles.itemLeftRight }>
             <Icon style={{ color: '#ccc' }} name="ios-arrow-forward" />
           </Col>
         </Grid>
