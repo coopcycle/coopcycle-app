6604001aa639873243cc7d660fce749f56db0922
Alexandre Segura <mex.zktk@gmail.com>
2018-02-20 01:20:03 +0100

    More space for buttons.

--- src/components/DateSelectHeader.js
+++ src/components/DateSelectHeader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import { Icon } from 'native-base'
+import { Icon, Button } from 'native-base'
+import { Col, Row, Grid } from 'react-native-easy-grid'
 
 import { primaryColor, whiteColor, dateSelectHeaderHeight, headerFontSize } from "../styles/common"
 
@@ -8,9 +9,6 @@ let styles = StyleSheet.create({
   dateHeader: {
     backgroundColor: primaryColor,
     height: dateSelectHeaderHeight,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row'
   },
   dateHeaderText: {
     color: whiteColor,
@@ -20,19 +18,45 @@ let styles = StyleSheet.create({
   icon: {
     color: whiteColor,
     fontSize: 32
+  },
+  body: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 
 class DateSelectHeader extends React.Component {
 
+  renderButton(iconName, onPress) {
+    return (
+      <Button block transparent onPress={ onPress }>
+        <Icon style={styles.icon} name={ iconName }></Icon>
+      </Button>
+    )
+  }
+
   render () {
     let { toPastDate, toFutureDate, selectedDate, buttonsEnabled } = this.props
 
     return (
-      <View style={styles.dateHeader}>
-        { buttonsEnabled && <Icon style={styles.icon} name="arrow-dropleft" onPress={toPastDate}></Icon> }
-        <Text style={styles.dateHeaderText}>{selectedDate.format('dddd Do MMM')}</Text>
-        { buttonsEnabled && <Icon style={styles.icon} name="arrow-dropright" onPress={toFutureDate}></Icon> }
+      <View style={ styles.dateHeader }>
+        <Grid>
+          <Row>
+            <Col size={ 2 } style={ styles.button }>
+            { buttonsEnabled && this.renderButton('arrow-dropleft', toPastDate) }
+            </Col>
+            <Col size={ 6 } style={ styles.body }>
+              <Text style={styles.dateHeaderText}>{selectedDate.format('dddd Do MMM')}</Text>
+            </Col>
+            <Col size={ 2 } style={ styles.button }>
+            { buttonsEnabled && this.renderButton('arrow-dropright', toFutureDate) }
+            </Col>
+          </Row>
+        </Grid>
       </View>
     )
   }
