913e7f13ccbfc5145ed9f59d4a98b9d542b53ad7
Elias Malik <elias0789@gmail.com>
2018-02-26 10:46:17 +0000

    i18n: Use react-i18next library and add English translations
    Also using the react-native-locale-detector library to detect locale.
    It's the most minimal library I could find (just a few lines)
    
    Github issue: https://github.com/coopcycle/coopcycle-app/issues/21

--- android/app/src/main/java/fr/coopcycle/MainApplication.java
+++ android/app/src/main/java/fr/coopcycle/MainApplication.java
@@ -3,6 +3,13 @@ package fr.coopcycle;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.gettipsi.stripe.StripeReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.corbt.keepawake.KCKeepAwakePackage;
+import com.gettipsi.stripe.StripeReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.corbt.keepawake.KCKeepAwakePackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
 import com.gettipsi.stripe.StripeReactPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +33,13 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new StripeReactPackage(),
+            new MapsPackage(),
+            new ReactNativeI18n(),
+            new KCKeepAwakePackage(),
+            new StripeReactPackage(),
+            new MapsPackage(),
+            new KCKeepAwakePackage(),
             new KCKeepAwakePackage(),
             new StripeReactPackage(),
             new MapsPackage()
---
+++ src/i18n/index.js
@@ -0,0 +1,34 @@
+/*
+ * i18n initialisation
+ *
+ * Initialise the i18n instance to be used in the component hierarchy
+ *
+ * See https://react.i18next.com/components/i18next-instance.html
+ */
+import { Platform } from 'react-native'
+import i18n from 'i18next'
+import { reactI18nextModule } from 'react-i18next'
+import locale from 'react-native-locale-detector'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+
+// https://www.i18next.com/misc/creating-own-plugins.html#languagedetector
+const languageDetector = {
+  type: 'languageDetector',
+  detect: () => locale,
+  init: () => { },
+  cacheUserLanguage: () => { }
+}
+
+i18n
+  .use(languageDetector)
+  .use(reactI18nextModule)
+  .init({
+    fallbackLng: 'en',
+    resources: { en, fr },
+    ns: ['common'],
+    defaultNS: 'common',
+    debug: process.env.DEBUG
+  })
+
+export default i18n
