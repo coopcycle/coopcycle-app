4fcf33807f1a3ae4b33197ab06aa3c0cea656bd4
Alexandre Segura <mex.zktk@gmail.com>
2018-03-01 17:38:45 +0100

    Configure push notifications when app starts.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   View,
   ActivityIndicator,
+  PushNotificationIOS
 } from 'react-native'
 
 import {
@@ -19,8 +20,10 @@ import material from './native-base-theme/variables/material'
 import { NavigationActions, StackNavigator } from 'react-navigation'
 import { Provider } from 'react-redux'
 import { translate, I18nextProvider } from 'react-i18next'
+import PushNotification from 'react-native-push-notification'
 
 import API from './src/API'
+import AppConfig from './src/AppConfig'
 import { Settings } from './src/Settings'
 import { Registry } from './src/Registry'
 import i18n from './src/i18n'
@@ -272,9 +275,54 @@ class App extends Component {
     if (baseURL) {
       client = API.createClient(baseURL, user)
       if (user && user.isAuthenticated() && (user.hasRole('ROLE_COURIER') || user.hasRole('ROLE_ADMIN'))) {
+
         Registry.initWebSocketClient(client)
           .then(() => store.dispatch(wsInit(Registry.getWebSocketClient())))
           .catch(e => console.log(e))
+
+        PushNotification.configure({
+
+          // (optional) Called when Token is generated (iOS and Android)
+          onRegister: function(token) {
+            console.log(`Push notification token for ${token.os}: ${token.token}`)
+            client.post('/api/me/remote_push_tokens', { platform: token.os, token: token.token })
+              .then(remotePushToken => {
+                console.log('Remote push token stored', remotePushToken)
+              })
+              .catch(e => console.log(e))
+          },
+
+          // (required) Called when a remote or local notification is opened or received
+          onNotification: function(notification) {
+            // process the notification
+            // required on iOS only
+            // (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+            notification.finish(PushNotificationIOS.FetchResult.NoData);
+          },
+
+          // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications,
+          // but is need to receive remote push notifications)
+          senderID: AppConfig.hasOwnProperty('GCM_SENDER_ID') ? AppConfig.GCM_SENDER_ID : "",
+
+          // IOS ONLY (optional): default: all - Permissions to register.
+          permissions: {
+            alert: true,
+            badge: true,
+            sound: true
+          },
+
+          // Should the initial notification be popped automatically
+          // default: true
+          popInitialNotification: true,
+
+          /**
+            * (optional) default: true
+            * - Specified if permissions (ios) and token (android and ios) will requested or not,
+            * - if not, you must call PushNotificationsHandler.requestPermissions() later
+            */
+          requestPermissions: true,
+        });
+
       }
     }
 
