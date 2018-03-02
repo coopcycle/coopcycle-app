dafceece400afeaf839009262fb35f0e9fcde86e
Atala <alois.guillope@gmail.com>
2018-03-02 15:29:02 +0100

    fix: use standard yarn conf for travis

--- jestSetup.js
+++ jestSetup.js
@@ -6,7 +6,6 @@ Enzyme.configure({ adapter: new Adapter() })
 
 // Avoid 'is not defined' errors when running tests
 global.FormData = require('FormData')
-global.Request = require('Request')
 
 // mock 'fetch' globally
 global.fetch = require('jest-fetch-mock')
