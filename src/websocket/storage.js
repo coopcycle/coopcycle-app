7b18f51e0097832e8414b80716a0f3a53d0b1dc6
Elias Malik <elias0789@gmail.com>
2018-03-16 11:19:40 +0000

    ws: Add msg queue to WS client using storage abstraction
    JSONAsyncStorage is a simple abstraction on top of AsyncStorage to avoid
    some boilerplate when handling JSON values.
    
    If the WS connection is not open, messages will be added to the queue.
    When the client re-connects, the messages are consumed via the send
    method.
    
    Note that `consume` empties the store before calling the consumer to
    avoid a potential problem with side-effects from the consumer.
    
    Github issue: https://github.com/coopcycle/coopcycle-app/issues/37

---
+++ src/websocket/storage.js
@@ -0,0 +1,43 @@
+/*
+ * JSON Async Storage
+ *
+ * A simple abstraction build on top of react-native's `AsyncStorage`.
+ * Simplifies dealing with the storage and manipulation of stringified JSON
+ */
+import { AsyncStorage } from 'react-native'
+
+
+const JSONAsyncStorage = {
+  get: (key) =>
+    AsyncStorage.getItem(key).then(JSON.parse),
+
+  set: (key, value) =>
+    AsyncStorage.setItem(key, JSON.stringify(value)),
+
+  update: (key, defaultValue, fn) =>
+    JSONAsyncStorage.get(key)
+      .then((v) => v === null ? defaultValue : v)
+      .then(fn)
+      .then((value) => JSONAsyncStorage.set(key, value)),
+
+  consume: (key, fn) => {
+    let data = null
+
+    return JSONAsyncStorage
+      .update(key, null, (json) => {
+        // Data must be cached locally and emptied in the store
+        // before the provided consumer function `fn` is called
+        // incase `fn` manipulates the store
+        data = json
+
+        return Array.isArray(json)
+          ? []
+          : json && typeof json === 'object'
+            ? {}
+            : json
+      })
+      .then(() => Object.values(data).forEach(fn))
+  },
+}
+
+export default JSONAsyncStorage
