8f440db1d3b011bba66e7edf1ac79476b52e394e
Alexandre Segura <mex.zktk@gmail.com>
2018-03-29 15:54:06 +0200

    Add react-native-geolocation-service.

--- android/app/src/main/java/fr/coopcycle/MainApplication.java
+++ android/app/src/main/java/fr/coopcycle/MainApplication.java
@@ -7,6 +7,7 @@ import com.corbt.keepawake.KCKeepAwakePackage;
 import com.gettipsi.stripe.StripeReactPackage;
 import com.coopcycle.pin.RNPinScreenPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -31,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
           new KCKeepAwakePackage(),
           new RNPinScreenPackage(),
           new StripeReactPackage(),
-          new MapsPackage()
+          new MapsPackage(),
+          new RNFusedLocationPackage()
       );
     }
 
