5707ed2f7c20bfa35bd4e92d1ccb2357ece0ee63
Alexandre Segura <mex.zktk@gmail.com>
2018-04-10 02:06:01 +0200

    Update Pods.

--- ios/Pods/Target Support Files/Pods-CoopCycle/Pods-CoopCycle-resources.sh
+++ ios/Pods/Target Support Files/Pods-CoopCycle/Pods-CoopCycle-resources.sh
@@ -84,10 +84,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "$PODS_CONFIGURATION_BUILD_DIR/Stripe/Stripe.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/Stripe/Stripe.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "$PODS_CONFIGURATION_BUILD_DIR/Stripe/Stripe.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/Stripe/Stripe.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
