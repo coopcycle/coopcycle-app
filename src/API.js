89dcaaf1eed3ade6101304bbabff28d8da6eae84
Alexandre Segura <mex.zktk@gmail.com>
2018-05-19 22:36:15 +0200

    Set Content-Type to application/ld+json.

--- src/API.js
+++ src/API.js
@@ -32,7 +32,7 @@ Client.prototype.createRequest = function(method, uri, data) {
 Client.prototype.createAuthorizedRequest = function(method, uri, data) {
   var headers = new Headers();
   headers.append("Authorization", "Bearer " + this.model.token);
-  headers.append("Content-Type", "application/json");
+  headers.append("Content-Type", "application/ld+json");
 
   var options = {
     method: method,
