3279f24c96bb59e80a448a989b2b208ad14386bf
Alexandre Segura <mex.zktk@gmail.com>
2018-06-12 21:36:12 +0200

    Extract navigator definition outside render.

--- src/navigation/navigators/HomeTab.js
+++ src/navigation/navigators/HomeTab.js
@@ -7,6 +7,53 @@ import { translate } from 'react-i18next'
 import AccountPage from '../AccountPage'
 import Home from '../Home'
 import RestaurantsPage from '../RestaurantsPage'
+import i18n from '../../i18n'
+
+const tabNavigatorConfig = {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: '#e4022d',
+    inactiveTintColor: '#4c4c4c',
+    showLabel: false,
+    showIcon: true,
+    style: {
+      backgroundColor: '#fff',
+    },
+    indicatorStyle: {
+      backgroundColor: '#e4022d'
+    }
+  },
+}
+
+const TabNav = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: i18n.t('HOME'),
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="home" style={{ color: tintColor }} />
+      ),
+    })
+  },
+  Restaurants: {
+    screen: RestaurantsPage,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: i18n.t('RESTAURANTS'),
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="restaurant" style={{ color: tintColor }} />
+      ),
+    })
+  },
+  Account: {
+    screen: AccountPage,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: i18n.t('MY_ACCOUNT'),
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="person" style={{ color: tintColor }} />
+      ),
+    })
+  },
+}, tabNavigatorConfig)
 
 class HomePage extends Component {
 
@@ -41,54 +88,8 @@ class HomePage extends Component {
 
   render() {
 
-    const tabNavigatorConfig = {
-      tabBarPosition: 'bottom',
-      tabBarOptions: {
-        activeTintColor: '#e4022d',
-        inactiveTintColor: '#4c4c4c',
-        showLabel: false,
-        showIcon: true,
-        style: {
-          backgroundColor: '#fff',
-        },
-        indicatorStyle: {
-          backgroundColor: '#e4022d'
-        }
-      },
-    }
-
     // Need to pass navigation prop from StackNavigator to trigger actions
     const screenProps = { ...this.props.navigation.state.params, navigation: this.props.navigation }
-
-    const TabNav = TabNavigator({
-      Home: {
-        screen: Home,
-        navigationOptions: ({ navigation }) => ({
-          tabBarLabel: this.props.t('HOME'),
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="home" style={{ color: tintColor }} />
-          ),
-        })
-      },
-      Restaurants: {
-        screen: RestaurantsPage,
-        navigationOptions: ({ navigation }) => ({
-          tabBarLabel: this.props.t('RESTAURANTS'),
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="restaurant" style={{ color: tintColor }} />
-          ),
-        })
-      },
-      Account: {
-        screen: AccountPage,
-        navigationOptions: ({ navigation }) => ({
-          tabBarLabel: this.props.t('MY_ACCOUNT'),
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="person" style={{ color: tintColor }} />
-          ),
-        })
-      },
-    }, tabNavigatorConfig)
 
     return (
       <TabNav screenProps={ screenProps } />
