94de062f864141f09693e3134d092f59fd2b74d4
Alexandre Segura <mex.zktk@gmail.com>
2018-12-20 13:25:25 +0100

    Handle text in HeaderButton.

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
-import { TouchableOpacity } from 'react-native'
-import { Icon } from 'native-base'
+import { TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, Icon } from 'native-base'
 
 class HeaderButton extends Component {
+
   render() {
+
+    const containerStyles = [ styles.base ]
+    if (this.props.textLeft) {
+      containerStyles.push(styles.withText)
+    }
+
     return (
       <TouchableOpacity
         onPress={ this.props.onPress }
-        style={{ paddingHorizontal: 20, paddingVertical: 5 }}>
+        style={ containerStyles }>
+        { this.props.textLeft && (
+          <Text style={ styles.textLeft }>{ this.props.textLeft }</Text>
+        )}
         <Icon name={ this.props.iconName } style={{ color: '#fff' }} />
       </TouchableOpacity>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  base: {
+    paddingHorizontal: 20,
+    paddingVertical: 5
+  },
+  withText: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  textLeft: {
+    color: '#fff',
+    paddingRight: 15
+  }
+})
 
 export default HeaderButton
