2a9cd78ca4856908cf598988649474d910a86df3
Alexandre Segura <mex.zktk@gmail.com>
2019-02-03 00:08:43 +0100

    Allow specifying icon type.

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -11,6 +11,14 @@ class HeaderButton extends Component {
       containerStyles.push(styles.withText)
     }
 
+    let iconProps = {}
+    if (this.props.iconType) {
+      iconProps = {
+        ...iconProps,
+        type: this.props.iconType
+      }
+    }
+
     return (
       <TouchableOpacity
         onPress={ this.props.onPress }
@@ -18,7 +26,7 @@ class HeaderButton extends Component {
         { this.props.textLeft && (
           <Text style={ styles.textLeft }>{ this.props.textLeft }</Text>
         )}
-        <Icon name={ this.props.iconName } style={{ color: '#fff' }} />
+        <Icon name={ this.props.iconName } style={{ color: '#fff' }} { ...iconProps } />
       </TouchableOpacity>
     )
   }
