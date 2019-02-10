3c3ddc96f82ac279657c4b41a488d058554b952e
Alexandre Segura <mex.zktk@gmail.com>
2019-02-10 12:27:21 +0100

    Improve registration form.
    - Show error messages if any.
    - Use same validation constraints as web.
    - Allow entering phone number in country format.

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,28 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Form, Item, Input, Label, Button, Text } from 'native-base'
+import { TextInput, TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Form, Item, Input, Label, Button, Icon, Text } from 'native-base'
 import { translate } from 'react-i18next'
 import validate from 'validate.js'
 import { PhoneNumberUtil } from 'google-libphonenumber'
+import phoneNumberExamples from 'libphonenumber-js/examples.mobile.json'
+import {
+  getExampleNumber,
+  parsePhoneNumberFromString,
+  AsYouType } from 'libphonenumber-js'
+import { Flag } from 'react-native-svg-flagkit'
 import _ from 'lodash'
+
 import i18n from '../i18n'
+import Settings from '../Settings'
 
 const phoneUtil = PhoneNumberUtil.getInstance()
 
-// Custom validator for phone numbers
-// `parseAndKeepRawInput` throws exceptions if it receives an un-parseable input
-// (including a phone number without a country-code)
 validate.validators.phoneNumber = (value, options, key, attributes) => {
-  let number = ''
 
-  try {
-    return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(value))
-      ? null
-      : options.message
+  if (!value) {
 
-  } catch (error) {
     return options.message
+  }
 
+  const country = Settings.get('country').toUpperCase()
+
+  const phoneNumber = parsePhoneNumberFromString(value, country)
+  if (phoneNumber) {
+
+    return phoneNumber.isValid() ? null : options.message
   }
 }
 
@@ -53,9 +60,15 @@ const inputs = [
     label: i18n.t('USERNAME'),
     constraints: {
       presence: { message: i18n.t('INVALID_USERNAME') },
+      // https://github.com/coopcycle/coopcycle-web/blob/82220bc36dd3c44e6e770abee022c6f736f4228c/src/AppBundle/Entity/ApiUser.php#L40-L45
       length: {
-        minimum: 2,
+        minimum: 3,
+        maximum: 15,
         message: i18n.t('INVALID_USERNAME'),
+      },
+      format: {
+        pattern: /^[a-zA-Z0-9_]{3,15}$/,
+        message: i18n.t('INVALID_USERNAME_FORMAT'),
       },
     }
   },
@@ -67,8 +80,10 @@ const inputs = [
     },
     constraints: {
       presence: { message: i18n.t('INVALID_PASSWORD') },
+      // https://github.com/FriendsOfSymfony/FOSUserBundle/blob/ee76c57c6a0966c24f4f9a693790ecd61bf2ddce/Resources/config/validation.xml#L65-L75
       length: {
         minimum: 8,
+        maximum: 4096,
         message: i18n.t('INVALID_PASSWORD'),
       },
     }
@@ -157,39 +172,105 @@ class RegisterForm extends React.Component {
         errors: newErrors
       })
 
-      // TODO Use first error message
-      // Object.values(newErrors)[0]
-
       return
     }
 
-    this.props.onSubmit(data)
+    const country = Settings.get('country').toUpperCase()
+    const phoneNumber = parsePhoneNumberFromString(data.telephone, country)
+
+    const newData = {
+      ...data,
+      telephone: phoneNumber.format('E.164')
+    }
+
+    this.props.onSubmit(newData)
+  }
+
+  _onChangeTelephone(value) {
+
+    const country = Settings.get('country').toUpperCase()
+
+    this.setState({
+      telephone: new AsYouType(country).input(value)
+    })
+  }
+
+  renderErrors(errors) {
+
+    return (
+      <View>
+        { errors.map((message, key) => (<Text key={ key } note style={{ marginLeft: 15, color: '#ed2f2f' }}>{ message }</Text>)) }
+      </View>
+
+    )
   }
 
   render() {
 
     const { errors } = this.state
+    const country = Settings.get('country').toUpperCase()
 
     return (
       <View>
         <Form>
           { inputs.map(input => {
 
-            const itemProps =
-              errors.hasOwnProperty(input.name) ? { error: true } : {}
+            const hasErrors = errors.hasOwnProperty(input.name)
+            const itemProps = hasErrors ? { error: true } : {}
+
+            if (input.name === 'telephone') {
+
+              // phoneUtil.getCountryCodeForRegion('FR')
+
+              const telephoneInputStyle = [ styles.telephoneInput ]
+              if (hasErrors) {
+                telephoneInputStyle.push(styles.telephoneInputError)
+              }
+
+              const phoneNumberExample = getExampleNumber(country, phoneNumberExamples)
+
+              // @see https://github.com/jackocnr/intl-tel-input
+              // @see https://medium.com/devopslinks/building-an-international-react-native-phone-input-with-expo-and-native-base-9040d935e206
+              return (
+                <View key={ input.name }>
+                  <View style={ styles.telephoneItem }>
+                    <Label style={ styles.stackedLabel }>{ input.label }</Label>
+                    <View style={ telephoneInputStyle }>
+                      <View>
+                        <TouchableOpacity style={ styles.telephoneCodeBtn }>
+                          <Flag id={ country } size={ 0.1 } />
+                        </TouchableOpacity>
+                      </View>
+                      <TextInput
+                        defaultValue={ this.state[input.name] }
+                        value={ this.state[input.name] }
+                        placeholder={ phoneNumberExample.formatNational() }
+                        autoCorrect={ false }
+                        autoCapitalize="none"
+                        style={{ flex: 1, height: 40, fontSize: 18 }}
+                        onChangeText={ this._onChangeTelephone.bind(this) }
+                        { ...input.props } />
+                    </View>
+                  </View>
+                  { hasErrors && this.renderErrors(errors[input.name]) }
+                </View>
+              )
+            }
 
             return (
-              <Item key={ input.name } stackedLabel { ...itemProps }>
-                <Label>{ input.label }</Label>
-                <Input
-                  defaultValue={ this.state[input.name] }
-                  autoCorrect={ false }
-                  autoCapitalize="none"
-                  style={{ height: 40 }}
-                  onChangeText={ value => this.setState({ [input.name]: value }) }
-                  { ...input.props }
-                />
-              </Item>
+              <View key={ input.name }>
+                <Item stackedLabel { ...itemProps }>
+                  <Label>{ input.label }</Label>
+                  <Input
+                    defaultValue={ this.state[input.name] }
+                    autoCorrect={ false }
+                    autoCapitalize="none"
+                    style={{ height: 40 }}
+                    onChangeText={ value => this.setState({ [input.name]: value }) }
+                    { ...input.props } />
+                </Item>
+                { hasErrors && this.renderErrors(errors[input.name]) }
+              </View>
             )
           }) }
         </Form>
@@ -202,5 +283,35 @@ class RegisterForm extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  telephoneItem: {
+    marginLeft: 15,
+  },
+  telephoneInput: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  telephoneInputError: {
+    borderBottomColor: '#ed2f2f',
+    borderBottomWidth: 1
+  },
+  telephoneCodeBtn: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#e7e7e7',
+    borderWidth: 1,
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  stackedLabel: {
+    fontSize: 15,
+    color: '#575757',
+    marginTop: 10,
+    marginBottom: 5,
+  }
+})
 
 export default translate()(RegisterForm)
