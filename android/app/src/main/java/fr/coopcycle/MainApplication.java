70eb16f88dc86fbb4bb4587f448d2e4a89af06d4
Alexandre Segura <mex.zktk@gmail.com>
2019-03-28 13:45:22 +0100

    Link react-native-camera for Android.

--- android/app/src/main/java/fr/coopcycle/MainApplication.java
+++ android/app/src/main/java/fr/coopcycle/MainApplication.java
@@ -2,6 +2,7 @@ package fr.coopcycle;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.polidea.reactnativeble.BlePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.terrylinla.rnsketchcanvas.SketchCanvasPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNCameraPackage(),
           new BlePackage(),
           new RNFetchBlobPackage(),
           new SketchCanvasPackage(),
