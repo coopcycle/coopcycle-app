81abdbe9deb88513562ad0c5cd2bfd84ae7eb4da
Alexandre Segura <mex.zktk@gmail.com>
2019-03-29 13:05:40 +0100

    Upgrade to React Native 0.59.

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,13 @@ module.exports = (async () => {
   } = await getDefaultConfig();
   return {
     transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
