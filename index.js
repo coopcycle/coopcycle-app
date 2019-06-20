61e003d592600c20cc646ca277b17c4d17911bff
Alexandre Segura <mex.zktk@gmail.com>
2019-06-20 21:35:51 +0200

    Activate Sentry.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config('https://78c4cd6982b1431da17959adeb1f1fdf@sentry.io/1486946').install();
 
 AppRegistry.registerComponent('CoopCycle', () => App);
