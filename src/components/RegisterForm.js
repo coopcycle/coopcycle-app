0db6abae2e6bdf4d72e985cda0703bedeeecc54f
Alexandre Segura <mex.zktk@gmail.com>
2019-08-16 00:32:31 +0200

    Simplify RegisterForm, remove flag icon.

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextInput, TouchableOpacity, StyleSheet, View } from 'react-native'
+import { TextInput, TouchableOpacity, View } from 'react-native'
 import { Form, Item, Input, Label, Button, Icon, Text } from 'native-base'
 import { withTranslation } from 'react-i18next'
 import validate from 'validate.js'
@@ -9,7 +9,6 @@ import {
   getExampleNumber,
   parsePhoneNumberFromString,
   AsYouType } from 'libphonenumber-js'
-import Flag from '../components/Flag'
 import _ from 'lodash'
 
 import i18n from '../i18n'
@@ -163,6 +162,8 @@ class RegisterForm extends React.Component {
 
     this._inputComponents = new Map()
     this._onSubmit.bind(this)
+    this.country = Settings.get('country').toUpperCase()
+    this.phoneNumberPlaceholder = getExampleNumber(this.country, phoneNumberExamples).formatNational()
   }
 
   _onSubmit() {
@@ -178,8 +179,7 @@ class RegisterForm extends React.Component {
       return
     }
 
-    const country = Settings.get('country').toUpperCase()
-    const phoneNumber = parsePhoneNumberFromString(data.telephone, country)
+    const phoneNumber = parsePhoneNumberFromString(data.telephone, this.country)
 
     const newData = {
       ...data,
@@ -190,11 +190,8 @@ class RegisterForm extends React.Component {
   }
 
   _onChangeTelephone(value) {
-
-    const country = Settings.get('country').toUpperCase()
-
     this.setState({
-      telephone: new AsYouType(country).input(value)
+      telephone: new AsYouType(this.country).input(value)
     })
   }
 
@@ -211,56 +208,48 @@ class RegisterForm extends React.Component {
   render() {
 
     const { errors } = this.state
-    const country = Settings.get('country').toUpperCase()
 
     return (
       <View>
         <Form>
-          { inputs.map(input => {
+          { inputs.map((input, index) => {
 
             const hasErrors = errors.hasOwnProperty(input.name)
             const itemProps = hasErrors ? { error: true } : {}
 
+            let inputProps = { ...input.props }
+
+            const isLast = index === (inputs.length - 1)
+
             if (input.name === 'telephone') {
-
-              // phoneUtil.getCountryCodeForRegion('FR')
-
-              const telephoneInputStyle = [ styles.telephoneInput ]
-              if (hasErrors) {
-                telephoneInputStyle.push(styles.telephoneInputError)
+              inputProps = {
+                ...inputProps,
+                placeholder: this.phoneNumberPlaceholder,
+                onChangeText: this._onChangeTelephone.bind(this),
               }
+            } else {
+              inputProps = {
+                ...inputProps,
+                onChangeText: value => this.setState({ [input.name]: value }),
+              }
+            }
 
-              const phoneNumberExample = getExampleNumber(country, phoneNumberExamples)
-
-              // @see https://github.com/jackocnr/intl-tel-input
-              // @see https://medium.com/devopslinks/building-an-international-react-native-phone-input-with-expo-and-native-base-9040d935e206
-              return (
-                <View key={ input.name }>
-                  <View style={ styles.telephoneItem }>
-                    <Label style={ styles.stackedLabel }>{ input.label }</Label>
-                    <View style={ telephoneInputStyle }>
-                      <View>
-                        <TouchableOpacity style={ styles.telephoneCodeBtn }>
-                          <Flag country={ country } width={ 30 } height={ 20 } />
-                        </TouchableOpacity>
-                      </View>
-                      <Input
-                        ref={component => this._inputComponents.set(input.name, component)}
-                        defaultValue={ this.state[input.name] }
-                        value={ this.state[input.name] }
-                        placeholder={ phoneNumberExample.formatNational() }
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        style={{ flex: 1, height: 40, fontSize: 18 }}
-                        onChangeText={ this._onChangeTelephone.bind(this) }
-                        { ...input.props }
-                        returnKeyType="done"
-                        onSubmitEditing={ _ => this._onSubmit() }/>
-                    </View>
-                  </View>
-                  { hasErrors && this.renderErrors(errors[input.name]) }
-                </View>
-              )
+            if (isLast) {
+              inputProps = {
+                ...inputProps,
+                returnKeyType: 'done',
+                onSubmitEditing: _ => this._onSubmit()
+              }
+            } else {
+              inputProps = {
+                ...inputProps,
+                returnKeyType: 'next',
+                onSubmitEditing: event => {
+                  let index = inputs.findIndex((el) => el.name == input.name)
+                  let nextInputName = inputs[index + 1].name
+                  this._inputComponents.get(nextInputName)._root.focus()
+                }
+              }
             }
 
             return (
@@ -268,19 +257,13 @@ class RegisterForm extends React.Component {
                 <Item stackedLabel { ...itemProps }>
                   <Label>{ input.label }</Label>
                   <Input
-                    ref={component => this._inputComponents.set(input.name, component)}
+                    ref={ component => this._inputComponents.set(input.name, component) }
                     defaultValue={ this.state[input.name] }
                     autoCorrect={ false }
                     autoCapitalize="none"
                     style={{ height: 40 }}
                     onChangeText={ value => this.setState({ [input.name]: value }) }
-                    { ...input.props }
-                    returnKeyType="next"
-                    onSubmitEditing={ event => {
-                      let index = inputs.findIndex((el) => el.name == input.name)
-                      let nextInputName = inputs[index + 1].name
-                      this._inputComponents.get(nextInputName)._root.focus()
-                    } }/>
+                    { ...inputProps } />
                 </Item>
                 { hasErrors && this.renderErrors(errors[input.name]) }
               </View>
@@ -296,35 +279,5 @@ class RegisterForm extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  telephoneItem: {
-    marginLeft: 15,
-  },
-  telephoneInput: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  telephoneInputError: {
-    borderBottomColor: '#ed2f2f',
-    borderBottomWidth: 1
-  },
-  telephoneCodeBtn: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderColor: '#e7e7e7',
-    borderWidth: 1,
-    marginRight: 10,
-    paddingHorizontal: 10,
-  },
-  stackedLabel: {
-    fontSize: 15,
-    color: '#575757',
-    marginTop: 10,
-    marginBottom: 5,
-  }
-})
 
 export default withTranslation()(RegisterForm)
