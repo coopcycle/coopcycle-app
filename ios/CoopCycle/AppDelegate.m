cb4206110f9600853f9f0dd2174cee0e6536e172
Alexandre Segura <mex.zktk@gmail.com>
2019-11-06 10:57:31 +0100

    Enable app links in AppDelegate.m.

--- ios/CoopCycle/AppDelegate.m
+++ ios/CoopCycle/AppDelegate.m
@@ -76,5 +76,12 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
   return [RCTLinkingManager application:application openURL:url
                       sourceApplication:sourceApplication annotation:annotation];
 }
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
 
 @end
