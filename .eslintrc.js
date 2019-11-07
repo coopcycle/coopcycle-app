c6e5db02d6b718849a9a50fe9a71ffa3932780bb
Alexandre Segura <mex.zktk@gmail.com>
2019-11-07 13:40:54 +0100

    Override ESLint config to match project style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    semi: 0,
+    // Disable Prettier, because it conflicts with ESLint's "object-curly-spacing"
+    // https://github.com/prettier/prettier/issues/1871
+    // https://itnext.io/how-to-replace-prettier-by-eslint-rules-21574359e041
+    'prettier/prettier': 'off',
+    // Allow inline styles (should be removed at some point)
+    'react-native/no-inline-styles': 'off'
+  }
 };
