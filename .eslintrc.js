e889a9b249f402bb13b5531bad0252bd20e9da30
Alexandre Segura <mex.zktk@gmail.com>
2019-11-07 14:00:08 +0100

    Disable space-unary-ops rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     semi: 0,
+    'space-unary-ops': 0,
     // Disable Prettier, because it conflicts with ESLint's "object-curly-spacing"
     // https://github.com/prettier/prettier/issues/1871
     // https://itnext.io/how-to-replace-prettier-by-eslint-rules-21574359e041
