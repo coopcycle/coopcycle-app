665927491445efea3432143ac74fda8899850c44
Alexandre Segura <mex.zktk@gmail.com>
2019-11-26 09:21:22 +0100

    Remove dead code.

--- src/Settings.js
+++ src/Settings.js
@@ -1,63 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage'
 
 import API from './API'
-import _ from 'lodash'
-
-const loadServerSettingsHash = baseURL => {
-  return new Promise((resolve, reject) => {
-    try {
-      AsyncStorage.getItem(`@Settings.hash.${baseURL}`)
-        .then((data, e) => {
-          if (e) {
-            return reject(e.message)
-          }
-          resolve(data)
-        })
-    } catch (error) {
-      reject(error.message)
-    }
-  })
-}
-
-const loadServerSettings = () => {
-  return new Promise((resolve, reject) => {
-    try {
-      AsyncStorage.getItem('@Settings.server')
-        .then((data, error) => {
-          if (error) {
-            return reject(error)
-          }
-          if (!data) {
-            return reject('No server settings found')
-          }
-          resolve(JSON.parse(data))
-        })
-    } catch (e) {
-      reject(e.message)
-    }
-  })
-}
-
-const saveServerSettings = (baseURL, settings, hash) => {
-  return new Promise((resolve, reject) => {
-    try {
-      Promise.all([
-        AsyncStorage.setItem(`@Settings.hash.${baseURL}`, hash),
-        AsyncStorage.setItem('@Settings.server', JSON.stringify(settings))
-      ])
-      .then(values => {
-        const errors = _.filter(values)
-        if (errors.length > 0) {
-          return reject()
-        }
-        resolve()
-      })
-      .catch(e => reject(e))
-    } catch (e) {
-      reject(e.message)
-    }
-  })
-}
 
 const defaultSettings = {
   google_api_key: '',
@@ -65,7 +8,6 @@ const defaultSettings = {
   locale: 'fr',
   country: 'fr',
   latlng: '48.872178,2.331797',
-  piwik_site_id: -1,
 }
 
 let serverSettings = {}
@@ -143,41 +85,6 @@ class Settings {
           resolve(serverSettings)
         })
         .catch(e => reject(e))
-
-      /*
-      return loadServerSettingsHash(baseURL)
-        .then(hash => {
-          const client = API.createClient(baseURL)
-          if (hash) {
-            return client.get('/api/settings?format=hash')
-              .then(remoteHash => {
-                if (remoteHash === hash) {
-                  return loadServerSettings()
-                }
-                return client.get('/api/settings')
-                  .then(settings => {
-                    return saveServerSettings(baseURL, settings, remoteHash)
-                      .then(() => settings)
-                  })
-              })
-          } else {
-            return Promise.all([
-              client.get('/api/settings'),
-              client.get('/api/settings?format=hash')
-            ]).then(values => {
-              const [ settings, hash ] = values
-              return saveServerSettings(baseURL, settings, hash)
-                .then(() => settings)
-            })
-          }
-        })
-        .then(settings => {
-          serverSettings = Object.assign(defaultSettings, settings)
-          return serverSettings
-        })
-        .then(settings => resolve(settings))
-        .catch(e => reject(e))
-      */
     })
   }
 
