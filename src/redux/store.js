df77bf3aee89a6a04627e840d17b79a2e6852536
Alexandre Segura <mex.zktk@gmail.com>
2019-12-24 18:55:47 +0100

    Add redux-async-queue middleware.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import ReduxAsyncQueue from 'redux-async-queue'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger'
 import reducers from './reducers'
 import PreferencesMiddleware from './middlewares/PreferencesMiddleware'
 import WsMiddleware from './middlewares/WebSocketMiddleware'
 
-const middlewares = [ thunk, PreferencesMiddleware, WsMiddleware() ]
+const middlewares = [ thunk, ReduxAsyncQueue, PreferencesMiddleware, WsMiddleware() ]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(createLogger({ collapsed: true }))
