e4b30cd54e44227e149a6df678cc1a455bae6962
Alexandre Segura <mex.zktk@gmail.com>
2020-01-22 20:43:48 +0100

    Setup Fastlane.

---
+++ ios/CoopCycleUITests/CoopCycleUITests.swift
@@ -0,0 +1,36 @@
+//
+//  CoopCycleUITests.swift
+//  CoopCycleUITests
+//
+//  Created by admin on 22/01/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import XCTest
+
+class CoopCycleUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
