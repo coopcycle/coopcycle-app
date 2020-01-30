f789e9f8a718f79d603fa747d0c64e260352d8ea
Alexandre Segura <mex.zktk@gmail.com>
2020-01-30 11:58:17 +0100

    Bigger timeout.

--- ios/CoopCycleUITests/CoopCycleUITests.swift
+++ ios/CoopCycleUITests/CoopCycleUITests.swift
@@ -15,7 +15,7 @@ class CoopCycleUITests: XCTestCase {
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-      
+
         super.setUp()
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -38,37 +38,38 @@ class CoopCycleUITests: XCTestCase {
         // https://krausefx.com/blog/run-xcode-7-ui-tests-from-the-command-line
         // https://www.hackingwithswift.com/articles/148/xcode-ui-testing-cheat-sheet
         // https://www.raywenderlich.com/233168-fastlane-tutorial-getting-started
-      
+
         let app = XCUIApplication()
 
         // Wait for the button to appear
-        app.otherElements["chooseCityBtn"].waitForExistence(timeout: 15)
-      
+        // Use a large timeout, to make sure the JavaScript bundle has been downloaded
+        app.otherElements["chooseCityBtn"].waitForExistence(timeout: 120)
+
         snapshot("00_Home")
-      
+
         // Tap the button
         app.otherElements["chooseCityBtn"].firstMatch.tap()
-      
+
         snapshot("01_ChooseCity")
-      
+
         app.otherElements["Poitiers"].firstMatch.tap()
-      
+
         snapshot("02_Restaurants")
-      
+
         app.otherElements["restaurantList"].waitForExistence(timeout: 15)
-      
+
         app.otherElements["restaurants:2"].tap()
-      
+
         snapshot("03_Restaurant")
-      
+
         app.otherElements["menu"].waitForExistence(timeout: 5)
         app.otherElements["menuItem:0:0"].waitForExistence(timeout: 5)
-      
+
         app.otherElements["menuItem:0:0"].tap()
-      
+
         app.otherElements["addressModal"].waitForExistence(timeout: 5)
         app.textFields["addressModalTypeahead"].waitForExistence(timeout: 5)
-      
+
         app.textFields["addressModalTypeahead"].typeText("23 av claude vellefaux")
     }
 
