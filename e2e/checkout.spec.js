220b62ad645c698259cee0e353e6950133a82675
Alexandre Segura <mex.zktk@gmail.com>
2020-01-30 14:21:43 +0100

    Take screenshots with Detox.
    - Run tests in all locales.

--- e2e/checkout.spec.js
+++ e2e/checkout.spec.js
@@ -1,82 +1,107 @@
-describe('Checkout', () => {
+const LOCALES = ['en-US', 'es-ES', 'fr-FR'];
 
-  beforeEach(async () => {
-    await device.reloadReactNative()
-  })
+LOCALES.forEach(locale => {
 
-  it('should complete checkout', async () => {
+  describe('Checkout', () => {
 
-    await expect(element(by.id('chooseCityBtn'))).toBeVisible()
-    await element(by.id('chooseCityBtn')).tap()
+    beforeEach(async () => {
+      await device.relaunchApp({
+        newInstance: true,
+        permissions: {
+          notifications: 'YES',
+        },
+        delete: true,
+        languageAndLocale: {
+          language: locale,
+          locale
+        }
+      })
+      await device.reloadReactNative()
+    })
 
-    await expect(element(by.id('moreServerOptions'))).toBeVisible()
-    await element(by.id('moreServerOptions')).tap()
+    it(`should complete checkout in ${locale}`, async () => {
 
-    await element(by.id('customServerURL')).typeText('demo.coopcycle.org')
-    await element(by.id('submitCustomServer')).tap()
+      await expect(element(by.id('chooseCityBtn'))).toBeVisible()
 
-    await expect(element(by.id('checkoutSearch'))).toBeVisible()
-    await expect(element(by.id('restaurantList'))).toBeVisible()
+      await device.takeScreenshot(`Home-${locale}`);
 
-    await waitFor(element(by.id('restaurantMatches:2')))
-      .toBeVisible()
-      .whileElement(by.id('restaurantList')).scroll(80, 'down')
-    await element(by.id('restaurantMatches:2')).tap()
+      await element(by.id('chooseCityBtn')).tap()
 
-    await waitFor(element(by.id('menu'))).toExist().withTimeout(5000)
-    await waitFor(element(by.id('menuItem:0:0'))).toExist().withTimeout(5000)
-    await waitFor(element(by.id('menuItem:0:1'))).toExist().withTimeout(5000)
-    await waitFor(element(by.id('menuItem:0:2'))).toExist().withTimeout(5000)
+      await expect(element(by.id('moreServerOptions'))).toBeVisible()
 
-    // Add item
-    await element(by.id('menuItem:0:0')).tap()
+      await device.takeScreenshot(`ChooseServer-${locale}`)
 
-    await waitFor(element(by.id('addressModal'))).toExist().withTimeout(5000)
-    await waitFor(element(by.id('addressModalTypeahead'))).toExist().withTimeout(5000)
-    await element(by.id('addressModalTypeahead')).typeText('23 av claude vellefaux')
+      await element(by.id('moreServerOptions')).tap()
 
-    await waitFor(element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE'))).toBeVisible().withTimeout(5000)
-    await element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE')).tap()
+      await element(by.id('customServerURL')).typeText('demo.coopcycle.org')
+      await element(by.id('submitCustomServer')).tap()
 
-    // Check if footer is present
-    await waitFor(element(by.id('cartFooter'))).toExist().withTimeout(5000)
-    await expect(element(by.id('cartFooter'))).toBeVisible()
+      await expect(element(by.id('checkoutSearch'))).toBeVisible()
+      await expect(element(by.id('restaurantList'))).toBeVisible()
 
-    // Add 2 more items
-    await element(by.id('menuItem:0:1')).tap()
-    await element(by.id('menuItem:0:2')).tap()
+      await waitFor(element(by.id('restaurantMatches:2')))
+        .toBeVisible()
+        .whileElement(by.id('restaurantList')).scroll(80, 'down')
+      await element(by.id('restaurantMatches:2')).tap()
 
-    await waitFor(element(by.id('cartSubmit'))).toExist().withTimeout(5000)
-    await element(by.id('cartSubmit')).tap()
+      await waitFor(element(by.id('menu'))).toExist().withTimeout(5000)
+      await waitFor(element(by.id('menuItem:0:0'))).toExist().withTimeout(5000)
+      await waitFor(element(by.id('menuItem:0:1'))).toExist().withTimeout(5000)
+      await waitFor(element(by.id('menuItem:0:2'))).toExist().withTimeout(5000)
 
-    await expect(element(by.id('cartSummarySubmit'))).toBeVisible()
-    await element(by.id('cartSummarySubmit')).tap()
+      await device.takeScreenshot(`Restaurant-${locale}`)
 
-    await expect(element(by.id('loginUsername'))).toBeVisible()
-    await expect(element(by.id('loginPassword'))).toBeVisible()
+      // Add item
+      await element(by.id('menuItem:0:0')).tap()
 
-    await element(by.id('loginUsername')).typeText('user_1')
-    await element(by.id('loginPassword')).typeText('user_1')
-    await element(by.id('loginSubmit')).tap()
+      await waitFor(element(by.id('addressModal'))).toExist().withTimeout(5000)
+      await waitFor(element(by.id('addressModalTypeahead'))).toExist().withTimeout(5000)
+      await element(by.id('addressModalTypeahead')).typeText('23 av claude vellefaux')
 
-    await expect(element(by.id('checkoutTelephone'))).toBeVisible()
-    await expect(element(by.id('moreInfosSubmit'))).toBeVisible()
+      await waitFor(element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE'))).toBeVisible().withTimeout(5000)
+      await element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE')).tap()
 
-    // Append "\n" to make sure virtual keybord is hidden after entry
-    // https://github.com/wix/detox/issues/209
-    await element(by.id('checkoutTelephone')).typeText('0612345678\n')
+      // Check if footer is present
+      await waitFor(element(by.id('cartFooter'))).toExist().withTimeout(5000)
+      await expect(element(by.id('cartFooter'))).toBeVisible()
 
-    await element(by.id('moreInfosSubmit')).tap()
+      // Add 2 more items
+      await element(by.id('menuItem:0:1')).tap()
+      await element(by.id('menuItem:0:2')).tap()
 
-    await waitFor(element(by.id('creditCardNumber'))).toExist().withTimeout(5000)
-    // await expect(element(by.id('creditCardNumber'))).toBeVisible()
+      await waitFor(element(by.id('cartSubmit'))).toExist().withTimeout(5000)
+      await element(by.id('cartSubmit')).tap()
 
-    await element(by.id('creditCardNumber')).typeText('4242424242424242')
-    await element(by.id('creditCardExpiry')).typeText('1221')
-    await element(by.id('creditCardCvc')).typeText('123')
+      await expect(element(by.id('cartSummarySubmit'))).toBeVisible()
+      await element(by.id('cartSummarySubmit')).tap()
 
-    await element(by.id('creditCardSubmit')).tap()
+      await expect(element(by.id('loginUsername'))).toBeVisible()
+      await expect(element(by.id('loginPassword'))).toBeVisible()
 
-    // await expect(element(by.id('accountOrder'))).toBeVisible()
+      await element(by.id('loginUsername')).typeText('user_1')
+      await element(by.id('loginPassword')).typeText('user_1')
+      await element(by.id('loginSubmit')).tap()
+
+      await expect(element(by.id('checkoutTelephone'))).toBeVisible()
+      await expect(element(by.id('moreInfosSubmit'))).toBeVisible()
+
+      // Append "\n" to make sure virtual keybord is hidden after entry
+      // https://github.com/wix/detox/issues/209
+      await element(by.id('checkoutTelephone')).typeText('0612345678\n')
+
+      await element(by.id('moreInfosSubmit')).tap()
+
+      await waitFor(element(by.id('creditCardNumber'))).toExist().withTimeout(5000)
+      // await expect(element(by.id('creditCardNumber'))).toBeVisible()
+
+      await element(by.id('creditCardNumber')).typeText('4242424242424242')
+      await element(by.id('creditCardExpiry')).typeText('1221')
+      await element(by.id('creditCardCvc')).typeText('123')
+
+      await element(by.id('creditCardSubmit')).tap()
+
+      // await expect(element(by.id('accountOrder'))).toBeVisible()
+    })
+
   })
 })
