1cdbcab76ae9e255505f090f84fe35fd7cba4653
Alexandre Segura <mex.zktk@gmail.com>
2020-02-01 13:47:52 +0100

    Assign task during test.

--- e2e/dispatch.spec.js
+++ e2e/dispatch.spec.js
@@ -31,5 +31,15 @@ describe('Dispatch', () => {
     await waitFor(element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE'))).toBeVisible().withTimeout(10000)
     await element(by.id('placeId:ChIJPSRadeBt5kcR4B2HzbBfZQE')).tap()
     await element(by.id('submitTaskForm')).tap()
+
+    await waitFor(element(by.id('task:0'))).toExist().withTimeout(5000)
+    await element(by.id('task:0')).swipe('right', 'fast', 0.5)
+
+    await element(by.id('task:0:assign')).tap()
+    await element(by.id('assignTo:bot_1')).tap()
+
+    await element(by.id('dispatch:assignedTab')).tap()
+    await element(by.id('dispatch:taskLists:bot_1')).tap()
+
   })
 })
