1671896b404cb99d16e390baa060517926f5b948
Alexandre Segura <mex.zktk@gmail.com>
2020-02-17 15:06:30 +0100

    Add ItemSeparator component.

---
+++ src/components/ItemSeparator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  itemSeparator: {
+    height: StyleSheet.hairlineWidth,
+    width: '100%',
+    backgroundColor: '#CCCCCC',
+  },
+})
+
+export default () => (
+  <View style={ styles.itemSeparator } />
+)
