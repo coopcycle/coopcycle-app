b5533b9094c570a19f8d5408ba7af7e9952f48a2
Vladimir <1525555+vova1vova@users.noreply.github.com>
2020-03-26 18:40:47 +0100

    android: launch app when order:created "data-only" messaged is received in the background

--- src/notifications/bgMessaging.js
+++ src/notifications/bgMessaging.js
@@ -2,14 +2,26 @@
 import firebase from 'react-native-firebase';
 // Optional flow type
 import type { RemoteMessage } from 'react-native-firebase';
-import LaunchActivity from './launchActivity';
+import launchActivity from './launchActivity';
+import store from '../redux/store'
+import {loadOrderAndPushNotification} from '../redux/Restaurant/actions'
+import {parseNotification} from './index.android'
 
-export default async (message: RemoteMessage) => {
-  //todo handle your data (message) received in background
+// data message was received in the background (works only on android)
+export default async (remoteMessage: RemoteMessage) => {
+  const message = parseNotification(remoteMessage, false)
 
-  console.log('data message (in background): ' + message.messageId)
+  const { event } = message.data
 
-  LaunchActivity.launch()
+  if (event && event.name === 'order:created') {
+    const {order} = event.data
 
+    launchActivity.invoke()
+
+    //todo we dont have much time here (10-20 seconds) may be just keep id somewhere
+    //and load order later?
+
+    store.dispatch(loadOrderAndPushNotification(order))
+  }
   return Promise.resolve();
 }
