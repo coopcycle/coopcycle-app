9e6973f23cfb0e8b1ebe66c20daca01025f4e9ce
Alexandre Segura <mex.zktk@gmail.com>
2020-05-12 13:01:00 +0200

    DRY.

--- src/components/OrderFulfillmentMethodIcon.js
+++ src/components/OrderFulfillmentMethodIcon.js
@@ -2,30 +2,17 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import { Icon } from 'native-base'
 
+import { resolveFulfillmentMethod } from '../utils/order'
+
 const styles = StyleSheet.create({
   small: {
     fontSize: 20,
   }
 })
 
-const resolve = order => {
-
-  if (Object.prototype.hasOwnProperty.call(order, 'fulfillmentMethod')) {
-
-    return order.fulfillmentMethod
-  }
-
-  if (Object.prototype.hasOwnProperty.call(order, 'takeaway')) {
-
-    return order.takeaway ? 'collection' : 'delivery'
-  }
-
-  return 'delivery'
-}
-
 export default ({ order, small }) => {
 
-  const fulfillmentMethod = resolve(order)
+  const fulfillmentMethod = resolveFulfillmentMethod(order)
 
   return (
     <Icon type="FontAwesome"
