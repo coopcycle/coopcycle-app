5a64b2a8dce2c750829f5584dab8f07169988de8
Vladimir <1525555+vova1vova@users.noreply.github.com>
2020-05-26 19:04:56 +0200

    add countly tracker

---
+++ src/analytics/CountlyTracker.js
@@ -0,0 +1,52 @@
+import BaseTracker from './BaseTracker'
+import Countly from 'countly-sdk-react-native-bridge';
+
+function CountlyTracker() {
+  this.userProperties = {}
+
+  // initialize
+  let serverURL = '';
+  let appKey = '';
+  let deviceId = null; // or use some string that identifies current app user
+  Countly.init(serverURL, appKey, deviceId);
+
+  // configure other Countly parameters if needed
+  Countly.enableParameterTamperingProtection('');
+  Countly.enableLogging();
+
+  // start session tracking
+  Countly.start();
+}
+CountlyTracker.prototype = Object.create(BaseTracker.prototype);
+CountlyTracker.prototype.constructor = CountlyTracker;
+
+CountlyTracker.prototype.setCurrentScreen = function(screenName) {
+  Countly.recordView(screenName);
+}
+
+CountlyTracker.prototype.logEvent = function(category, action, text, number) {
+  let eventName;
+
+  if (text != null) {
+    eventName = `${category}_${action}_${text}`;
+  } else {
+    eventName = `${category}_${action}`;
+  }
+
+  let event = {'eventName': eventName};
+
+  if (number != null) {
+    event.eventCount = number
+  }
+
+  event.segments = this.userProperties
+
+  Countly.sendEvent(event);
+}
+
+CountlyTracker.prototype.setUserProperty = function(name, value) {
+  Countly.userData.setProperty(name, value);
+  this.userProperties[name] = value
+}
+
+export default CountlyTracker
--- src/analytics/Tracker.js
+++ src/analytics/Tracker.js
@@ -1,5 +1,7 @@
 import FirebaseTracker from './FirebaseTracker'
+import CountlyTracker from './CountlyTracker'
+import MultipleTrackers from './MultipleTrackers'
 
-const tracker = new FirebaseTracker()
+const tracker = new MultipleTrackers(new FirebaseTracker(), new CountlyTracker())
 
 export default tracker
