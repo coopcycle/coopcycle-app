f6e59a4c348b69759c15be742d2806679602e58e
Alexandre Segura <mex.zktk@gmail.com>
2020-06-28 19:55:45 +0200

    Mock react-native-dotenv in Jest.

--- jestSetup.js
+++ jestSetup.js
@@ -47,6 +47,12 @@ jest.mock('countly-sdk-react-native-bridge', () => ({
   recordView: () => {},
 }));
 
+jest.mock('react-native-dotenv', () => ({
+  COUNTLY_SERVER_URL: '',
+  COUNTLY_APP_KEY: '',
+  COUNTLY_SALT: ''
+}));
+
 const fakeNavigator = {
   dispatch: (action) => {}
 }
