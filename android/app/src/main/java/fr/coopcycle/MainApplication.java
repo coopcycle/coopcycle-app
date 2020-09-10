eb475103ebcf4388f3a667492ef8d69f0146fd5d
Alexandre Segura <mex.zktk@gmail.com>
2020-09-10 09:01:07 +0200

    Fix class name.

--- android/app/src/main/java/fr/coopcycle/MainApplication.java
+++ android/app/src/main/java/fr/coopcycle/MainApplication.java
@@ -104,7 +104,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rndiffapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("fr.coopcycle.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
