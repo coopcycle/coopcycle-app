171a33a0cd25cc6b35943e8471dabb42480a66d0
Alexandre Segura <mex.zktk@gmail.com>
2020-09-12 16:43:57 +0200

    Make notifications work on iOS.

--- src/notifications/index.ios.js
+++ src/notifications/index.ios.js
@@ -22,17 +22,29 @@ class PushNotification {
       // WARNING
       // We need to call addEventListener BEFORE calling requestPermissions, or the whole thing does not work
       // @see https://github.com/facebook/react-native/issues/9105#issuecomment-246180895
-
       registerListener = deviceToken => options.onRegister(deviceToken)
       PushNotificationIOS.addEventListener('register', registerListener)
 
-      notificationListener = notification => {
-        // Don't use PushNotificationIOS.FetchResult.NoData
-        // @see https://github.com/invertase/react-native-firebase/issues/1870
-        notification.finish('backgroundFetchResultNoData')
-        options.onNotification(parseNotification(notification, AppState.currentState === 'active'))
-      }
-      PushNotificationIOS.addEventListener('notification', notificationListener)
+      // @see https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/pushing_background_updates_to_your_app
+      //
+      // The system treats background notifications as low priority:
+      // you can use them to refresh your app’s content, but the system doesn’t guarantee their delivery.
+      // In addition, the system may throttle the delivery of background notifications if the total number becomes excessive.
+      // The number of background notifications allowed by the system depends on current conditions,
+      // but don’t try to send more than two or three per hour.
+      //
+      // The server does *NOT* include a content-available key in the payload.
+      // Thus, this listener is never called
+      //
+      // PushNotificationIOS.addEventListener('notification', () => {})
+
+      // This listener is called when a notification is *TAPPED*,
+      // wether the app is in the foreground or the background
+      PushNotificationIOS.addEventListener('localNotification', notification => {
+        // We *MUST* call this or the rest doesn't work
+        notification.finish(PushNotificationIOS.FetchResult.NoData)
+        options.onNotification(parseNotification(notification, false))
+      })
 
       PushNotificationIOS
         .requestPermissions()
@@ -48,7 +60,6 @@ class PushNotification {
       PushNotificationIOS.getInitialNotification()
         .then(notification => {
           if (notification) {
-            notification.finish(PushNotificationIOS.FetchResult.NoData)
             resolve(parseNotification(notification, false))
           } else {
             resolve(null)
