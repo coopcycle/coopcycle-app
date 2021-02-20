9107f95d39874a6096f73efa04a9859136352d97
Alexandre Segura <mex.zktk@gmail.com>
2021-02-20 12:06:47 +0100

    Use Picker from NativeBase.

--- src/navigation/checkout/ShippingDate.js
+++ src/navigation/checkout/ShippingDate.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { TouchableOpacity, View, StyleSheet } from 'react-native'
-import { Picker } from '@react-native-picker/picker'
-import { Text } from 'native-base'
+import { Picker, Text } from 'native-base'
 import { withTranslation } from 'react-i18next'
 import { connect } from 'react-redux'
 import _ from 'lodash'
