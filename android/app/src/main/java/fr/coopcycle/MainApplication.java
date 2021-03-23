1c718c9c799e96bf2f8c3c33ed091c93e9582a43
Alexandre Segura <mex.zktk@gmail.com>
2021-03-23 19:50:47 +0100

    Remove (unused) code that may be causing #1002.

--- android/app/src/main/java/fr/coopcycle/MainApplication.java
+++ android/app/src/main/java/fr/coopcycle/MainApplication.java
@@ -45,8 +45,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          // packages.add(new LaunchActivityPackage());
-          // packages.add(new NotificationManagerPackage());
 
           // Add unimodules
           List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
