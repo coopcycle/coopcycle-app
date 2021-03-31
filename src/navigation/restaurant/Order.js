ddd10278da21dfd09cbef85488752f1431a4dc2d
Alexandre Segura <mex.zktk@gmail.com>
2021-03-31 12:40:03 +0200

    Refactor into components.

--- src/navigation/restaurant/Order.js
+++ src/navigation/restaurant/Order.js
@@ -1,105 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import {
   Container,
-  Footer,
   Left,
   Icon, Text, Button,
   Card, CardItem,
 } from 'native-base';
-import { Col, Row, Grid } from 'react-native-easy-grid'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
-import moment from 'moment'
 
 import OrderItems from '../../components/OrderItems'
 import SwipeToAcceptOrRefuse from './components/SwipeToAcceptOrRefuse'
 import OrderHeading from './components/OrderHeading'
+import OrderAcceptedFooter from './components/OrderAcceptedFooter'
 
 import { acceptOrder, printOrder, fulfillOrder } from '../../redux/Restaurant/actions'
-import material from '../../../native-base-theme/variables/material'
-import { resolveFulfillmentMethod } from '../../utils/order'
+
+const OrderNotes = ({ order }) => {
+
+  if (order.notes) {
+
+    return (
+      <View style={{ paddingHorizontal: 20 }}>
+        <Card>
+          <CardItem>
+            <Left>
+              <Icon type="FontAwesome" name="quote-left" />
+              <Text note>{ order.notes }</Text>
+            </Left>
+          </CardItem>
+        </Card>
+      </View>
+    )
+  }
+
+  return null
+}
 
 class OrderScreen extends Component {
-
-  renderActionButtons() {
-
-    const { order } = this.props
-    const { navigate } = this.props.navigation
-
-    if (order.state === 'new') {
-
-      return (
-        <SwipeToAcceptOrRefuse
-          onAccept={ () => this.props.acceptOrder(this.props.order, order => this.props.navigation.setParams({ order })) }
-          onRefuse={ () => this.props.navigation.navigate('RestaurantOrderRefuse', { order: this.props.order }) } />
-      )
-    }
-
-    if (order.state === 'accepted') {
-
-      const fulfillmentMethod = resolveFulfillmentMethod(order)
-
-      return (
-        <Footer style={{ backgroundColor: '#fbfbfb' }}>
-          <Grid>
-            <Row>
-              <Col style={{ padding: 10 }}>
-                <TouchableOpacity
-                  style={ [ styles.footerBtn, styles.refuseBtn ] }
-                  onPress={() => navigate('RestaurantOrderCancel', { order })}>
-                  <Text style={ styles.refuseBtnText }>
-                    { this.props.t('RESTAURANT_ORDER_BUTTON_CANCEL') }
-                  </Text>
-                </TouchableOpacity>
-              </Col>
-              <Col style={{ padding: 10 }}>
-                <TouchableOpacity
-                  style={ [ styles.footerBtn, styles.delayBtn ] }
-                  onPress={() => navigate('RestaurantOrderDelay', { order })}>
-                  <Text style={ styles.delayBtnText }>
-                    { this.props.t('RESTAURANT_ORDER_BUTTON_DELAY') }
-                  </Text>
-                </TouchableOpacity>
-              </Col>
-              { fulfillmentMethod === 'collection' && (
-              <Col style={{ padding: 10 }}>
-                <TouchableOpacity
-                  style={ [ styles.footerBtn, styles.fulfillBtn ] }
-                  onPress={() => this.fulfillOrder(order) }>
-                  <Text style={ styles.fulfillBtnText }>
-                    { this.props.t('RESTAURANT_ORDER_BUTTON_FULFILL') }
-                  </Text>
-                </TouchableOpacity>
-              </Col>
-              )}
-            </Row>
-          </Grid>
-        </Footer>
-      )
-    }
-  }
-
-  renderNotes() {
-
-    const { order } = this.props
-
-    if (order.notes) {
-
-      return (
-        <View style={{ paddingHorizontal: 20 }}>
-          <Card>
-            <CardItem>
-              <Left>
-                <Icon type="FontAwesome" name="quote-left" />
-                <Text note>{ order.notes }</Text>
-              </Left>
-            </CardItem>
-          </Card>
-        </View>
-      )
-    }
-  }
 
   fulfillOrder(order) {
     this.props.fulfillOrder(order, o => this.props.navigation.setParams({ order: o }))
@@ -118,43 +56,24 @@ class OrderScreen extends Component {
             onPrinterClick={ () => this.props.navigation.navigate('RestaurantPrinter') }
             printOrder={ () => this.props.printOrder(this.props.order) } />
           <OrderItems order={ order } />
-          { this.renderNotes() }
+          <OrderNotes order={ order } />
         </View>
-        { this.renderActionButtons() }
+        { order.state === 'new' &&
+          <SwipeToAcceptOrRefuse
+            onAccept={ () => this.props.acceptOrder(order, order => this.props.navigation.setParams({ order })) }
+            onRefuse={ () => this.props.navigation.navigate('RestaurantOrderRefuse', { order }) } />
+        }
+        { order.state === 'accepted' &&
+          <OrderAcceptedFooter
+            order={ order }
+            onPressCancel={  () => this.props.navigation.navigate('RestaurantOrderCancel', { order }) }
+            onPressDelay={   () => this.props.navigation.navigate('RestaurantOrderDelay', { order }) }
+            onPressFulfill={ () => this.fulfillOrder(order) } />
+        }
       </Container>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  footerBtn: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-  },
-  refuseBtn: {
-    borderColor: material.brandDanger,
-  },
-  refuseBtnText: {
-    color: material.brandDanger,
-    fontWeight: 'bold',
-  },
-  delayBtn: {
-    borderColor: '#333',
-  },
-  fulfillBtn: {
-    borderColor: material.brandSuccess,
-  },
-  delayBtnText: {
-    color: '#333',
-    fontWeight: 'bold',
-  },
-  fulfillBtnText: {
-    color: material.brandSuccess,
-    fontWeight: 'bold',
-  },
-});
 
 function mapStateToProps(state, ownProps) {
 
---
+++ src/navigation/restaurant/components/OrderAcceptedFooter.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  Footer,
+  Icon, Text, Button,
+} from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid'
+import { withTranslation } from 'react-i18next'
+
+import { resolveFulfillmentMethod } from '../../../utils/order'
+import material from '../../../../native-base-theme/variables/material'
+
+const styles = StyleSheet.create({
+  footerBtn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
+  refuseBtn: {
+    borderColor: material.brandDanger,
+  },
+  refuseBtnText: {
+    color: material.brandDanger,
+    fontWeight: 'bold',
+  },
+  delayBtn: {
+    borderColor: '#333',
+  },
+  fulfillBtn: {
+    borderColor: material.brandSuccess,
+  },
+  delayBtnText: {
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  fulfillBtnText: {
+    color: material.brandSuccess,
+    fontWeight: 'bold',
+  },
+});
+
+const OrderAcceptedFooter = ({ order, onPressCancel, onPressDelay, onPressFulfill }) => {
+
+  const { t } = useTranslation()
+
+  const fulfillmentMethod = resolveFulfillmentMethod(order)
+
+  return (
+    <Footer style={{ backgroundColor: '#fbfbfb' }}>
+      <Grid>
+        <Row>
+          <Col style={{ padding: 10 }}>
+            <TouchableOpacity
+              style={ [ styles.footerBtn, styles.refuseBtn ] }
+              onPress={ onPressCancel }>
+              <Text style={ styles.refuseBtnText }>
+                { t('RESTAURANT_ORDER_BUTTON_CANCEL') }
+              </Text>
+            </TouchableOpacity>
+          </Col>
+          <Col style={{ padding: 10 }}>
+            <TouchableOpacity
+              style={ [ styles.footerBtn, styles.delayBtn ] }
+              onPress={ onPressDelay }>
+              <Text style={ styles.delayBtnText }>
+                { t('RESTAURANT_ORDER_BUTTON_DELAY') }
+              </Text>
+            </TouchableOpacity>
+          </Col>
+          { fulfillmentMethod === 'collection' && (
+          <Col style={{ padding: 10 }}>
+            <TouchableOpacity
+              style={ [ styles.footerBtn, styles.fulfillBtn ] }
+              onPress={ onPressFulfill }>
+              <Text style={ styles.fulfillBtnText }>
+                { t('RESTAURANT_ORDER_BUTTON_FULFILL') }
+              </Text>
+            </TouchableOpacity>
+          </Col>
+          )}
+        </Row>
+      </Grid>
+    </Footer>
+  )
+}
+
+export default OrderAcceptedFooter
