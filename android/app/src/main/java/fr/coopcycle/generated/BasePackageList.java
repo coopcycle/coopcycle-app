dbbabb0417b5d5a2f0c906064aa1af622cd70c3a
Alexandre Segura <mex.zktk@gmail.com>
2021-08-03 12:38:52 +0200

    Bump Expo & Gradle plugin, target iOS 11.

--- android/app/src/main/java/fr/coopcycle/generated/BasePackageList.java
+++ android/app/src/main/java/fr/coopcycle/generated/BasePackageList.java
@@ -13,12 +13,7 @@ public class BasePackageList {
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
-        new expo.modules.keepawake.KeepAwakePackage(),
-        new expo.modules.lineargradient.LinearGradientPackage(),
-        new expo.modules.location.LocationPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.securestore.SecureStorePackage(),
-        new expo.modules.sqlite.SQLitePackage()
+        new expo.modules.keepawake.KeepAwakePackage()
     );
   }
 }
