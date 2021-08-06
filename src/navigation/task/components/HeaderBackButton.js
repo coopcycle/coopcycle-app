48f2de118265cd4779185a5558fc21e3b7e32790
Alexandre Segura <mex.zktk@gmail.com>
2021-08-06 16:13:21 +0200

    Add Redux-aware HeaderBackButton components.

---
+++ src/navigation/task/components/HeaderBackButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { HeaderBackButton } from '@react-navigation/stack'
+import { useNavigation } from '@react-navigation/native'
+
+const HeaderBackButtonWrapper = (props) => {
+
+  const { t } = useTranslation()
+  const navigation = useNavigation()
+
+  let { onPress, title, backImage, currentRoute, ...otherProps } = props
+
+  if (currentRoute === 'TaskCompleteHome') {
+    title = t('CANCEL')
+  }
+  if (currentRoute === 'TaskPhoto' || currentRoute === 'TaskSignature' || currentRoute === 'TaskHome') {
+    title = 'Back'
+  }
+
+  return (
+    <HeaderBackButton { ...otherProps }
+      onPress={ () => navigation.goBack(null) }
+      title={ title }
+      backImage={ backImage } />
+  )
+}
+
+function mapStateToProps (state) {
+
+  return {
+    currentRoute: state.app.currentRoute,
+  }
+}
+
+export default connect(mapStateToProps)(HeaderBackButtonWrapper)
