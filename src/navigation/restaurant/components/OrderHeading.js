f901b93271882c3ef5d1784acbd10226acd55a4a
Alexandre Segura <mex.zktk@gmail.com>
2021-10-11 23:53:44 +0200

    Use HStack.

--- src/navigation/restaurant/components/OrderHeading.js
+++ src/navigation/restaurant/components/OrderHeading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import moment from 'moment'
-import { Icon, Text } from 'native-base'
+import { Icon, Text, HStack } from 'native-base'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 import material from '../../../../native-base-theme/variables/material'
@@ -15,12 +15,6 @@ const fallbackFormat = 'dddd D MMM'
 const styles = StyleSheet.create({
   fulfillment: {
     backgroundColor: '#f9ca24',
-    paddingHorizontal: material.contentPadding,
-    paddingVertical: (material.contentPadding * 1.5),
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 5,
   },
   timeline: {
     flexDirection: 'row',
@@ -40,7 +34,7 @@ const OrderHeading = ({ order, isPrinterConnected, onPrinterClick, printOrder })
 
   return (
     <View style={{ borderBottomWidth: StyleSheet.hairlineWidth, borderBottomColor: '#CCCCCC' }}>
-      <View style={ styles.fulfillment }>
+      <HStack justifyContent="space-between" alignItems="center" style={ styles.fulfillment } p="2" mb="1">
         <OrderFulfillmentMethodIcon order={ order } />
         <Text style={{ fontWeight: '700' }}>{ moment(pickupExpectedAt).calendar(null, {
           lastDay : fallbackFormat,
@@ -51,7 +45,7 @@ const OrderHeading = ({ order, isPrinterConnected, onPrinterClick, printOrder })
           sameElse : fallbackFormat,
         }) }</Text>
         <Text>{ t(`FULFILLMENT_METHOD.${resolveFulfillmentMethod(order)}`) }</Text>
-      </View>
+      </HStack>
       <View style={ styles.timeline }>
         <Icon as={FontAwesome} name="clock-o" />
         <View style={{ alignItems: 'flex-end' }}>
