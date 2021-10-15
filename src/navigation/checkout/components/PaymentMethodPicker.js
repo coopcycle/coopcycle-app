b7a55fa6383b49dc5ff90b8747313a12aa5bdd79
Lucas Ferraro <lferraro@fiqus.com>
2021-10-15 16:47:51 -0300

    fix dark mode in payment method picker

--- src/navigation/checkout/components/PaymentMethodPicker.js
+++ src/navigation/checkout/components/PaymentMethodPicker.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { useTranslation } from 'react-i18next'
-import { Icon, Text } from 'native-base'
+import { Icon, Text, useColorMode } from 'native-base'
 import Foundation from 'react-native-vector-icons/Foundation'
 
 const styles = StyleSheet.create({
@@ -10,11 +10,11 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#f7f7f7',
     padding: 15,
     marginBottom: 10,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonIcon: {
     marginRight: 5,
@@ -29,6 +29,7 @@ const icons = {
 const PaymentMethodPicker = ({ methods, onSelect }) => {
 
   const { t } = useTranslation()
+  const { colorMode } = useColorMode()
 
   return (
     <View>
@@ -36,7 +37,7 @@ const PaymentMethodPicker = ({ methods, onSelect }) => {
       <View>
         { methods.map(method => (
           <TouchableOpacity key={ method.type }
-            style={ styles.button }
+            style={ [styles.button, {backgroundColor: colorMode === 'dark' ? '#3f3f3f' : '#f7f7f7'}] }
             onPress={ () => onSelect(method.type) }>
             <Icon as={Foundation} name={ icons[method.type] } style={ styles.buttonIcon } />
             <Text>{ t(`PAYMENT_METHOD.${method.type}`) }</Text>
