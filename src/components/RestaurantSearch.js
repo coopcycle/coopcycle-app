c7f6d890c1b204ace45a8cfd1322e1d9fa5823eb
Alexandre Segura <mex.zktk@gmail.com>
2022-07-12 14:43:52 +0200

    Remove dead code, format.

--- src/components/RestaurantSearch.js
+++ src/components/RestaurantSearch.js
@@ -40,76 +40,20 @@ function RestaurantSearch(props) {
 
   const navigation = useNavigation()
 
-  const renderButton = () => {
-
-    const iconName = props.defaultValue ? 'times' : 'search'
-    const iconSize = props.defaultValue ? 24 : 18
-
-    let touchableProps = {}
-    if (props.defaultValue) {
-      touchableProps = {
-        ...touchableProps,
-        onPress: props.onReset,
-      }
-    }
-
-    return (
-      <View style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 5, paddingLeft: 10, paddingRight: 15 }}>
-        <TouchableOpacity
-          { ...touchableProps }>
-          <Icon as={ FontAwesome5 } name={ iconName } style={{ color: '#ffffff', fontSize: 24 }} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={ () => AddressUtils.getAddressFromCurrentPosition().then(address => props.onSelect(address)) }>
-          <Icon as={ MaterialIcons } name="my-location" style={{ color: '#ffffff', fontSize: 24 }} />
-        </TouchableOpacity>
-      </View>
-    )
-  }
-
-
-    //this.props.onSelect
-
-    return (
-      <View style={ [ styles.container, { width: props.width }] }>
-          <TouchableNativeFeedback onPress={() => {
-            navigation.navigate('AccountAddresses', { action: 'search' })
-          }}>
-            <Center>
-
-            <Text fontSize={'md'} style={{
-          color: whiteColor,
-
-        }}>{props.defaultValue?.streetAddress} </Text>
-          <ChevronDownIcon color={whiteColor}/>
-            </Center>
-
-          </TouchableNativeFeedback>
-      </View>
-    )
+  return (
+    <View style={ [ styles.container, { width: props.width } ] }>
+      <TouchableNativeFeedback onPress={() => {
+        navigation.navigate('AccountAddresses', { action: 'search' })
+      }}>
+        <Center>
+          <Text fontSize={'md'} style={{
+            color: whiteColor,
+          }}>{ props.defaultValue?.streetAddress }</Text>
+          <ChevronDownIcon color={ whiteColor }/>
+        </Center>
+      </TouchableNativeFeedback>
+    </View>
+  )
 }
-/*
-<AddressAutocomplete
-          location={ this.props.location }
-          country={ this.props.country }
-          onSelectAddress={ this.props.onSelect }
-          containerStyle={{
-            flex: 1,
-            justifyContent: 'center',
-          }}
-          inputContainerStyle={{
-            flex: 1,
-            justifyContent: 'center',
-            borderWidth: 0,
-            paddingLeft: 15,
-            height: textInputContainerHeight,
-          }}
-          style={{
-            height: (textInputContainerHeight * 0.7),
-          }}
-          onChangeText={ this.props.onChangeText }
-          value={ this.props.defaultValue }
-          renderRight={ this.renderButton.bind(this) }
-          addresses={ this.props.savedAddresses } />
- */
+
 export default withTranslation()(RestaurantSearch)
