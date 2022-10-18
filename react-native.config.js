834678c49ab2aa400115fbc2c668b402bc2ef61e
Alexandre Segura <mex.zktk@gmail.com>
2022-10-19 00:07:07 +0200

    Disable react-native-background-geolocation auto-linking by reading env.

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,27 @@
-module.exports = {
-  assets: ['./assets/fonts/'],
-  dependencies: {
-    '@react-native-firebase/messaging': {
-      platforms: {
-        ios: null,
-      },
+require('dotenv').config()
+
+let dependencies = {
+  // We disable @react-native-firebase/messaging auto-linking on iOS
+  '@react-native-firebase/messaging': {
+    platforms: {
+      ios: null,
     },
   },
+};
+
+if (process.env.DEFAULT_SERVER !== '') {
+  // We disable react-native-background-geolocation auto-linking for custom apps
+  dependencies = Object.assign(dependencies, {
+    'react-native-background-geolocation': {
+      platforms: {
+        ios: null,
+        android: null,
+      },
+    },
+  })
+}
+
+module.exports = {
+  assets: ['./assets/fonts/'],
+  dependencies: dependencies
 };
