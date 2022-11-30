2eb3659c1e9f230801910d21f38d35e014d19d63
Alexandre Segura <mex.zktk@gmail.com>
2022-11-30 21:03:15 +0100

    Bump Jest, fix tests.

--- __tests__/Settings.js
+++ __tests__/Settings.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Settings from '../src/Settings'
 import API from '../src/API'
 
