3070b332022522decc9aeb2276c1eff3fa4c1709
Alexandre Segura <mex.zktk@gmail.com>
2022-12-01 14:19:38 +0100

    Remove Enzyme as it's deprecated and blocks Jest.
    - See https://dev.to/wojtekmaj/enzyme-is-dead-now-what-ekl

--- jestSetup.js
+++ jestSetup.js
@@ -1,11 +1,6 @@
 /* global jest */
 
-import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import NavigationHolder from './src/NavigationHolder'
-
-// React 16 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() })
 
 jest.mock('react-native-localize', () => ({
   findBestAvailableLanguage: () => ({ languageTag: 'en' }),
