2fa29f78ff057f932b11537ade1aeafc01678ea5
Alexandre Segura <mex.zktk@gmail.com>
2023-09-07 17:03:49 +0200

    Allow selecting number of each return.

--- src/navigation/checkout/Loopeat.js
+++ src/navigation/checkout/Loopeat.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { Linking, FlatList } from 'react-native'
-import { Box, Text, Button, VStack, Pressable, HStack, Heading } from 'native-base'
+import { Box, Text, Button, VStack, Pressable, HStack, Heading, Switch, Input } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useTranslation } from 'react-i18next'
+import NumericInput from 'react-native-numeric-input'
 
 import { selectCart } from '../../redux/Checkout/selectors'
 import { updateLoopeatReturns } from '../../redux/Checkout/actions'
@@ -53,21 +54,44 @@ function Loopeat({ cart, updateLoopeatReturns }) {
 			<VStack flex={ 1 } justifyContent="space-between">
 				{ containersCount > 0 && (
 					<Box p="3" mb="3" bg="light.200">
-						<Heading size="sm">{ t('CHECKOUT_LOOPEAT_RETURN_CONTAINERS') }</Heading>
+						<Heading size="sm" mb="4">{ t('CHECKOUT_LOOPEAT_RETURN_CONTAINERS') }</Heading>
 						<FlatList
 		          data={ containers }
 		          keyExtractor={ (item, index) => `loopeat_return_#${item.format_id}` }
-		          renderItem={ ({ item }) => (
-		          	<HStack justifyContent="space-between" alignItems="center" mb="2">
-		          		<Text>{ `${item.quantity} × ${getNameFromId(item.format_id, formats)} (${formatPrice(getPriceFromId(item.format_id, formats))})` }</Text>
-		          		<Button variant="outline" size="sm" onPress={ () => {
-		          			const newReturns = _.find(returns, r => r.format_id === item.format_id) ?
-	                    _.filter(returns, r => r.format_id !== item.format_id) : [ ...returns, item ]
+		          renderItem={ ({ item }) => {
 
-	                  setReturns(newReturns)
-		          		} }>{ _.find(returns, r => r.format_id === item.format_id) ? t('CANCEL') : t('CHECKOUT_LOOPEAT_RETURN_CONTAINER') }</Button>
-		          	</HStack>
-		          ) } />
+		          	const isSelected = !!_.find(returns, r => r.format_id === item.format_id)
+
+		          	return (
+			          	<HStack justifyContent="space-between" alignItems="center" mb="2">
+			          		<HStack alignItems="center">
+			          			<Switch
+                        size="sm"
+			          				isChecked={ isSelected }
+			          				onToggle={ checked => {
+				          				const newReturns = checked ?
+				                    [ ...returns, item ] : _.filter(returns, r => r.format_id !== item.format_id)
+				                  setReturns(newReturns)
+				          			} } />
+			          			<Text ml="2">{ `${getNameFromId(item.format_id, formats)} (${formatPrice(getPriceFromId(item.format_id, formats))})` }</Text>
+			          		</HStack>
+                      <NumericInput
+                        totalWidth={ 70 }
+                        minValue={ 1 }
+                        maxValue={ item.quantity }
+                        editable={ isSelected }
+                        onChange={ value => {
+                          const idx = _.findIndex(returns, r => r.format_id === item.format_id)
+                          if (idx !== -1) {
+                            const newReturns = returns.map(function(ret, retIndex) {
+                              return retIndex === idx ? { ...ret, quantity: value } : ret;
+                            });
+                            setReturns(newReturns)
+                          }
+                        }} />
+                    </HStack>
+                )
+		        	}} />
 		        </Box>
 				) }
 				{ missingAmount > 0 &&
