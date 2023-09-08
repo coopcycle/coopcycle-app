8359c1f277fed1d4de5cc9b25ec1597f37608360
Antoine Bagnaud <contact@antoinebagnaud.me>
2023-09-08 13:41:46 +0200

    fix: platform specific calendar height

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,6 +1,7 @@
 // Source file for common CSS
 
 import Config from 'react-native-config'
+import { Platform } from 'react-native'
 
 let primaryColor = Config.PRIMARY_COLOR || '#e4022d',
     whiteColor = '#fff',
@@ -13,7 +14,10 @@ let primaryColor = Config.PRIMARY_COLOR || '#e4022d',
     orangeColor = '#d35400',
     fontTitleName = 'Raleway-Regular',
     dateSelectHeaderHeight = 40,
-    calendarHeight = 370, // an actual height is slightly different per platform; android is ~367, ios is ~357
+    calendarHeight = Platform.select({
+      ios: 357,
+      android: 367,
+    }), // an actual height is slightly different per platform; android is ~367, ios is ~357
     headerFontSize = 18
 
 export {
