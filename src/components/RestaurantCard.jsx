779b1ab08b403308b5d659bf006ca0537b2b6335
Pierre LHOSTE <vingt-douze@protonmail.com>
2023-12-19 19:32:34 +0100

    Init restaurant card new design

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,149 @@
+import {Image, StyleSheet, View} from 'react-native';
+import {Text, useColorModeValue} from 'native-base';
+import {TimingBadge} from '../navigation/checkout/components/RestaurantBadges';
+import {RestaurantBadge} from './RestaurantBadge';
+import {RestaurantTag} from './RestaurantTag';
+import React from 'react';
+
+const logoSize = 64;
+
+const OneLineText = props => (
+  <Text
+    numberOfLines={props.numberOfLines || 1}
+    ellipsizeMode="tail"
+    {...props}>
+    {props.children}
+  </Text>
+);
+
+export const RestaurantCard = ({restaurant}) => {
+  const backgroundColor = useColorModeValue('white', '#121212');
+  //   const backgroundColor = useColorModeValue('white', '#FFFFFF16');
+
+  const styles = StyleSheet.create({
+    item: {
+      marginHorizontal: 16,
+      marginTop: 16,
+      borderRadius: 8,
+      overflow: 'hidden',
+      marginBottom: 0,
+      backgroundColor: backgroundColor,
+    },
+    images: {
+      width: '100%',
+    },
+    banner: {
+      aspectRatio: '16/9',
+      width: '100%',
+      marginBottom: -50,
+    },
+    logoWrapper: {
+      // backgroundColor: '#fff',
+      backgroundColor: backgroundColor,
+      borderRadius: 16,
+      display: 'inline-flex',
+      marginLeft: 16,
+      padding: 8,
+      position: 'relative',
+      boxSizing: 'border-box',
+      width: logoSize + 16,
+    },
+    logo: {
+      width: logoSize,
+      aspectRatio: '1',
+      borderRadius: 8,
+    },
+    content: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      paddingTop: 8,
+      paddingRight: 24,
+      paddingBottom: 24,
+      paddingLeft: 24,
+      gap: 8,
+    },
+    name: {
+      fontWeight: '600',
+      fontSize: 18,
+    },
+    timing: {
+      alignItems: 'center',
+      alignSelf: 'left',
+      borderRadius: 4,
+    },
+    badgesWrapper: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      gap: 8,
+      left: 0,
+      padding: 12,
+      position: 'absolute',
+      top: 0,
+      width: '100%',
+    },
+    details: {
+      overflow: "hidden",
+      whiteSpace: 'nowrap',
+      display: 'flex',
+      flexDirection: "row",
+      gap: 4
+    },
+    detailsText: {
+      fontSize: 14,
+      opacity: 0.75,
+    },
+  });
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.images}>
+        <View styles={styles.bannerWrapper}>
+          <Image
+            style={styles.banner}
+            resizeMode="cover"
+            source={{uri: restaurant.bannerImage}}
+          />
+        </View>
+        <View style={styles.logoWrapper}>
+          <Image
+            style={styles.logo}
+            resizeMode="cover"
+            source={{uri: restaurant.image}}
+          />
+        </View>
+        <View style={styles.badgesWrapper}>
+          {restaurant.badges.map((badge, i) => (
+            <RestaurantBadge type={badge} key={i} />
+          ))}
+        </View>
+      </View>
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.name}>{restaurant.name}</Text>
+        </View>
+        <View>
+          <TimingBadge restaurant={restaurant} />
+        </View>
+        <View style={styles.details}>
+          {restaurant.tags.length > 0 ? (
+            restaurant.tags.map((tag, i) => (
+              <RestaurantTag key={i} text={tag} />
+            ))
+          ) : restaurant.description ? (
+            <OneLineText style={styles.detailsText}>
+              {restaurant.description}
+            </OneLineText>
+          ) : restaurant.address ? (
+            <OneLineText style={styles.detailsText}>
+              {restaurant.address.streetAddress}
+            </OneLineText>
+          ) : (
+            <Text style={styles.detailsText}>No description</Text>
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
--- src/navigation/checkout/components/RestaurantBadges.js
+++ src/navigation/checkout/components/RestaurantBadges.js
@@ -1,41 +1,73 @@
-import { StyleSheet } from 'react-native'
-import { Badge, HStack, Icon, Text, useColorModeValue } from 'native-base'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import { getNextShippingTimeAsText } from '../../../utils/checkout'
-import React from 'react'
+import {StyleSheet} from 'react-native';
+import {Badge, HStack, Icon, Text, useColorModeValue} from 'native-base';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {
+  getNextShippingTimeAsText,
+  shouldShowPreOrder,
+} from '../../../utils/checkout';
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
 
 const styles = StyleSheet.create({
   badge: {
     alignItems: 'center',
-    position: 'absolute',
-    height: 30,
-    alignSelf: 'center',
-    bottom: -15,
-    borderRadius: 4,
   },
   badgeText: {
-    fontSize: 12,
+    fontSize: 14,
+    fontWeight: 600,
+    marginLeft: 4,
   },
-})
+  badgeTextPreOrder: {
+    fontSize: 14,
+    fontWeight: 400,
+    marginLeft: 4,
+  },
+});
 
-export const CategoryBadge = ({ label }) => {
+export const CategoryBadge = ({label}) => {
   // styled via theme
   return (
     <Badge variant="subtle" mr="1">
       {label}
     </Badge>
-  )
-}
+  );
+};
 
-export const TimingBadge = ({ restaurant }) => {
-  const bg = useColorModeValue('gray.200', 'gray.800')
+export const TimingBadge = ({restaurant}) => {
+  const color = useColorModeValue('#000', '#fff');
+
+  const shippingTime = getNextShippingTimeAsText(restaurant);
+  const showPreOrder = shouldShowPreOrder(restaurant);
 
   return (
-    <HStack style={[styles.badge]} bg={bg} px="2">
-      <Icon as={FontAwesome} name="clock-o" size="xs" mr="1" />
-      <Text style={styles.badgeText}>
-        {getNextShippingTimeAsText(restaurant)}
-      </Text>
+    <HStack style={[styles.badge]}>
+      {showPreOrder ? (
+        <Svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={17}
+          height={17}
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke={color}
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <Path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+          <Path d="M12 7v5l3 3" />
+        </Svg>
+      ) : (
+        <Svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={19}
+          height={17}
+          fill={color}
+          viewBox="0 0 18 11.38">
+          <Path d="M14.52 4.41c-.29 0-.58.05-.85.12l-.72-2.23s-.02-.06-.04-.09v-.03l.8-.14v.47c0 .1.08.17.17.17h1c.1 0 .17-.08.17-.17V1.22c0-.2-.09-.39-.24-.53a.684.684 0 00-.55-.16l-2.49.43a.71.71 0 00-.49.37c-.07.15-.09.32-.05.48H6.52l-.29-.74h.86c.33 0 .59-.24.59-.54s-.27-.54-.59-.54h-2.1c-.33 0-.59.24-.59.54 0 .23.15.42.37.5l.56 1.44-.79 1.63a3.5 3.5 0 00-.97-.14c-1.98 0-3.58 1.66-3.58 3.7s1.61 3.7 3.58 3.7c1.66 0 3.07-1.19 3.47-2.79l.04.13c.07.25.27.44.52.49.25.05.5-.05.66-.25l3.8-5 .37 1.16c-.85.64-1.4 1.64-1.4 2.78 0 1.92 1.56 3.48 3.48 3.48s3.48-1.56 3.48-3.48-1.56-3.48-3.48-3.48zm2.17 3.48c0 1.19-.97 2.17-2.17 2.17s-2.17-.97-2.17-2.17.97-2.17 2.17-2.17 2.17.97 2.17 2.17zM5.83 7.67c0 1.28-1 2.32-2.24 2.32S1.35 8.95 1.35 7.67s1-2.32 2.24-2.32 2.24 1.04 2.24 2.32zM6.08 5c-.1-.1-.2-.19-.31-.27l.15-.31.16.58zm4.87-1.8L8.04 7.03 6.98 3.2h3.96z" />
+        </Svg>
+      )}
+
+      <Text style={showPreOrder ? styles.badgeTextPreOrder : styles.badgeText}>{shippingTime}</Text>
     </HStack>
-  )
-}
+  );
+};
