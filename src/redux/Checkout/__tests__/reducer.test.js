6b615b927c524b21a6908fa9ad496bef35d51e8f
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2023-12-26 15:35:45 -0800

    fixed; remove loading indicator when a cart failed to init

---
+++ src/redux/Checkout/__tests__/reducer.test.js
@@ -0,0 +1,58 @@
+import reducer from '../reducers'
+import { initCartFailure, updateCartSuccess } from '../actions'
+
+// As we may be using setTimeout(), we need to mock timers
+// @see https://jestjs.io/docs/en/timer-mocks.html
+jest.useFakeTimers()
+
+describe('Redux | Checkout | Reducers', () => {
+  const initialState = reducer()
+
+  describe('reducers', () => {
+    test(`initCartFailure`, () => {
+      const action = initCartFailure({
+        restaurant: '/api/restaurants/10',
+        error: new Error('Something went wrong'),
+      })
+
+      const prevState = {
+        ...initialState,
+        loadingCarts: [ '/api/restaurants/10', '/api/restaurants/11' ],
+      }
+
+      const newState = reducer(prevState, action)
+
+      expect(newState).toEqual({
+        ...initialState,
+        loadingCarts: ['/api/restaurants/11'],
+      })
+    })
+
+    test(`updateCartSuccess`, () => {
+      const action = updateCartSuccess({
+        restaurant: '/api/restaurants/10',
+        // ...
+      })
+
+      const prevState = {
+        ...initialState,
+        loadingCarts: [ '/api/restaurants/10', '/api/restaurants/11' ],
+      }
+
+      const newState = reducer(prevState, action)
+
+      expect(newState).toEqual({
+        ...initialState,
+        loadingCarts: ['/api/restaurants/11'],
+        carts: {
+          '/api/restaurants/10': {
+            cart: {
+              restaurant: '/api/restaurants/10',
+              // ...
+            },
+          },
+        },
+      })
+    })
+  })
+})
