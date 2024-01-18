9e5b64cbc099e5d2aa2f95992cfb509007491d63
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-18 12:48:25 -0800

    added reactotron support (dev tool)

---
+++ ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from "reactotron-react-native";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { reactotronRedux } from 'reactotron-redux'
+
+const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+.configure() // controls connection & communication settings
+  .useReactNative() // add all built-in react native plugins
+  .use(reactotronRedux())
+  .connect(); // let's connect!
+
+export default reactotron;
