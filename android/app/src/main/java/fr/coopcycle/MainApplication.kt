0f7887f29e6484a9bdeeca2c001f1468e5731218
Alexandre Segura <mex.zktk@gmail.com>
2024-02-23 08:41:26 +0100

    Bump React Native, Expo & esc-pos-encoder.

---
+++ android/app/src/main/java/fr/coopcycle/MainApplication.kt
@@ -0,0 +1,83 @@
+package fr.coopcycle
+
+import android.content.res.Configuration
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
+
+import android.content.Intent
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.security.ProviderInstaller
+import com.google.android.gms.security.ProviderInstaller.ProviderInstallListener
+
+import java.util.List
+import java.util.Arrays
+
+import androidx.multidex.MultiDexApplication
+
+// To enable multidex on API Level < 21,
+// we need to extend android.support.multidex.MultiDexApplication instead of android.app.Application
+// https://developer.android.com/studio/build/multidex.html
+class MainApplication : MultiDexApplication(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+      this,
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return PackageList(this).packages
+        }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+  )
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    upgradeSecurityProvider()
+    SoLoader.init(this, false)
+    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
+      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
+    }
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
+    }
+    if (BuildConfig.DEBUG) {
+      ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
+
+  private fun upgradeSecurityProvider() {
+    ProviderInstaller.installIfNeededAsync(this, new ProviderInstallListener() {
+      public fun onProviderInstalled() {}
+
+      public fun onProviderInstallFailed(errorCode: int, recoveryIntent: Intent) {
+        GoogleApiAvailability.getInstance().showErrorNotification(MainApplication.this, errorCode);
+      }
+    });
+  }
+}
