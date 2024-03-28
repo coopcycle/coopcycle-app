2698ccc0320e374ab2c28ffe083e5c8eaf1e66da
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-03-28 09:33:40 -0700

    updated: code styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['eslint:recommended', '@react-native', 'prettier'],
   rules: {
-    semi: 0,
-    'space-unary-ops': 0,
+    // Allow inline styles (should be removed at some point)
+    'react-native/no-inline-styles': 'off',
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: true,
+      },
+    ],
+
     // Disable Prettier, because it conflicts with ESLint's "object-curly-spacing"
     // https://github.com/prettier/prettier/issues/1871
     // https://itnext.io/how-to-replace-prettier-by-eslint-rules-21574359e041
     'prettier/prettier': 'off',
-    // Allow inline styles (should be removed at some point)
-    'react-native/no-inline-styles': 'off',
-    'sort-imports': [ 'warn', {
-      'ignoreCase': false,
-      'ignoreDeclarationSort': true,
-      'ignoreMemberSort': false,
-      'allowSeparatedGroups': true,
-    }],
-    'object-curly-spacing': [ 'warn', 'always' ],
-    'array-bracket-spacing': [ 'warn', 'always', {
-      'singleValue': false,
-      'objectsInArrays': false,
-      'arraysInArrays': false,
-    }],
+
+    // Stylistic rules are deprecated in eslint v8.54.0
+    // and should be added via prettier or https://eslint.style/
   },
 };
