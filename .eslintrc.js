8c82b2c4593148fb6c2b087c1e12b71efc916ab7
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-04-01 11:40:23 -0700

    (temporary) treat recommended eslint settings as warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,18 @@ module.exports = {
   root: true,
   extends: ['eslint:recommended', '@react-native', 'prettier'],
   rules: {
-    // Allow inline styles (should be removed at some point)
+    //FIXME: fix warnings reported by this rule and increase the severity to 'error'
+    "no-irregular-whitespace": "warn",
+    //FIXME: fix warnings reported by this rule and increase the severity to 'error'
+    "no-case-declarations": "warn",
+    //FIXME: fix warnings reported by this rule and increase the severity to 'error'
+    "no-async-promise-executor": "warn",
+
+    "no-prototype-builtins": "warn",
+
+    // Allow inline styles
     'react-native/no-inline-styles': 'off',
+
     'sort-imports': [
       'warn',
       {
