6b5f8975ee9205cc01725ae7e71d29a0e25dbdfe
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-07-30 13:22:51 +0200

    Add packages render & improve styles

--- src/navigation/store/ModalFormWrapper.js
+++ src/navigation/store/ModalFormWrapper.js
@@ -1,20 +1,26 @@
+import { Box, Button, VStack } from 'native-base';
 import { SafeAreaView } from 'react-native';
 import { useBackgroundContainerColor } from '../../styles/theme';
-import { Box, Button, VStack } from 'native-base';
 
 export default function ModalFormWrapper({ children, handleSubmit, t }) {
   const backgroundColor = useBackgroundContainerColor();
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor,
+      }}>
       <VStack
         flex={1}
         justifyContent="space-between"
         style={{
           backgroundColor,
         }}>
-        <Box p="3">{children}</Box>
-        <Box p="3">
+        <Box p="5" gap="3">
+          {children}
+        </Box>
+        <Box p="5">
           <Button onPress={handleSubmit}>{t('SUBMIT')}</Button>
         </Box>
       </VStack>
