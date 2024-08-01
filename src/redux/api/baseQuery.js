3203b132cf25269b1b95f6b9c51146f740fa1129
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-08-01 15:43:37 -0700

    added: token refresh flow to rtk api client

--- src/redux/api/baseQuery.js
+++ src/redux/api/baseQuery.js
@@ -1,70 +1,129 @@
-import VersionNumber from 'react-native-version-number'
-import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
-import { selectBaseURL, selectUser } from '../App/selectors'
+import VersionNumber from 'react-native-version-number';
+import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { selectBaseURL, selectUser } from '../App/selectors';
+import { Mutex } from 'async-mutex';
+import qs from 'qs';
+import AppUser from '../../AppUser';
+import { logout } from '../App/actions';
+import { setUser } from '../middlewares/HttpMiddleware'
 
 const guestCheckoutEndpoints = [
   'getOrderValidate',
   'getOrderTiming',
   'updateOrder',
-]
+];
 
-const appVersion = VersionNumber.bundleIdentifier +
+const appVersion =
+  VersionNumber.bundleIdentifier +
   '@' +
   VersionNumber.appVersion +
   ' (' +
   VersionNumber.buildVersion +
-  ')'
+  ')';
 
-const buildBaseQuery = (baseUrl) => {
+// create a new mutex
+const mutex = new Mutex();
+
+const buildBaseQuery = (baseUrl, anonymous = false) => {
   return fetchBaseQuery({
     baseUrl,
     prepareHeaders: (headers, { getState, endpoint }) => {
-      headers.set('X-Application-Version', appVersion)
+      headers.set('X-Application-Version', appVersion);
 
-      const user = selectUser(getState())
-      if (user) {
-        headers.set('Authorization', `Bearer ${user.token}`)
-      } else if (guestCheckoutEndpoints.includes(endpoint)) {
-        //todo
-        // const orderAccessToken = selectOrderAccessToken(getState())
-        //
-        // if (orderAccessToken) {
-        //   headers.set('Authorization', `Bearer ${orderAccessToken}`)
-        // }
+      if (!anonymous) {
+        const user = selectUser(getState());
+        if (user) {
+          headers.set('Authorization', `Bearer ${user.token}`);
+        } else if (guestCheckoutEndpoints.includes(endpoint)) {
+          //todo
+          // const orderAccessToken = selectOrderAccessToken(getState())
+          //
+          // if (orderAccessToken) {
+          //   headers.set('Authorization', `Bearer ${orderAccessToken}`)
+          // }
+        }
       }
 
-      return headers
+      return headers;
     },
     jsonContentType: 'application/ld+json',
     timeout: 30000,
-  })
-}
+  });
+};
 
 //based on https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#automatic-re-authorization-by-extending-fetchbasequery
 export const baseQueryWithReauth = async (args, api, extraOptions) => {
-  const { getState } = api
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock();
 
-  const baseUrl = selectBaseURL(getState()) + '/'
-  const baseQuery = buildBaseQuery(baseUrl)
+  const { getState } = api;
 
-  let result= await baseQuery(args, api, extraOptions)
+  const baseUrl = selectBaseURL(getState()) + '/';
+  const baseQuery = buildBaseQuery(baseUrl);
+
+  let result = await baseQuery(args, api, extraOptions);
 
   if (result.error && result.error.status === 401) {
-    //todo
-    // try to get a new token; works only for logged in users
-    // const refreshResponse = await baseQuery(
-    //   window.Routing.generate('profile_jwt'),
-    //   api,
-    //   extraOptions,
-    // )
-    //
-    // if (refreshResponse.data && refreshResponse.data.jwt) {
-    //   api.dispatch(setAccessToken(refreshResponse.data.jwt))
-    //   // retry the initial query
-    //   result = await baseQuery(args, api, extraOptions)
-    // } else {
-    //   // api.dispatch(loggedOut())
-    // }
+    const user = selectUser(getState());
+
+    if (!user) {
+      return result;
+    }
+
+    const refreshToken = user.refreshToken;
+
+    // checking whether the mutex is locked
+    if (mutex.isLocked()) {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      const release = await mutex.acquire();
+      try {
+        // try to get a new token
+        const refreshResult = await buildBaseQuery(baseUrl, true)(
+          {
+            url: '/api/token/refresh',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: qs.stringify({
+              refresh_token: refreshToken,
+            }),
+          },
+          api,
+          extraOptions,
+        );
+        if (refreshResult.data) {
+          const { token, refresh_token } = refreshResult.data;
+
+          const { username, email, roles, enabled } = user;
+
+          const updUser = new AppUser(
+            username,
+            email,
+            token,
+            roles,
+            refresh_token,
+            enabled,
+          );
+
+          // store the new token
+          api.dispatch(setUser(updUser));
+          await user.save();
+          console.log('Credentials saved!')
+
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions);
+        } else {
+          api.dispatch(logout());
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release();
+      }
+    }
   }
-  return result
-}
+  return result;
+};
