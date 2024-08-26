1aee2736dd98dadecdf91ca39ba764548edd56f5
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-08-26 16:50:06 -0700

    refactor: timing range grouping

--- src/navigation/checkout/components/TimingCartSelect.js
+++ src/navigation/checkout/components/TimingCartSelect.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { groupBy, map, reduce } from 'lodash';
 import moment from 'moment/moment';
 import { HStack, Skeleton, View } from 'native-base';
@@ -17,16 +17,16 @@ export default function TimingCartSelect({ orderNodeId, onValueChange }) {
     setSelectedDay(0);
   }, []);
 
-  const values = useMemo(() => {
+  const rangesByDay = useMemo(() => {
     if (!isSuccess) {
       return null;
     }
     return reduce(
       groupBy(data.ranges, d => moment(d[0]).startOf('day').format()),
-      (acc, day, index) => {
+      (acc, ranges, key) => {
         acc.push({
-          day: moment(index),
-          values: map(day, range => ({
+          day: moment(key),
+          values: map(ranges, range => ({
             value: range,
             label: `${moment(range[0]).format('LT')} - ${moment(
               range[1],
@@ -39,42 +39,50 @@ export default function TimingCartSelect({ orderNodeId, onValueChange }) {
     );
   }, [data, isSuccess]);
 
+  const getRangesForDay = useCallback(
+    dayIndex => {
+      if (!isSuccess) {
+        return null;
+      }
+
+      if (rangesByDay.length <= dayIndex) {
+        return null;
+      }
+
+      return rangesByDay[dayIndex].values;
+    },
+    [isSuccess, rangesByDay],
+  );
+
   useEffect(() => {
     if (!isSuccess) {
       return;
     }
-    if (values.length < selectedDay) {
-      return;
-    }
-
-    onValueChange(values[selectedDay].values[selectedRange].value);
-  }, [selectedDay, selectedRange, values, isSuccess, onValueChange]);
+    onValueChange(getRangesForDay(selectedDay)[selectedRange].value);
+  }, [selectedDay, selectedRange, getRangesForDay, isSuccess, onValueChange]);
 
   const dayItems = useMemo(() => {
     return (
       isSuccess &&
-      values.map((day, index) => (
+      rangesByDay.map((rangesOnDay, index) => (
         <Picker.Item
           value={index}
-          label={day.day.format('ddd DD MMMM')}
+          label={rangesOnDay.day.format('ddd DD MMMM')}
           key={index}
         />
       ))
     );
-  }, [isSuccess, values]);
+  }, [isSuccess, rangesByDay]);
 
   const slotItems = useMemo(() => {
     if (!isSuccess) {
       return null;
     }
-    if (values.length < selectedDay) {
-      return null;
-    }
 
-    return values[selectedDay].values.map((range, index) => (
+    return getRangesForDay(selectedDay).map((range, index) => (
       <Picker.Item value={index} label={range.label} key={index} />
     ));
-  }, [isSuccess, selectedDay, values]);
+  }, [isSuccess, selectedDay, getRangesForDay]);
 
   return (
     <HStack
