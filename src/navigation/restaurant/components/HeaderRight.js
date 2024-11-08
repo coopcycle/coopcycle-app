62326f158876e47c9f0a5483c95e6319454f9a47
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-11-08 15:17:14 -0800

    move `close restaurant for today` button into the settings menu

--- src/navigation/restaurant/components/HeaderRight.js
+++ src/navigation/restaurant/components/HeaderRight.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
-import { Alert } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {
   HeaderButton,
@@ -9,44 +8,24 @@ import {
 } from 'react-navigation-header-buttons';
 import { connect } from 'react-redux';
 
-import { closeRestaurant } from '../../../redux/Restaurant/actions';
-import { selectSpecialOpeningHoursSpecificationForToday } from '../../../redux/Restaurant/selectors';
-
 const FontAwesomeHeaderButton = props => (
   <HeaderButton {...props} IconComponent={FontAwesome} iconSize={23} />
 );
 
 class HeaderRight extends Component {
-  onPressClose() {
-    Alert.alert(
-      this.props.t('RESTAURANT_CLOSE_ALERT_TITLE'),
-      this.props.t('RESTAURANT_CLOSE_ALERT_MESSAGE'),
-      [
-        {
-          text: this.props.t('RESTAURANT_CLOSE_ALERT_CONFIRM'),
-          onPress: () => this.props.closeRestaurant(this.props.restaurant),
-        },
-        {
-          text: this.props.t('CANCEL'),
-          style: 'cancel',
-        },
-      ],
-    );
-  }
 
   render() {
     const { navigate } = this.props.navigation;
-    const { specialOpeningHoursSpecification } = this.props;
 
     return (
       <HeaderButtons HeaderButtonComponent={FontAwesomeHeaderButton}>
-        {!specialOpeningHoursSpecification && (
-          <Item
-            title="close"
-            iconName="power-off"
-            onPress={() => this.onPressClose()}
-          />
-        )}
+        {/*{!specialOpeningHoursSpecification && (*/}
+        {/*  <Item*/}
+        {/*    title="close"*/}
+        {/*    iconName="power-off"*/}
+        {/*    onPress={() => this.onPressClose()}*/}
+        {/*  />*/}
+        {/*)}*/}
         <Item
           title="openSettings"
           iconName="cog"
@@ -60,14 +39,11 @@ class HeaderRight extends Component {
 function mapStateToProps(state) {
   return {
     restaurant: state.restaurant.restaurant,
-    specialOpeningHoursSpecification:
-      selectSpecialOpeningHoursSpecificationForToday(state),
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    closeRestaurant: restaurant => dispatch(closeRestaurant(restaurant)),
   };
 }
 
