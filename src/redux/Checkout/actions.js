d561a684e9d8af8d956fd0a43df6707fc838ebc1
Alexandre Segura <mex.zktk@gmail.com>
2025-01-07 15:24:54 +0100

    WIP

--- src/redux/Checkout/actions.js
+++ src/redux/Checkout/actions.js
@@ -1919,8 +1919,6 @@ export function setPaymentMethod(paymentMethod, cb) {
         },
       )
       .then(res => {
-        console.log('setPaymentMethod', JSON.stringify(res))
-        console.log('redirectUrl', res.redirectUrl)
         // We update the state with the same cart
         // We can't use response, because it's not a cart
         // TODO Use the payments returned
