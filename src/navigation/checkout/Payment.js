1dc388fef0af87ac9134d16457227fd470c640e0
Alexandre Segura <mex.zktk@gmail.com>
2025-02-04 11:21:54 +0100

    Refactor into functional component.

--- src/navigation/checkout/Payment.js
+++ src/navigation/checkout/Payment.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Center } from 'native-base';
 import { Linking, View } from 'react-native';
 import { connect } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
 import {
   loadPaymentMethods,
@@ -18,76 +19,75 @@ import PaymentMethodPicker from './components/PaymentMethodPicker';
 import HeaderHeightAwareKeyboardAvoidingView from '../../components/HeaderHeightAwareKeyboardAvoidingView';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-class CreditCard extends Component {
-  componentDidMount() {
-    this.props.loadPaymentMethods();
+const routesByCardGateway = {
+  stripe: 'CheckoutPaymentMethodCard',
+  // https://github.com/coopcycle/coopcycle-app/issues/1697
+  // 'mercadopago': 'CheckoutMercadopago',
+};
+
+function onPaymentMethodSelected(type, paymentGateway, setPaymentMethod, navigation) {
+
+  const cardRoute = Object.prototype.hasOwnProperty.call(routesByCardGateway, paymentGateway) ?
+    routesByCardGateway[paymentGateway] : 'CheckoutPaymentMethodCard';
+
+  const routesByMethod = {
+    cash_on_delivery: 'CheckoutPaymentMethodCashOnDelivery',
+    card: cardRoute,
+    edenred: 'CheckoutPaymentMethodEdenred',
+    'edenred+card': 'CheckoutPaymentMethodEdenred',
+  };
+
+  setPaymentMethod(type, (result) => {
+    if (result.redirectUrl) {
+      Linking.openURL(result.redirectUrl);
+    } else {
+      navigation.navigate(routesByMethod[type]);
+    }
+  })
+}
+
+const CreditCard = ({ cart, paymentMethods, paymentGateway, loadPaymentMethods, setPaymentMethod }) => {
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    loadPaymentMethods();
+  }, [ loadPaymentMethods ])
+
+  if (!cart || paymentMethods.length === 0) {
+    return <View />;
   }
 
-  _onPaymentMethodSelected(type) {
-
-    const routesByCardGateway = {
-      stripe: 'CheckoutPaymentMethodCard',
-      // https://github.com/coopcycle/coopcycle-app/issues/1697
-      // 'mercadopago': 'CheckoutMercadopago',
-    };
-
-    const cardRoute = Object.prototype.hasOwnProperty.call(routesByCardGateway, this.props.paymentGateway) ?
-      routesByCardGateway[this.props.paymentGateway] : 'CheckoutPaymentMethodCard';
-
-    const routesByMethod = {
-      cash_on_delivery: 'CheckoutPaymentMethodCashOnDelivery',
-      card: cardRoute,
-      edenred: 'CheckoutPaymentMethodEdenred',
-      'edenred+card': 'CheckoutPaymentMethodEdenred',
-    };
-
-    this.props.setPaymentMethod(type, (result) => {
-      if (result.redirectUrl) {
-        Linking.openURL(result.redirectUrl);
-      } else {
-        this.props.navigation.navigate(routesByMethod[type]);
-      }
-    })
-  }
-
-  render() {
-    const { cart, paymentMethods, paymentGateway } = this.props;
-
-    if (!cart || paymentMethods.length === 0) {
-      return <View />;
-    }
-
-    if (paymentMethods.length === 1 && paymentMethods[0].type === 'card') {
-      if (paymentGateway === 'mercadopago') {
-        this.props.navigation.navigate('CheckoutMercadopago');
-        return null;
-      }
-
-      return (
-        <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
-          <HeaderHeightAwareKeyboardAvoidingView>
-            <CreditCardComp cart={cart} />
-          </HeaderHeightAwareKeyboardAvoidingView>
-        </SafeAreaView>
-      );
-    }
-
-    if (
-      paymentMethods.length === 1 &&
-      paymentMethods[0].type === 'cash_on_delivery'
-    ) {
-      return <CashComp />;
+  if (paymentMethods.length === 1 && paymentMethods[0].type === 'card') {
+    if (paymentGateway === 'mercadopago') {
+      navigation.navigate('CheckoutMercadopago');
+      return null;
     }
 
     return (
-      <Center flex={1}>
-        <PaymentMethodPicker
-          methods={paymentMethods}
-          onSelect={this._onPaymentMethodSelected.bind(this)}
-        />
-      </Center>
+      <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+        <HeaderHeightAwareKeyboardAvoidingView>
+          <CreditCardComp cart={cart} />
+        </HeaderHeightAwareKeyboardAvoidingView>
+      </SafeAreaView>
     );
   }
+
+  if (
+    paymentMethods.length === 1 &&
+    paymentMethods[0].type === 'cash_on_delivery'
+  ) {
+    return <CashComp />;
+  }
+
+  return (
+    <Center flex={1}>
+      <PaymentMethodPicker
+        methods={paymentMethods}
+        onSelect={ (type) => onPaymentMethodSelected(type, paymentGateway, setPaymentMethod, navigation) }
+      />
+    </Center>
+  );
 }
 
 function mapStateToProps(state, ownProps) {
