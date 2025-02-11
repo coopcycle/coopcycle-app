1b09599b00cb95d81d0461b7677a8376fa9e5d17
Juan Acosta <juan.acosta@kilimoagtech.com>
2025-02-11 14:00:11 -0300

    Minor change

--- src/navigation/navigators/StoreNavigator.js
+++ src/navigation/navigators/StoreNavigator.js
@@ -31,7 +31,8 @@ function MainNavigator() {
               'NewDelivery',
               {
                 screen: 'NewDeliveryPickup',
-              })
+              }
+            )
           }
 
           return {
