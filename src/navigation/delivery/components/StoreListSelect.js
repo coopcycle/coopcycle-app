efe52c9726eca88290a2059a5186410359bb98c1
Juan Acosta <juan.93.ctm@gmail.com>
2025-02-20 14:05:30 -0300

    Updated test id

--- src/navigation/delivery/components/StoreListSelect.js
+++ src/navigation/delivery/components/StoreListSelect.js
@@ -1,7 +1,8 @@
-import { Icon, Text } from 'native-base';
 import { FlatList, RefreshControl, StyleSheet } from 'react-native';
+import { Icon, Text } from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from 'react'
 
 import ItemSeparator from '../../../components/ItemSeparator';
 
@@ -27,28 +28,31 @@ export default function StoreListSelect({
   });
 
 
-  const renderItem = (store) => {
+  const renderItem = ({ item, index }) => {
     return (
       <TouchableOpacity
-        onPress={() => onSelectStore(store)}
+        onPress={() => onSelectStore(item)}
         style={styles.item}
-        testID={`dispatch:storeList:${store.id}`}>
+        testID={`dispatch:storeList:${index}`}>
         <Text style={styles.itemLabel}>
-          {store.name}
+          {item.name}
         </Text>
         <Icon as={FontAwesome} name="arrow-right" />
       </TouchableOpacity>
     );
   }
-  return <FlatList
-            data={stores}
-            keyExtractor={(item, index) => item.id}
-            renderItem={({ item }) => renderItem(item)}
-            ItemSeparatorComponent={ItemSeparator}
-            refreshControl={
-              <RefreshControl
-                refreshing={isRefreshing}
-                onRefresh={onRefreshStores}
-              />}
-          />
+
+  return (
+    <FlatList
+      data={stores}
+      keyExtractor={(item, index) => item.id}
+      renderItem={renderItem}
+      ItemSeparatorComponent={ItemSeparator}
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefreshStores}
+        />}
+    />
+  )
 }
