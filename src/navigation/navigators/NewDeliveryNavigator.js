a45fc4aa2789e531338be31e43456c703fb2dc38
warmijusti <justinaparma@gmail.com>
2025-02-21 07:35:08 -0400

    hide beader back title

--- src/navigation/navigators/NewDeliveryNavigator.js
+++ src/navigation/navigators/NewDeliveryNavigator.js
@@ -19,6 +19,7 @@ export const NewDeliveryNavigator = () => (
       component={screens.NewDeliveryStore}
       options={{
         headerShown: false,
+        headerBackTitleVisible: false,
       }}
     />
     <NewDeliveryStack.Screen
