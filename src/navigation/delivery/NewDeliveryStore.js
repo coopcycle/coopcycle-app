00b533f44e5f7a89d67c697c36a145dafe4d5d1c
Juan Acosta <juan.acosta@kilimoagtech.com>
2025-02-25 11:49:26 -0300

    Show Delivery's state stores

--- src/navigation/delivery/NewDeliveryStore.js
+++ src/navigation/delivery/NewDeliveryStore.js
@@ -1,14 +1,15 @@
-import { Box, Text } from 'native-base'
-import React, { useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import { ActivityIndicator, SafeAreaView } from 'react-native'
+import { Box, Text } from 'native-base'
+import { useDispatch, useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import React, { useCallback, useEffect, useState } from 'react'
 
-import { useDispatch } from 'react-redux'
-import KeyboardAdjustView from '../../components/KeyboardAdjustView'
-import { useFetchAllStores } from '../../hooks/useFetchAllFunctions'
-import { loadAddresses, setStore } from '../../redux/Delivery/actions'
+import { loadAddresses, setStore, setStores } from '../../redux/Delivery/actions'
+import { selectStores } from '../../redux/Delivery/selectors'
 import { sortByName } from '../../redux/util'
+import { useFetchAllStores } from '../../hooks/useFetchAllStores'
 import FormInput from './components/FormInput'
+import KeyboardAdjustView from '../../components/KeyboardAdjustView'
 import StoreListSelect from './components/StoreListSelect'
 
 
@@ -21,13 +22,27 @@ const NewDeliveryStore = (props) => {
 
   const [searchQuery, setSearchQuery] = useState('');
   const [storeList, setStoreList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const stores = useSelector(selectStores)
 
   const {
-    stores,
+    stores: backendStores,
     error,
-    isLoading,
+    isLoading: isLoadingBackendStores,
     refreshStores,
-  } = useFetchAllStores(setStoreList);
+  } = useFetchAllStores();
+
+  useEffect(() => {
+    if (backendStores) {
+      dispatch(setStores(backendStores));
+    }
+  }, [backendStores, dispatch]);
+
+  useEffect(() => {
+    setIsLoading(isLoadingBackendStores && (
+      !stores || stores.length === 0
+    ));
+  }, [isLoadingBackendStores, stores]);
 
   const onSelectStore = (store) => {
     dispatch(setStore(store))
@@ -43,18 +58,20 @@ const NewDeliveryStore = (props) => {
   // Filter store by name
   const normalizeString = (str) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
+  const filterStores = useCallback((query, _stores) => {
     const normalizedQuery = normalizeString(query);
-
-    const filtered = (stores || []).filter(store =>
+    const filtered = _stores.filter(store =>
       normalizeString(store.name).includes(normalizedQuery)
     );
 
     // TODO: Is there a way to avoid calling "sortByName" here?
     // It would be nice to delegate it to the "useFetchAllStores" hook since it's already done in there..!
     setStoreList(sortByName(filtered));
-  };
+  }, []);
+
+  useEffect(() => {
+    filterStores(searchQuery, stores);
+  }, [filterStores, searchQuery, stores]);
 
   // TODO: We should do something about the "KeyboardAdjustView" solution..!
   return (
@@ -63,12 +80,12 @@ const NewDeliveryStore = (props) => {
         style={{
           flex: 1,
         }}>
-      {isLoading &&  
+      {isLoading &&
         <Box flex={1} justifyContent="center" alignItems="center">
           <ActivityIndicator animating={true} size="large" />
         </Box>}
       {error && <Text style={{ textAlign: 'center' }}>{t('AN_ERROR_OCCURRED')}</Text>}
-      {!isLoading && !error && 
+      {!isLoading && !error &&
         <>
           <Box p="5">
             <FormInput
@@ -76,7 +93,7 @@ const NewDeliveryStore = (props) => {
             autoCorrect={false}
             autoCapitalize="none"
             returnKeyType="done"
-            onChangeText={handleSearch}
+            onChangeText={setSearchQuery}
             placeholder={t('DISPATCH_NEW_DELIVERY_FILTER_STORE_PLACEHOLDER')}
           />
           </Box>
