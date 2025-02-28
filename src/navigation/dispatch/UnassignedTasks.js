6d2b223804bda730b472ddaa18b7248d9ed3d567
warmijusti <justinaparma@gmail.com>
2025-02-28 08:53:01 -0400

    clear HOC

--- src/navigation/dispatch/UnassignedTasks.js
+++ src/navigation/dispatch/UnassignedTasks.js
@@ -1,60 +1,38 @@
-import { connect } from 'react-redux';
-import { InteractionManager, View } from 'react-native';
-import { Text } from 'native-base';
-import { withTranslation } from 'react-i18next';
-import moment from 'moment';
+import { Box, Text } from 'native-base';
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ActivityIndicator, InteractionManager, View } from 'react-native';
+import { connect, useDispatch, useSelector } from 'react-redux';
 
-import {
-  assignTask,
-  bulkAssignmentTasks,
-  initialize,
-  loadUnassignedTasks,
-} from '../../redux/Dispatch/actions';
+import TapToRefresh from '../../components/TapToRefresh';
+import TaskList from '../../components/TaskList';
 import {
   selectSelectedDate,
   selectTasksWithColor,
+  selectUnassignedTasks,
 } from '../../coopcycle-frontend-js/logistics/redux';
+import { useFetchAllUnassignedTasks } from '../../hooks/useFetchAllUnassignedTasks';
 import { navigateToTask } from '../../navigation/utils';
-import { selectUnassignedTasksNotCancelled } from '../../redux/Dispatch/selectors';
-import { tasksSort } from '../../shared/src/logistics/redux/taskUtils';
-import { useFetchAllTasks } from '../../hooks/useFetchAllTasks';
-import AddButton from './components/AddButton';
-import TapToRefresh from '../../components/TapToRefresh';
-import TaskList from '../../components/TaskList';
-
-
-// hight order component that uses the hook
-function withTasksLoader(WrappedWithTasksComponent) {
-  return function WithTasksLoader(props) {
-    const date = props.date || moment()
-    const {
-      tasks,
-      error,
-      isLoading,
-      refreshTasks
-    } = useFetchAllTasks(date, { enabled: true})
-
-    return (
-      <WrappedWithTasksComponent
-        {...props}
-        tasksFromHook={tasks}
-        tasksError={error}
-        tasksLoading={isLoading}
-        refreshTasks={refreshTasks}
-      />
-    )
-  }
-}
-
-function UnassignedTasks({
-  initialize,
-  navigation,
+import {
   assignTask,
   bulkAssignmentTasks,
-  unassignedTasks,
+  initialize,
+  /*   loadUnassignedTasks, */
+  setUnassignedTasks,
+} from '../../redux/Dispatch/actions';
+import { selectUnassignedTasksNotCancelled } from '../../redux/Dispatch/selectors';
+import { tasksSort } from '../../shared/src/logistics/redux/taskUtils';
+import AddButton from './components/AddButton';
+
+
+function UnassignedTasks({
+  /* refreshTasks,,
+  initialize,
+  assignTask,
+  bulkAssignmentTasks,
+  unassignedTasks, */
   date,
-  refreshTasks,
+  navigation,
   tasksWithColor,
   route,
 }) {
@@ -62,7 +40,29 @@ function UnassignedTasks({
     InteractionManager.runAfterInteractions(() => {
       initialize();
     });
-  }, [initialize]);
+  }, [/* initialize */]);
+
+  const {
+    unassignedTasks: backendUnassignedTasks,
+    error,
+    isLoading,
+    refreshTasks
+  } = useFetchAllUnassignedTasks(date, { enabled: true})
+
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (backendUnassignedTasks && backendUnassignedTasks.length > 0) {
+      dispatch(setUnassignedTasks(backendUnassignedTasks))
+    }
+  }, [backendUnassignedTasks, dispatch])
+
+  const { t } = useTranslation()
+  // check selectUnassignedTasks, shoud we create a new one on Dispatch/selectors ?
+  const unassignedTasks = useSelector(selectUnassignedTasks)
+  console.log('unassignedTasks:', unassignedTasks);
+
 
   const _assignTask = (task, user) => {
     navigation.navigate('DispatchUnassignedTasks');
@@ -85,7 +85,7 @@ function UnassignedTasks({
   }
 
   const { navigate } = navigation;
-  const isEmpty = unassignedTasks.length === 0;
+  const isEmpty = unassignedTasks?.length === 0;
 
   return (
     <View style={{ flex: 1 }}>
@@ -99,6 +99,11 @@ function UnassignedTasks({
         </AddButton>
       </View>
       <View style={{ flex: 1 }}>
+        {isLoading &&
+          <Box flex={1} justifyContent="center" alignItems="center">
+            <ActivityIndicator animating={true} size="large" />
+          </Box>}
+        {error && <Text style={{ textAlign: 'center' }}>{t('AN_ERROR_OCCURRED')}</Text>}
         {isEmpty && <TapToRefresh onPress={refreshTasks} />}
         {!isEmpty && (
           <TaskList
@@ -144,7 +149,7 @@ function mapDispatchToProps(dispatch) {
   return {
     assignTask: (task, username) => dispatch(assignTask(task, username)),
     initialize: () => dispatch(initialize()),
-    /* loadUnassignedTasks: () => dispatch(loadUnassignedTasks()), */
+/*     loadUnassignedTasks: () => dispatch(loadUnassignedTas()), */
     bulkAssignmentTasks: (tasks, username) =>
       dispatch(bulkAssignmentTasks(tasks, username)),
   };
@@ -153,4 +158,4 @@ function mapDispatchToProps(dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(withTasksLoader(withTranslation()(UnassignedTasks)));
+)(UnassignedTasks);
