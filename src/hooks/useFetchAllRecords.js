e8b7e076900c0ccb6157340938d9e7b117d4ff67
Juan Acosta <juan.93.ctm@gmail.com>
2025-03-11 14:43:51 -0300

    Revert changes in useFetchAllRecords

--- src/hooks/useFetchAllRecords.js
+++ src/hooks/useFetchAllRecords.js
@@ -7,18 +7,13 @@ import { fetchAllRecords } from "../redux/util";
 export function useFetchAllRecords(url, itemsPerPage, options = {}) {
   const httpClient = useSelector(state => state.app.httpClient);
 
-  useEffect(() => {
-    console.log("AAAAAAAAAA useFetchAllRecords useEffect: ", url, itemsPerPage, JSON.stringify(options));
-  }, [itemsPerPage, options, url]);
-
   const [data, setData] = useState();
   const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
   const [force, setForce] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = useCallback(() => {
     if(options.enabled || force) {
-      console.log("AAAAAAAAAA useFetchAllRecords useCallback: ", JSON.stringify(options));
       setIsLoading(true);
       fetchAllRecords(httpClient, url, itemsPerPage, options.params)
           .then(setData)
@@ -31,7 +26,6 @@ export function useFetchAllRecords(url, itemsPerPage, options = {}) {
   }, [force, httpClient, itemsPerPage, options, url]);
 
   useEffect(() => {
-    console.log("AAAAAAAAAA useFetchAllRecords fetchData");
     fetchData();
   }, [fetchData]);
 
