fc4a67cf5dcb9d9a15b668ff8bf56f964b07d996
warmijusti <justinaparma@gmail.com>
2025-03-14 13:55:44 -0400

    remove unused function and update tests

--- src/redux/util.js
+++ src/redux/util.js
@@ -38,10 +38,13 @@ export function sortByKey(list, key) {
   return _.sortBy(list, [key]);
 }
 
-export async function fetchAllRecordsUsingHttpClient(httpClient, url, itemsPerPage, otherParams = null) {
+export async function fetchAllRecordsUsingFetchWithBQ(fetchWithBQ, url, itemsPerPage, otherParams = null) {
   const fetch = async (page) => {
     const params = new URLSearchParams({page, itemsPerPage, ...otherParams});
-    return httpClient.get(`${url}?${params.toString()}`)
+    const result = await fetchWithBQ(`${url}?${params.toString()}`);
+    console.log('RESULTTT', result)
+    return result.data;
+    
   };
   const firstRs = await fetch(1);
 
@@ -61,31 +64,4 @@ export async function fetchAllRecordsUsingHttpClient(httpClient, url, itemsPerPa
       .map(page => fetch(page))
     )
     .then(results => results.reduce((acc, rs) => acc.concat(rs['hydra:member']), firstRs['hydra:member']));
-}
-
-export async function fetchAllRecordsUsingFetchWithBQ(fetchWithBQ, url, itemsPerPage, otherParams = null) {
-  const fetch = async (page) => {
-    const params = new URLSearchParams({page, itemsPerPage, ...otherParams});
-    const result = await fetchWithBQ(`${url}?${params.toString()}`);
-
-    return result.data;
-  };
-  const firstRs = await fetch(1);
-
-  if (!Object.hasOwn(firstRs, 'hydra:totalItems') || firstRs['hydra:totalItems'] <= firstRs['hydra:member'].length) {
-    // Total items were already returned in the 1st request!
-    return firstRs['hydra:member'];
-  }
-
-  // OK more pages are needed to be fetched to get all items..!
-  const totalItems = firstRs['hydra:totalItems'];
-  const maxPage = Math.trunc(totalItems / itemsPerPage) + (totalItems % itemsPerPage === 0 ? 0 : 1);
-
-  return Promise
-    .all(
-      [...Array(maxPage+1).keys()]
-      .slice(2)
-      .map(page => fetch(page))
-    )
-    .then(results => results.reduce((acc, rs) => acc.concat(rs.data['hydra:member']), firstRs['hydra:member']));
 }
