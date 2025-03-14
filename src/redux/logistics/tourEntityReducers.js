8227b362b43c8618bfea46942ca10dfdfff805f6
Juan Acosta <juan.93.ctm@gmail.com>
2025-03-14 10:48:29 -0300

    Moved tourEntityReducer to redux/logistics

--- src/redux/logistics/tourEntityReducers.js
+++ src/redux/logistics/tourEntityReducers.js
@@ -2,15 +2,13 @@ import {
   DELETE_TOUR_SUCCESS,
   LOAD_TOURS,
   UPDATE_TOUR,
-} from './actions'
-import {
-  tourAdapter
-} from './adapters'
+  tourAdapter,
+} from "../../shared/logistics/redux";
+
 
 const initialState = tourAdapter.getInitialState()
 
 export default (state = initialState, action) => {
-
   switch (action.type) {
     case LOAD_TOURS:
       return tourAdapter.upsertMany(state, action.payload)
@@ -18,7 +16,7 @@ export default (state = initialState, action) => {
       return tourAdapter.upsertOne(state, action.payload);
     case DELETE_TOUR_SUCCESS:
       return tourAdapter.removeOne(state, action.payload);
+    default:
+      return state;
   }
-
-  return state
 }
