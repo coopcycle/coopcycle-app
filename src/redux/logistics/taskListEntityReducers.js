a127d295fa16f3d98b49ce3f8528a577da8d6dca
Juan Acosta <juan.93.ctm@gmail.com>
2025-04-16 13:44:16 -0300

    Restore update TaskList on assign/unassign

--- src/redux/logistics/taskListEntityReducers.js
+++ src/redux/logistics/taskListEntityReducers.js
@@ -1,15 +1,20 @@
 import {
+  bulkAssignmentTasksSuccess,
   changeDate,
+  createTaskSuccess,
   loadTaskListsSuccess,
+  unassignTaskSuccess,
   updateTaskListsSuccess,
 } from '../Dispatch/actions';
 import {
   taskListAdapter,
+  taskListEntityUtils,
   taskListUtils,
 } from '../../coopcycle-frontend-js/logistics/redux';
 import { SET_USER } from '../App/actions';
 
 const initialState = taskListAdapter.getInitialState();
+const selectors = taskListAdapter.getSelectors(state => state);
 
 export default (state = initialState, action) => {
   if (changeDate.match(action) || action.type === SET_USER) {
@@ -27,6 +32,37 @@ export default (state = initialState, action) => {
     const taskList = taskListUtils.replaceItemsWithItemIds(action.payload);
     return taskListAdapter.upsertOne(state, taskList);
   }
+
+  if (createTaskSuccess.match(action)) {
+    let task = action.payload;
+
+    if (task.isAssigned) {
+      let newItems = taskListEntityUtils.addAssignedTask(
+        selectors.selectEntities(state),
+        task,
+      );
+      return taskListAdapter.upsertMany(state, newItems);
+    } else {
+      return state;
+    }
+  }
+
+  if (bulkAssignmentTasksSuccess.match(action)) {
+    const taskList = taskListEntityUtils.addAssignedTasks(
+      selectors.selectEntities(state),
+      action.payload,
+    )
+    return taskListAdapter.upsertOne(state, taskList);
+  }
+
+  if (unassignTaskSuccess.match(action)) {
+    let newItems = taskListEntityUtils.removeUnassignedTask(
+      selectors.selectEntities(state),
+      action.payload,
+    );
+    return taskListAdapter.upsertMany(state, newItems);
+  }
+
 
   return state;
 };
