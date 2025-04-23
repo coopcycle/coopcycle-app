32ce60e7ec3054d2efe7b364969a0c49c81b487a
warmijusti <justinaparma@gmail.com>
2025-04-23 07:49:16 -0400

    update assing using slice - update class to functional component

--- src/navigation/dispatch/AssignTask.js
+++ src/navigation/dispatch/AssignTask.js
@@ -1,20 +1,23 @@
 import { Text } from 'native-base';
-import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import TaskList from '../../components/TaskList';
-import { selectTasksWithColor } from '../../coopcycle-frontend-js/logistics/redux';
-import { assignTask } from '../../redux/Dispatch/actions';
+import { selectAllTasks, selectSelectedDate, selectTasksWithColor } from '../../coopcycle-frontend-js/logistics/redux';
+import { useBulkAssignTasksMutation } from '../../redux/api/slice';
+import { withUnassignedLinkedTasks } from '../../shared/src/logistics/redux/taskUtils';
 import { selectUnassignedTasksNotCancelled } from '../../redux/Dispatch/selectors';
 
-class AssignTask extends Component {
-  render() {
-    const { username } = this.props.route.params;
-    const isEmpty = this.props.unassignedTasks.length === 0;
+export default function AssignTask({ route }) {
+ const { t }  = useTranslation()
+ const unassignedTasks = useSelector(selectUnassignedTasksNotCancelled);
+ const isEmpty = unassignedTasks.length === 0;
+ const tasksWithColor = useSelector(selectTasksWithColor);
+ const selectedDate = useSelector(selectSelectedDate);
 
-    let contentProps = {};
+ let contentProps = {};
     if (isEmpty) {
       contentProps = {
         flex: 1,
@@ -22,40 +25,35 @@ class AssignTask extends Component {
       };
     }
 
-    return (
-      <View style={contentProps}>
-        {isEmpty && (
-          <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text note>{this.props.t('DISPATCH_NO_TASKS')}</Text>
-          </View>
-        )}
-        {!isEmpty && (
-          <TaskList
-            tasks={this.props.unassignedTasks}
-            tasksWithColor={this.props.tasksWithColor}
-            onTaskClick={task => this.props.assignTask(task, username)}
-          />
-        )}
+   // USING SLICE
+   const [bulkAssignTasks] = useBulkAssignTasksMutation();
+   const allTasks = useSelector(selectAllTasks); 
+
+ const assignTask = (task) => {
+  const username = route.params.username
+  const taskIdToAssign = withUnassignedLinkedTasks(task, allTasks)
+        .map(item => item['@id']);
+        bulkAssignTasks({
+        tasks: taskIdToAssign,
+        username: username,
+        date: selectedDate
+      })
+ }
+
+ return (
+  <View style={contentProps}>
+    {isEmpty && (
+      <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text note>{t('DISPATCH_NO_TASKS')}</Text>
       </View>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    unassignedTasks: selectUnassignedTasksNotCancelled(state),
-    tasksWithColor: selectTasksWithColor(state),
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    assignTask: (task, username) => dispatch(assignTask(task, username)),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(withTranslation()(AssignTask));
+    )}
+    {!isEmpty && (
+      <TaskList
+        tasks={unassignedTasks}
+        tasksWithColor={tasksWithColor}
+        onTaskClick={task => assignTask(task)}
+      />
+    )}
+  </View>
+)}
