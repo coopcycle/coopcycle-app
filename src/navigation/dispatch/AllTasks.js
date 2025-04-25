cd7318a579405ff4f047e8065a90490b5fe33433
warmijusti <justinaparma@gmail.com>
2025-04-25 09:54:05 -0400

    add error handlig and isFetching

--- src/navigation/dispatch/AllTasks.js
+++ src/navigation/dispatch/AllTasks.js
@@ -1,14 +1,67 @@
-import { InteractionManager, RefreshControl } from "react-native";
 import { ScrollView } from 'native-base';
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { InteractionManager, RefreshControl, StyleSheet } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import { initialize } from "../../redux/Dispatch/actions";
-import { selectSelectedDate, selectTaskLists } from "../../shared/logistics/redux";
 import { selectUnassignedTasksNotCancelled } from "../../redux/Dispatch/selectors";
-import { useAllTasks } from "./useAllTasks";
+import { selectSelectedDate, selectTaskLists } from "../../shared/logistics/redux";
 import GroupedTasks from "./components/GroupedTasks";
+import { useAllTasks } from "./useAllTasks";
+
+import {
+  Center,
+  Heading,
+  Text
+} from 'native-base';
+
+import {
+  dateSelectHeaderHeight,
+  headerFontSize,
+  primaryColor,
+  whiteColor,
+} from '../../styles/common';
+
+let styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  dateHeader: {
+    backgroundColor: primaryColor,
+    height: dateSelectHeaderHeight,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  dateHeaderText: {
+    color: whiteColor,
+    paddingHorizontal: 15,
+    fontSize: headerFontSize,
+  },
+  icon: {
+    color: whiteColor,
+    fontSize: 32,
+  },
+  body: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  todayContainer: {
+    backgroundColor: whiteColor,
+    alignItems: 'center',
+    paddingVertical: 12,
+    marginBottom: 4,
+  },
+  todayButton: {
+    color: primaryColor,
+    fontSize: 18,
+    fontWeight: '300',
+  },
+});
 
 
 export default function AllTasks({
@@ -35,6 +88,15 @@ export default function AllTasks({
     });
   }, [dispatch]);
 
+  if (isError) {
+    return (
+      <Center w="95%" h="80%">
+        <Heading>{t('AN_ERROR_OCCURRED')} </Heading>
+        <Text>{t('TRY_LATER')}</Text>
+      </Center>
+    );
+  }
+
   return (
     <ScrollView 
       style={{ flex: 1 }}
@@ -42,25 +104,29 @@ export default function AllTasks({
         <RefreshControl refreshing={isFetching} onRefresh={refetch} />
       }
       >
-      <GroupedTasks
-          color={'#000000'}
-          navigation
-          route
-          tasks={unassignedTasks}
-          title={t('New orders')}
-      />
-
+      {!isFetching &&
+      <>
+        <GroupedTasks
+            backgroundColor={whiteColor}
+            textColor={primaryColor}
+            navigation
+            route
+            tasks={unassignedTasks}
+            title={t('UNASSIGNED_TASKS')}
+        />
       {taskLists
         .filter(taskList => taskList.items.length > 0)
         .map(taskList => (
         <GroupedTasks
-            color={taskList.color}
+            backgroundColor={taskList.color}
+            textColor={'#000'}
             navigation
             route
             tasks={taskList.items}
             title={taskList.username}
         />
       ))}
+      </>}
     </ScrollView>
   );
 }
