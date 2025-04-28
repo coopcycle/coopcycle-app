8ad06ba43577eacfce211f27bac7e2a6106b34c4
warmijusti <justinaparma@gmail.com>
2025-04-28 08:05:49 -0400

    clean styling

--- src/navigation/dispatch/components/GroupedTasks.js
+++ src/navigation/dispatch/components/GroupedTasks.js
@@ -1,15 +1,17 @@
 import { Box, Text } from "native-base";
 import { useDispatch, useSelector } from "react-redux";
 
-import { navigateToTask } from '../../../navigation/utils';
-import { selectTasksWithColor } from "../../../shared/logistics/redux";
-import { selectUnassignedTasksNotCancelled } from "../../../redux/Dispatch/selectors";
-import { unassignTask } from "../../../redux/Dispatch/actions";
 import TaskList from "../../../components/TaskList";
+import { navigateToTask } from '../../../navigation/utils';
+import { unassignTask } from "../../../redux/Dispatch/actions";
+import { selectUnassignedTasksNotCancelled } from "../../../redux/Dispatch/selectors";
+import { selectTasksWithColor } from "../../../shared/logistics/redux";
+import { headerFontSize } from "../../../styles/common";
 
 
 export default function GroupedTasks({
-  color,
+  backgroundColor,
+  textColor,
   navigation,
   route,
   tasks,
@@ -24,28 +26,32 @@ export default function GroupedTasks({
 
   return (
     <Box marginBottom={4}>
-      <Box backgroundColor={color}>
+      <Box backgroundColor={backgroundColor}>
         <Text
-          color={'white'}
+          color={textColor}
+          fontSize={headerFontSize}
           padding={4}
         >
             {title}
         </Text>
       </Box>
-      <TaskList
-        tasks={tasks}
-        tasksWithColor={tasksWithColor}
-        onSwipeRight={unassignTaskHandler}
-        swipeOutRightEnabled={task => task.status !== 'DONE'}
-        swipeOutRightIconName="close"
-        onTaskClick={task =>
-          navigateToTask(
-            navigation,
-            route,
-            task,
-            unassignedTasks,
-          )
-        }
-      />
+      <Box paddingHorizontal={8}>
+        <TaskList
+          tasks={tasks}
+          tasksWithColor={tasksWithColor}
+          onSwipeRight={unassignTaskHandler}
+          swipeOutRightEnabled={task => task.status !== 'DONE'}
+          swipeOutRightIconName="close"
+          swipeOutLeftEnabled={task => task.assignedTo === null}
+          onTaskClick={task =>
+            navigateToTask(
+              navigation,
+              route,
+              task,
+              unassignedTasks,
+            )
+          }
+        />
+      </Box>
     </Box>)
 }
