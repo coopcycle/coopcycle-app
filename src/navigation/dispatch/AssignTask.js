e26bb7992483408851ded2f66141fcdd2836b897
Juan Acosta <juan.93.ctm@gmail.com>
2025-04-29 09:41:03 -0300

    Assign task with related tasks and tours using hook in AssignTasks

--- src/navigation/dispatch/AssignTask.js
+++ src/navigation/dispatch/AssignTask.js
@@ -4,11 +4,10 @@ import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
 import React from 'react';
 
-import { getUserTasks, withUnassignedLinkedTasks } from '../../shared/src/logistics/redux/taskUtils';
-import { selectAllTasks, selectSelectedDate, selectTaskLists, selectTasksWithColor } from '../../coopcycle-frontend-js/logistics/redux';
+import { selectTasksWithColor } from '../../coopcycle-frontend-js/logistics/redux';
 import { selectUnassignedTasksNotCancelled } from '../../redux/Dispatch/selectors';
-import { useSetTaskListsItemsMutation } from '../../redux/api/slice';
 import TaskList from '../../components/TaskList';
+import useSetTaskListsItems from '../../shared/src/logistics/redux/hooks/useSetTaskListItems';
 
 
 export default function AssignTask({ route }) {
@@ -16,7 +15,6 @@ export default function AssignTask({ route }) {
   const unassignedTasks = useSelector(selectUnassignedTasksNotCancelled);
   const isEmpty = unassignedTasks.length === 0;
   const tasksWithColor = useSelector(selectTasksWithColor);
-  const selectedDate = useSelector(selectSelectedDate);
 
   let contentProps = {};
   if (isEmpty) {
@@ -26,21 +24,13 @@ export default function AssignTask({ route }) {
     };
   }
 
-  // USING SLICE
-  const [setTaskListsItems] = useSetTaskListsItemsMutation();
-  const allTasks = useSelector(selectAllTasks);
-  const allTaskLists = useSelector(selectTaskLists);
+  const {
+    assignTaskWithRelatedTasks,
+  } = useSetTaskListsItems();
 
   const assignTask = (task) => {
-    const username = route.params.username
-    const existingTaskIds = getUserTasks(username, allTaskLists).map(item => item['@id']);
-    const taskIdsToAssign = withUnassignedLinkedTasks(task, allTasks).map(item => item['@id']);
-    const allTaskIds = [...existingTaskIds, ...taskIdsToAssign];
-    setTaskListsItems({
-      tasks: allTaskIds,
-      username: username,
-      date: selectedDate
-    })
+    const user = {username: route.params.username};
+    assignTaskWithRelatedTasks(task, user);
   }
 
   return (
