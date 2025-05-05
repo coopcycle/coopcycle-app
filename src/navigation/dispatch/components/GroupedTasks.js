305e967fa00bd62a866f535263c21de20da081f1
warmijusti <justinaparma@gmail.com>
2025-05-05 15:01:40 -0400

    Add section list

--- src/navigation/dispatch/components/GroupedTasks.js
+++ src/navigation/dispatch/components/GroupedTasks.js
@@ -1,97 +1,106 @@
-import { Box, Text } from "native-base";
-import { useSelector } from "react-redux";
+import { Text } from 'native-base';
+import { useSelector } from 'react-redux';
 
-import TaskList from "../../../components/TaskList";
-import { navigateToTask } from '../../../navigation/utils';
-import { selectUnassignedTasksNotCancelled } from "../../../redux/Dispatch/selectors";
-import { selectTasksWithColor } from "../../../shared/logistics/redux";
-import { headerFontSize } from "../../../styles/common";
 import { useNavigation } from '@react-navigation/native';
-import useSetTaskListsItems from "../../../shared/src/logistics/redux/hooks/useSetTaskListItems";
-
+import { RefreshControl, SectionList } from 'react-native';
+import TaskList from '../../../components/TaskList';
+import { navigateToTask } from '../../../navigation/utils';
+import { selectUnassignedTasksNotCancelled } from '../../../redux/Dispatch/selectors';
+import { selectTasksWithColor } from '../../../shared/logistics/redux';
+import useSetTaskListsItems from '../../../shared/src/logistics/redux/hooks/useSetTaskListItems';
 
 export default function GroupedTasks({
-  backgroundColor,
-  textColor,
+  sections,
   route,
-  tasks,
-  tasksType,
-  title,
+  refetch,
+  isFetching,
 }) {
   const navigation = useNavigation();
 
   const tasksWithColor = useSelector(selectTasksWithColor);
   const unassignedTasks = useSelector(selectUnassignedTasksNotCancelled);
 
-  const { 
+  const {
     unassignTaskWithRelatedTasks,
     assignTaskWithRelatedTasks,
-    bulkAssignTasksWithRelatedTasks
-   } = useSetTaskListsItems()
+    bulkAssignTasksWithRelatedTasks,
+  } = useSetTaskListsItems();
 
-  const unassignTaskHandler = task => unassignTaskWithRelatedTasks(task)
+  const unassignTaskHandler = task => unassignTaskWithRelatedTasks(task);
 
-  const allowToSelect = (task) => {
+  const allowToSelect = task => {
     return task.status !== 'DONE';
-  }
+  };
 
   const _assignTask = (task, user) => {
     assignTaskWithRelatedTasks(task, user);
-  }
+  };
 
-  
-  const assignSelectedTasks = (selectedTasks) => {
-    const selectedTasksIds = selectedTasks.map((task) => task['@id'])
-    console.log('selectedTasks', selectedTasksIds)
+  const assignSelectedTasks = selectedTasks => {
+    const selectedTasksIds = selectedTasks.map(task => task['@id']);
+    console.log('selectedTasks', selectedTasksIds);
     navigation.navigate('DispatchPickUser', {
       onItemPress: user => _bulkAssign(user, selectedTasks),
     });
-  }
+  };
 
   const _bulkAssign = (user, selectedTasks) => {
     bulkAssignTasksWithRelatedTasks(selectedTasks, user);
-  }
+  };
 
   return (
-    <Box marginBottom={4}>
-      <Box backgroundColor={backgroundColor}>
-        <Text
-          color={textColor}
-          fontSize={headerFontSize}
-          padding={4}
-        >
-            {title}
-        </Text>
-      </Box>
-      <Box paddingHorizontal={8}>
-        <TaskList
-          tasks={tasks}
-          tasksType={tasksType}
-          tasksWithColor={tasksWithColor}
-          onSwipeRight={unassignTaskHandler}
-          swipeOutRightEnabled={task => task.status !== 'DONE'}
-          swipeOutRightIconName="close"
-          swipeOutLeftEnabled={task => !task.isAssigned}
-          onSwipeLeft={task =>
-            navigation.navigate('DispatchPickUser', {
-              onItemPress: user => _assignTask(task, user),
-            })
+    <>
+      <SectionList
+        sections={sections}
+        keyExtractor={(item, index) => item?.id?.toString() || index.toString()}
+        renderItem={({ section, index }) => {
+          // TODO check why lists are repeating, is this necessary?
+          if (index === 0) {
+            return (
+              <TaskList
+                tasks={section.data}
+                tasksType={section.tasksType}
+                tasksWithColor={tasksWithColor}
+                onSwipeRight={unassignTaskHandler}
+                swipeOutRightEnabled={task => task.status !== 'DONE'}
+                swipeOutRightIconName="close"
+                swipeOutLeftEnabled={task => !task.isAssigned}
+                onSwipeLeft={task =>
+                  navigation.navigate('DispatchPickUser', {
+                    onItemPress: user => _assignTask(task, user),
+                  })
+                }
+                swipeOutLeftIconName="user"
+                onTaskClick={task =>
+                  navigateToTask(navigation, route, task, unassignedTasks)
+                }
+                allowMultipleSelection={allowToSelect}
+                multipleSelectionIcon="user"
+                onMultipleSelectionAction={assignSelectedTasks}
+              />
+            );
           }
-          swipeOutLeftIconName="user"
-          onTaskClick={task =>
-            navigateToTask(
-              navigation,
-              route,
-              task,
-              unassignedTasks,
-            )
-          }
-          allowMultipleSelection={allowToSelect}
-          multipleSelectionIcon="user"
-          onMultipleSelectionAction={selectedTasks =>
-            assignSelectedTasks(selectedTasks)
-          }
-        />
-      </Box>
-    </Box>)
+          return null; // Avoid rendering per item
+        }}
+        renderSectionHeader={({ section }) => (
+          <Text
+            style={{
+              backgroundColor: section.backgroundColor,
+              color: section.textColor,
+              padding: 20,
+              fontWeight: 700
+            }}>
+            {section.title}
+          </Text>
+        )}
+        onRefresh={refetch}
+        refreshing={isFetching}
+        // TODO loader disappears
+        refreshControl={
+          <RefreshControl refreshing={isFetching} onRefresh={refetch} />
+        } 
+        stickySectionHeadersEnabled={true}
+      />
+    </>
+  );
 }
