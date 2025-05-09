3c442d4050070445de842d1949ca9d014dfacb11
Juan Acosta <juan.93.ctm@gmail.com>
2025-05-09 10:31:34 -0300

    Split tests cases in different files

--- e2e/dispatch/success__complete_task.spec.js
+++ e2e/dispatch/success__complete_task.spec.js
@@ -13,39 +13,15 @@ import {
 const USERNAME = 'jane';
 
 //FIXME: run these tests for iOS too (see https://github.com/coopcycle/coopcycle-ops/issues/97)
-describeif(device.getPlatform() === 'android')('Dispatch - Task lifecycle', () => {
+describeif(device.getPlatform() === 'android')('Dispatch - Complete a task', () => {
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await device.reloadReactNative();
     await loadDispatchFixture();
     await doLoginForUserWithRoleDispatcher();
     await assignTaskToUser(USERNAME);
-  })
-
-  beforeEach(async () => {
   });
 
-  it('should mark a task as started', async () => {
-      // Open jane's task list
-      await tapById('dispatch:assignedTab');
-      await tapById(`dispatch:taskLists:${USERNAME}`);
-
-      // Open assigned task
-      await tapById('task:0:assign');
-
-      // Swipe complete button, tap ok and press 'Start'
-      await swipeRight('task:completeButton');
-      await tapById('task:completeSuccessButton');
-      await tapByText('▶ Start');
-
-      // Go back to jane's task list
-      await device.pressBack();
-
-      // Verify task has status "DOING"
-      await expect(element(by.id('taskListItemIcon-DOING'))).toBeVisible();
-  });
-
-  // TODO: move to specific file
   it('should mark a task as done', async () => {
       // Open jane's task list
       await tapById('dispatch:assignedTab');
