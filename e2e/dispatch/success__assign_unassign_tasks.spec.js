2d63eb5245fea0f674bf294d91ddf06465b38527
warmijusti <justinaparma@gmail.com>
2025-05-13 07:58:09 -0400

    should assing a single task to a courier

--- e2e/dispatch/success__assign_unassign_tasks.spec.js
+++ e2e/dispatch/success__assign_unassign_tasks.spec.js
@@ -1,28 +1,31 @@
-import { element } from "prop-types";
-import { assignTaskToUser, doLoginForUserWithRoleDispatcher, loadDispatchFixture } from "./utils";
-import { itif } from "../utils";
+import { element } from 'prop-types';
+import {
+  assignTaskToUser,
+  doLoginForUserWithRoleDispatcher,
+  loadDispatchFixture,
+} from './utils';
+import { itif } from '../utils';
 
 const USERNAME = 'jane';
 
 describe('Dispatch - Assing and unassign tasks', () => {
-    beforeEach(async () => {
-        await device.reloadReactNative();
-        await loadDispatchFixture()
-        await doLoginForUserWithRoleDispatcher();
-    });
+  beforeEach(async () => {
+    await device.reloadReactNative();
+    await loadDispatchFixture();
+    await doLoginForUserWithRoleDispatcher();
+  });
 
-    itif(device.getPlatform() === 'android')(
-        'should assing a single task to a courier',
-        async () => {
-            // TODO check - this refers to the list, not tasks
-            await expect(element(by.id('unassignedTasksList'))).toBeVisible();
-            await expect(element(by.id('task:0'))).toBeVisible();
-            await assignTaskToUser(USERNAME)
-            // Verify task is on Jane's task list
-                // Jane has a taskList
-            await expect(element(by.text('jane'))).toBeVisible()
-                // TODO: how to check this only on Jane's task list
-            await expect(element(by.id('task:0'))).toBeVisible();
-                }
-    )
-})
+  itif(device.getPlatform() === 'android')(
+    'should assing a single task to a courier',
+    async () => {
+      // TODO check - this refers to the list, not tasks
+      await expect(
+        element(by.id('unassignedTasksList')).typeText('John Doe'),
+      ).toBeVisible();
+      await assignTaskToUser(USERNAME);
+
+      // Verify task is on Jane's task list
+      await expect(element(by.text('jane')).typeText('John Doe')).toBeVisible();
+    },
+  );
+});
