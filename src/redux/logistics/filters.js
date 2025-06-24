431a9d5411d2b3cf648efde28692d974f8c55835
Juan Acosta <juan.acosta@kilimoagtech.com>
2025-06-24 10:29:55 -0300

    Added utils functions to create filters

--- src/redux/logistics/filters.js
+++ src/redux/logistics/filters.js
@@ -3,3 +3,8 @@ export const filterStatusDone = { status: 'DONE' };
 export const filterStatusFailed = { status: 'FAILED' };
 
 export const filterHasIncidents = { hasIncidents: true };
+
+export const filterByKeyword = keyword => ({keyword});
+
+// TODO: unify key as "tag". It is used in courier/Tags.js view
+export const filterByTag = tag => ({tags: tag});
