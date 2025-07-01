46045faaff1bdd3da71b5b35b28fc4d74654d032
warmijusti <justinaparma@gmail.com>
2025-07-01 13:32:33 -0500

    remove DEP comments

--- src/navigation/dispatch/components/BulkEditTasksFloatingButton.js
+++ src/navigation/dispatch/components/BulkEditTasksFloatingButton.js
@@ -2,87 +2,27 @@ import _ from 'lodash';
 import { Fab, Icon } from 'native-base';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-import { darkRedColor, whiteColor } from '../../../styles/common';
-import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { selectSelectedTasks } from '../../../redux/Dispatch/selectors';
+import { darkRedColor, whiteColor } from '../../../styles/common';
 
-function BulkEditTasksFloatingButton({
-  onPress,
-  iconName
-}) {
-
-  // useImperativeHandle(ref, () => ({
-  //     addOrder: (task, taskListId) => addItem(task, taskListId, true),
-  //     addTask: (task, taskListId) => addItem(task, taskListId, false),
-  //     removeOrder: (task, taskListId) => removeItem(task, taskListId, true),
-  //     removeTask: (task, taskListId) => removeItem(task, taskListId, false),
-  //     clearSelectedTasks: () => setSelectedTasks({
-  //       orders: {},
-  //       tasks: {},
-  // })
-  //   }
-  // ), [addItem, removeItem]);
-
-  // const addItem = useCallback((task, taskListId, isAssigningOrder) => {
-  //   removeItem(task, taskListId, !isAssigningOrder);
-
-  //   setSelectedTasks(prevSelectedTasks => {
-  //     const { orders, tasks } = prevSelectedTasks;
-  //     const itemsList = isAssigningOrder ? orders : tasks;
-  //     const newItemsList = {...itemsList};
-
-  //     if (!newItemsList[taskListId]) {
-  //       newItemsList[taskListId] = [];
-  //     }
-
-  //     newItemsList[taskListId].push(task);
-
-  //     return {
-  //       orders: isAssigningOrder ? newItemsList : orders,
-  //       tasks: !isAssigningOrder ? newItemsList : tasks,
-  //     };
-  //   });
-  // }, [removeItem]);
-
-  // const removeItem = useCallback((task, taskListId, isUnassigningOrder) => {
-  //   setSelectedTasks(prevSelectedTasks => {
-  //     const { orders, tasks } = prevSelectedTasks;
-  //     const itemsList = isUnassigningOrder ? orders : tasks;
-  //     const newItemsList = {...itemsList};
-  //     const selectedTasksForTaskList = newItemsList[taskListId];
-
-  //     if(!selectedTasksForTaskList) {
-  //       return prevSelectedTasks;
-  //     }
-
-  //     newItemsList[taskListId] = selectedTasksForTaskList.filter(t => t.id !== task.id);
-
-  //     if (newItemsList[taskListId].length === 0) {
-  //       delete newItemsList[taskListId];
-  //     }
-
-  //     return {
-  //       orders: isUnassigningOrder ? newItemsList : orders,
-  //       tasks: !isUnassigningOrder ? newItemsList : tasks,
-  //     };
-  //   });
-  // }, []);
-
-  const selectedTasks = useSelector(state => state.dispatch.ui.selectedTasks);
+function BulkEditTasksFloatingButton({ onPress, iconName }) {
+  const selectedTasks = useSelector(selectSelectedTasks);
 
   const allSelectedTasks = useMemo(() => {
-     const ordersByTaskList = selectedTasks.orders || {};
-     const tasksByTaskList = selectedTasks.tasks || {};
+    const ordersByTaskList = selectedTasks.orders || {};
+    const tasksByTaskList = selectedTasks.tasks || {};
 
-     const orders = _.flatMap(Object.values(ordersByTaskList));
-     const tasks = _.flatMap(Object.values(tasksByTaskList));
+    const orders = _.flatMap(Object.values(ordersByTaskList));
+    const tasks = _.flatMap(Object.values(tasksByTaskList));
 
-     return [...tasks, ...orders];
-   }, [selectedTasks])
+    return [...tasks, ...orders];
+  }, [selectedTasks]);
 
-   const handleOnPress = () => {
-     onPress(selectedTasks);
-   };
+  const handleOnPress = () => {
+    onPress(selectedTasks);
+  };
 
   return (
     <>
@@ -98,15 +38,15 @@ function BulkEditTasksFloatingButton({
             marginRight: 8,
             padding: 0,
             height: 94,
-            width: 94
+            width: 94,
           }}
           icon={
             <Icon
               as={FontAwesome}
               name={iconName}
-              size='4xl'
+              size="4xl"
               color={darkRedColor}
-              style={{ padding: 0}}
+              style={{ padding: 0 }}
               testID="bulkAssignButton"
             />
           }
