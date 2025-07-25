1916baa2a29d61a7e2b705017bc3c8d7cef6445a
gonzalosanto <sancruzgon@gmail.com>
2025-07-25 12:06:14 -0300

    navigation to task details, changed to flatlist

--- src/navigation/dispatch/DispatchOrderDetails.js
+++ src/navigation/dispatch/DispatchOrderDetails.js
@@ -1,21 +1,28 @@
-import React, { useMemo, useState } from 'react';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import React, { useCallback, useMemo, useState } from 'react';
+import { FlatList } from 'native-base';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 
+import i18n from '../../i18n';
 import { selectAllTasks } from '../../shared/logistics/redux';
 import MiniMap from '../task/components/MiniMap';
 import { getAspectRatio } from '../task/components/mapUtils';
 import Details from '../task/components/Details';
 import OrderDetails from '../task/components/OrderDetails';
+import { getTaskTitle } from '../../shared/src/utils';
+import { navigateToTask } from '../utils';
 
 const DispatchOrderDetails = ({ route }) => {
+  const navigation = useNavigation();
   const [mapDimensions, setMapDimensions] = useState({ height: 0, width: 0 });
   const aspectRatio = useMemo(
     () => getAspectRatio(mapDimensions),
     [mapDimensions],
   );
-  const orderId = route.params.order;
   const tasks = useSelector(selectAllTasks);
+
+  const orderId = route.params.order;
 
   const filteredTasksByOrderId = tasks.filter(
     task => task.metadata.order_number === orderId,
@@ -26,45 +33,54 @@ const DispatchOrderDetails = ({ route }) => {
     setMapDimensions({ height, width });
   };
 
+    const handleTaskTitleClick = useCallback(task => {
+      navigateToTask(navigation, route, task, filteredTasksByOrderId)
+    }, [navigation, route, filteredTasksByOrderId])
+
+  const renderItem = ({ item }) => (
+    <View>
+      <TouchableOpacity>
+        <Text
+          onPress={() => {handleTaskTitleClick(item)}}
+          style={{
+            fontWeight: 'bold',
+            fontSize: 18,
+            paddingLeft: 10,
+            marginVertical: 20,
+          }}>
+          {getTaskTitle(item)}
+        </Text>
+      </TouchableOpacity>
+      <Details task={item} />
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <View style={{ height: '35%' }}>
-          <MiniMap
-            tasks={filteredTasksByOrderId}
-            onLayout={handleLayout}
-            aspectRatio={aspectRatio}
-          />
-        </View>
-        <View>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              fontSize: 18,
-              paddingLeft: 10,
-              marginVertical: 10,
-            }}>
-            {'DETAILS'}
-          </Text>
-          <OrderDetails tasks={filteredTasksByOrderId} />
-        </View>
-        <ScrollView style={{ height: '55%' }}>
-          {filteredTasksByOrderId.map(task => {
-            return (
-              <>
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 18,
-                    paddingLeft: 10,
-                    marginVertical: 20,
-                  }}>{`Task - ${task.id}`}</Text>
-                <Details task={task} />
-              </>
-            );
-          })}
-        </ScrollView>
+    <View>
+      <View style={{ height: '35%' }} onLayout={handleLayout}>
+        <MiniMap tasks={filteredTasksByOrderId} aspectRatio={aspectRatio} />
       </View>
+      <FlatList
+        style={{height: '55%'}}
+        data={filteredTasksByOrderId}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        ListHeaderComponent={
+          <View>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 18,
+                paddingLeft: 10,
+                marginVertical: 10,
+              }}>
+              {i18n.t('DETAILS')}
+            </Text>
+            <OrderDetails tasks={filteredTasksByOrderId} />
+          </View>
+        }
+      />
     </View>
   );
 };
