6785adaaa083f43c6df50f2113dea822422c5680
warmijusti <justinaparma@gmail.com>
2025-07-29 10:37:20 -0300

    remove androidBehavior prop and KeyboardAvoidingView on android

--- src/components/KeyboardAdjustView.js
+++ src/components/KeyboardAdjustView.js
@@ -15,12 +15,12 @@ import { AvoidSoftInputView } from 'react-native-avoid-softinput';
 export default function KeyboardAdjustView({
   children,
   style,
-  androidBehavior = 'padding', // FIXME: try to avoid using this prop; see the comment below
   testID,
 }) {
-  const [viewHeight, setViewHeight] = useState(0);
 
   const windowDimensions = useWindowDimensions();
+  const [viewHeight, setViewHeight] = useState(0);
+
 
   const onLayout = event => {
     const currentFrame = event.nativeEvent.layout;
@@ -30,37 +30,14 @@ export default function KeyboardAdjustView({
   };
 
   if (Platform.OS === 'android') {
-    // on Android we rely on the OS to put focused text input above the keyboard
-
-    const topOffset = windowDimensions.height - viewHeight; // status bar (~24) + toolbar (~56)
-
-    // extra View is a workaround for this issue: https://github.com/facebook/react-native/issues/35599
-    return (
-      <View style={style} onLayout={onLayout} testID={testID}>
-        <KeyboardAvoidingView
-          style={{ flex: 1 }}
-          keyboardVerticalOffset={topOffset}
-          behavior={androidBehavior}>
+      // AndroidManifest.xml updated to adjustResize to handle keyboard positioning
+      // conflicts with KeyboardAvoidingView eliminated
+      
+      return (
+        <View style={[style, { flex: 1 }]} onLayout={onLayout} testID={testID}>
           {children}
-        </KeyboardAvoidingView>
-      </View>
-    );
-
-    // FIXME:
-    //  we current have both android:windowSoftInputMode="adjustPan" and KeyboardAvoidingView,
-    //  which are conflicting in some situations.
-    //  I propose to switch to android:windowSoftInputMode="adjustResize"
-    //  which is the default one for React Native,
-    //  then we can fully rely on the OS to adjust the view and put the focused text input above the keyboard
-    //  and then we can get rid of KeyboardAvoidingView on Android
-    //  we will need to test all screens that have a text input
-    //  and also apply this fix: https://github.com/react-navigation/react-navigation/issues/10715#issuecomment-1852564585
-
-    // return (
-    //   <View style={style} {...props}>
-    //     {children}
-    //   </View>
-    // )
+        </View>
+      );
   } else if (Platform.OS === 'ios') {
     // on iOS we need to adjust the view and put the focused text input above the keyboard manually
 
