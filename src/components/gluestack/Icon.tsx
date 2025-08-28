284fe9b7b025eb0cbe32088fa0e137110f6b619f
warmijusti <justinaparma@gmail.com>
2025-08-28 12:53:23 -0300

    Add an assign icon button to the screen header.

--- src/components/gluestack/Icon.tsx
+++ src/components/gluestack/Icon.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { StyleProp, ViewStyle } from 'react-native';
 
 export interface IconProps {
-  name: string; // FontAwesome icon name
+  name: string; // Icon name
   color?: string;
   size?: number;
   style?: StyleProp<ViewStyle>;
+  useFontAwesome?: boolean; // If true, uses FontAwesome, otherwise FontAwesome5 (default)
 }
 
-const Icon: React.FC<IconProps> = ({ name, color, size, style, ...rest }) => {
+const Icon: React.FC<IconProps> = ({
+  name,
+  color,
+  size,
+  style,
+  useFontAwesome = false,
+  ...rest
+}) => {
+  const IconComponent = useFontAwesome ? FontAwesome : FontAwesome5;
+
   return (
-    <FontAwesome
+    <IconComponent
       name={name}
       color={color}
       size={size}
