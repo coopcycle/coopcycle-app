70485cd5e205916c92a0a37be79204b7fa74dc25
Gonzalo Santa Cruz <sancruzgon@gmail.com>
2025-08-29 14:27:45 -0300

    [PERTE-213] Generate recurrent orders from App (#2027)
    * add: new endpoint to generateOrders by RRules
    
    * add: extracted SectionHeader component
    
    * removed SectionHeader to new component
    
    * made suggested change
    
    * missing change

--- src/redux/api/slice.ts
+++ src/redux/api/slice.ts
@@ -173,7 +173,15 @@ export const apiSlice = createApi({
         body: patch,
       }),
     }),
-  }),
+    recurrenceRulesGenerateOrders: builder.mutation ({
+      query: (date: DateOnlyString) => ({
+        url: 'api/recurrence_rules/generate_orders',
+        params: {
+          date,
+        },
+      }),
+    }),
+  })
 });
 
 // Export the auto-generated hook for the query endpoints
@@ -190,6 +198,7 @@ export const {
   useSetTaskListItemsMutation,
   useSubscriptionGenerateOrdersMutation,
   useUpdateOrderMutation,
+  useRecurrenceRulesGenerateOrdersMutation,
 } = apiSlice;
 
 export const clearApiState = apiSlice.util.resetApiState;
