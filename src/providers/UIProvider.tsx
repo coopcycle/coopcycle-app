10379ca94fd2e2feedf00cf26ba0980aeab55ede
warmijusti <justinaparma@gmail.com>
2025-09-01 09:55:22 -0300

    add color mode on provider

--- src/providers/UIProvider.tsx
+++ src/providers/UIProvider.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { gluestackConfig } from '../config/gluestack-ui.config';
@@ -12,8 +13,10 @@ interface UIProviderProps {
  * This allows both libraries to coexist
  */
 export const UIProvider: React.FC<UIProviderProps> = ({ children }) => {
+  const colorScheme = useColorScheme();
+  
   return (
-    <GluestackUIProvider config={gluestackConfig}>
+    <GluestackUIProvider config={gluestackConfig} colorMode={colorScheme}>
       <NativeBaseProvider theme={nativeBaseTheme}>
         {children}
       </NativeBaseProvider>
