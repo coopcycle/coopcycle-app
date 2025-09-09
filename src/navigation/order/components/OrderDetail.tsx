2d0124163cbfe0e29a1dff17b392bc30befc7516
Alexandre Segura <mex.zktk@gmail.com>
2025-09-09 19:59:08 +0200

    Remove useless hook.

--- src/navigation/order/components/OrderDetail.tsx
+++ src/navigation/order/components/OrderDetail.tsx
@@ -6,7 +6,6 @@ import { Text } from '@/components/ui/text';
 import { useTranslation } from 'react-i18next';
 import IconText from '../../../components/IconText';
 import TaskTagsList from '../../../components/TaskTagsList';
-import { useBaseTextColor } from '../../../styles/theme';
 import { Tasks } from '../../../types/tasks';
 import { formatPrice } from '../../../utils/formatting';
 import { getOrderTimeFrame } from '../../task/components/utils';
@@ -31,7 +30,6 @@ const OrderDetail = ({ tasks }: { tasks: Tasks }) => {
   const orderTags = getUniqueTagsFromTasks(tasks);
   const orderValue = orderInfoInMetadata(tasks, 'order_total');
   const comments = commentsInOrder(tasks);
-  const titleColor = useBaseTextColor();
 
   return (
     <Box style={{ gap: 12, padding: 24 }}>
@@ -39,7 +37,6 @@ const OrderDetail = ({ tasks }: { tasks: Tasks }) => {
         size="xl"
         style={{
           textTransform: 'uppercase',
-          color: titleColor,
         }}>
         {orderTitle}
       </Text>
