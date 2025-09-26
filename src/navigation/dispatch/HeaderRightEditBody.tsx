7439331ff8270123d39e184d2cc0e668ab222eae
gonzalosanto <sancruzgon@gmail.com>
2025-09-26 16:04:01 -0300

    ADD: start option and i18n

--- src/navigation/dispatch/HeaderRightEditBody.tsx
+++ src/navigation/dispatch/HeaderRightEditBody.tsx
@@ -5,40 +5,57 @@ import { useTaskListsContext } from "../courier/contexts/TaskListsContext";
 import TasksMenu from "../components/TasksMenu";
 import { navigateToCompleteTask } from "../utils";
 import { useRoute } from "@react-navigation/native";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { startTask } from "@/src/redux/Courier";
 
 export const HeaderRightEditBody = ({navigation}) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
   const context = useTaskListsContext();
   const selectedTasks = context?.selectedTasksToEdit;
   const route = useRoute();
   const options = [
     {
+      key: "Start Task",
+      text: t('START'),
+      action: () => {
+        selectedTasks?.forEach((t) => {
+          return dispatch(startTask(t, () => {console.log("TASK START")})) 
+        })
+      },
+    },
+    {
       key: "Complete Task",
-      text: "Completar",
+      text: t('COMPLETE_TASK'),
       action: () => {
         navigateToCompleteTask(navigation, route, null, selectedTasks, true);
       },
     },
     {
       key: "Cancel Task",
-      text: "Cancelar",
+      text: t('CANCEL'),
       action: () => {
+        // TODO: Implement endpoint request to send tasks to cancel
         console.log("CANCEL TASK")
       },
     },
     {
       key: "Report incidence",
-      text: "Reportar Incidencia",
+      text: t('REPORT_INCIDENT'),
+      isDisabled: selectedTasks?.length > 1,
       action: () => { 
         return selectedTasks?.length > 1 ?
+            
           console.log("REPORT INCIDENTS")
           : 
           navigateToCompleteTask(navigation, route, selectedTasks[0], [], false)
       },
-
     },
     {
       key: "Edit",
-      text: "Editar",
+      text: t('EDIT'),
+      isDisabled: true,
       action: () => {
         //TODO: Implement edit feature as described at https://github.com/coopcycle/coopcycle/issues/498
         console.log("EDIT TASK")
