02cdc0880977419b1216a96ff0bdd2bd62a61a08
gonzalosanto <sancruzgon@gmail.com>
2025-09-30 16:22:01 -0300

    wip: latest changes

--- e2e/courier/success__selected_tasks_actions.spec.js
+++ e2e/courier/success__selected_tasks_actions.spec.js
@@ -4,16 +4,11 @@ import {
   expectToNotExist,
   loadFixturesAndConnect,
   longPressById,
+  sleep,
   tapById,
   tapByText,
-  waitToBeVisible
+  waitToBeVisible,
 } from "../support/commands";
-import {
-  assignOrderToUser,
-  loadDispatchFixture,
-  loginDispatcherUser,
-  toggleSectionUnassigned,
-} from '../dispatch/utils';
 
 const USER_JANE = 'jane';
 
@@ -21,19 +16,26 @@ describeif(device.getPlatform() === 'android')
   ('Courier - Select and tap an action for selected tasks', () => {
 
   beforeEach(async () => {
-
-    
     await loadFixturesAndConnect('courier.yml');
-    await authenticateWithCredentials('jane', '12345678');
+    await authenticateWithCredentials(USER_JANE, '12345678');
 
-    // Assign task #1
-    await assignOrderToUser(USER_JANE);
+    try {
+      // dismiss BACKGROUND_PERMISSION_DISCLOSURE alert
+      await tapByText('I accept');
+    } catch {}
 
-    // Hide unassigned tasks section
-    await toggleSectionUnassigned();
+    try {
+      // dismiss HMS Core alert
+      await tapByText('OK');
+    } catch {}
+
+    await waitToBeVisible('messengerTabMap');
+    await waitToBeVisible('messengerTabList');
+
+    await tapById('messengerTabList');
 
     // long press assigned task #1
-    await longPressById(`${USER_JANE}TasksList:task:0`);
+    await longPressById(`courierTaskList:task:0`);
 
     await tapById('selectedTasksToEditMenuButton');
   });
@@ -59,8 +61,9 @@ describeif(device.getPlatform() === 'android')
     // Tap Cancel button
     await tapById('CancelTaskButton');
 
+    await sleep(5000);
     // Waits to see if it was removed
-    await expectToNotExist(`${USER_JANE}TasksList:task:0`);
+    await expectToNotExist(`courierTasksList:task:0`);
   });
   
   it('should mark a task as INCIDENT REPORTED ', async () => {
@@ -69,7 +72,7 @@ describeif(device.getPlatform() === 'android')
 
     await waitToBeVisible('task:finishButton');
     await tapByText('Report incident');
-
+    await sleep(3000);
     // Verify task #1 has status "FAILED"
     await waitToBeVisible('taskListItemIcon:FAILED:1');
   });
