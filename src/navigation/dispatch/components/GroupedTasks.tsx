01f84fb5ac1ffedc9f0c6774c2825980428dfaed
gonzalosanto <sancruzgon@gmail.com>
2025-10-13 16:37:17 -0300

    fixed spinner style issue

--- src/navigation/dispatch/components/GroupedTasks.tsx
+++ src/navigation/dispatch/components/GroupedTasks.tsx
@@ -383,6 +383,10 @@ export default function GroupedTasks({
       {isLoading && (
         <View style={{
           position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
           flex: 1,
           justifyContent: 'center',
           alignItems: 'center',
