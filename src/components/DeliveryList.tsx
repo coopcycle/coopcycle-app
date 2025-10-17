84d25e3cf428c4f0eb158ecb47c938589324bc09
Alexandre Segura <mex.zktk@gmail.com>
2025-10-17 10:17:52 +0200

    Convert to function, remove defaultProps.

--- src/components/DeliveryList.tsx
+++ src/components/DeliveryList.tsx
@@ -3,7 +3,6 @@ import moment from 'moment';
 import { Icon, ChevronRightIcon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
 import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
 import {
   ActivityIndicator,
   Platform,
@@ -74,127 +73,127 @@ interface DeliveryListProps {
   refreshing?: boolean;
 }
 
-class DeliveryList extends Component<DeliveryListProps> {
-  _onItemPress(item) {
-    if (this.props.loading || this.props.refreshing) {
-      return;
-    }
+const ListItem = ({ item, itemCaptionLines, onPress }) => {
 
-    this.props.onItemPress(item);
-  }
-
-  renderItemCaption(item) {
-    const lines = this.props.itemCaptionLines
-      ? this.props.itemCaptionLines(item)
-      : [item.pickup.address.streetAddress, item.dropoff.address.streetAddress];
-
-    return (
-      <View>
-        {lines.map((line, index) => (
-          <Text
-            key={`${item['@id']}-caption-line-${index}`}
-            style={styles.textSmall}
-            {...captionTextProps}>
-            {line}
+  return (
+    <TouchableOpacity
+      onPress={() => onPress(item)}
+      style={styles.item}>
+      <View style={styles.itemBody}>
+        <View style={{ flex: 1 }}>
+          <FontAwesome5
+            name="circle"
+            solid
+            style={{ color: stateColor(item.state), fontSize: 14 }}
+          />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.textSmall}>{`#${
+            item.orderNumber ? item.orderNumber : item.id
+          }`}</Text>
+        </View>
+        <View style={[styles.details, { flex: 6 }]}>
+          <ListItemCaption item={item} itemCaptionLines={itemCaptionLines} />
+        </View>
+        {/* @see https://stackoverflow.com/questions/43143258/flex-vs-flexgrow-vs-flexshrink-vs-flexbasis-in-react-native */}
+        <View style={{ flex: 0, flexShrink: 1 }}>
+          <Text style={styles.textSmall} numberOfLines={1}>
+            {moment(item.dropoff.doneBefore).format('LT')}
           </Text>
-        ))}
-      </View>
-    );
-  }
-
-  renderItem(item) {
-    return (
-      <TouchableOpacity
-        onPress={() => this._onItemPress(item)}
-        style={styles.item}>
-        <View style={styles.itemBody}>
-          <View style={{ flex: 1 }}>
-            <FontAwesome5
-              name="circle"
-              solid
-              style={{ color: stateColor(item.state), fontSize: 14 }}
-            />
-          </View>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.textSmall}>{`#${
-              item.orderNumber ? item.orderNumber : item.id
-            }`}</Text>
-          </View>
-          <View style={[styles.details, { flex: 6 }]}>
-            {this.renderItemCaption(item)}
-          </View>
-          {/* @see https://stackoverflow.com/questions/43143258/flex-vs-flexgrow-vs-flexshrink-vs-flexbasis-in-react-native */}
-          <View style={{ flex: 0, flexShrink: 1 }}>
-            <Text style={styles.textSmall} numberOfLines={1}>
-              {moment(item.dropoff.doneBefore).format('LT')}
-            </Text>
-          </View>
         </View>
-        <View>
-          <Icon as={ChevronRightIcon} />
-        </View>
-      </TouchableOpacity>
-    );
-  }
-
-  renderFooter() {
-    if (!this.props.loading) {
-      return null;
-    }
-
-    return (
-      <View
-        style={{
-          position: 'relative',
-          paddingVertical: 20,
-          marginTop: 10,
-          marginBottom: 10,
-        }}>
-        <ActivityIndicator animating size="large" />
       </View>
-    );
-  }
-
-  render() {
-    if (this.props.data.length === 0) {
-      return <View />;
-    }
-
-    const groups = _.groupBy(this.props.data, item =>
-      moment(item.dropoff.doneBefore).format('LL'),
-    );
-    const sections = _.map(groups, (value, key) => ({
-      title: key,
-      data: value,
-    }));
-
-    return (
-      <SectionList
-        stickySectionHeadersEnabled={false}
-        initialNumToRender={17}
-        sections={sections}
-        // scrollEnabled={ !this.state.loadingMore }
-        onEndReached={this.props.onEndReached}
-        onEndReachedThreshold={Platform.OS === 'ios' ? 0 : 0.01}
-        onRefresh={this.props.onRefresh}
-        refreshing={this.props.refreshing}
-        keyExtractor={(item, index) => item['@id']}
-        renderItem={({ item }) => this.renderItem(item)}
-        ItemSeparatorComponent={ItemSeparator}
-        ListFooterComponent={this.renderFooter.bind(this)}
-        renderSectionHeader={({ section: { title } }) => (
-          <SectionHeaderComponent title={title} />
-        )}
-      />
-    );
-  }
-
-  static defaultProps = {
-    data: [],
-    loading: false,
-    refreshing: false,
-    onRefresh: () => {},
-  };
+      <View>
+        <Icon as={ChevronRightIcon} />
+      </View>
+    </TouchableOpacity>
+  );
 }
 
-export default withTranslation()(DeliveryList);
+const ListItemCaption = ({ item, itemCaptionLines }) => {
+  const lines = itemCaptionLines
+    ? itemCaptionLines(item)
+    : [item.pickup.address.streetAddress, item.dropoff.address.streetAddress];
+
+  return (
+    <View>
+      {lines.map((line, index) => (
+        <Text
+          key={`${item['@id']}-caption-line-${index}`}
+          style={styles.textSmall}
+          {...captionTextProps}>
+          {line}
+        </Text>
+      ))}
+    </View>
+  );
+}
+
+const ListFooter = ({ loading }) => {
+
+  if (!loading) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{
+        position: 'relative',
+        paddingVertical: 20,
+        marginTop: 10,
+        marginBottom: 10,
+      }}>
+      <ActivityIndicator animating size="large" />
+    </View>
+  );
+}
+
+function _onItemPress(item, loading, refreshing, onItemPress) {
+  if (loading || refreshing) {
+    return;
+  }
+
+  onItemPress(item);
+}
+
+const DeliveryList = ({
+  onItemPress,
+  onEndReached,
+  itemCaptionLines,
+  onRefresh = () => {},
+  data = [],
+  loading = false,
+  refreshing = false }: DeliveryListProps) => {
+
+  if (data.length === 0) {
+    return <View />;
+  }
+
+  const groups = _.groupBy(data, item =>
+    moment(item.dropoff.doneBefore).format('LL'),
+  );
+  const sections = _.map(groups, (value, key) => ({
+    title: key,
+    data: value,
+  }));
+
+  return (
+    <SectionList
+      stickySectionHeadersEnabled={false}
+      initialNumToRender={17}
+      sections={sections}
+      onEndReached={onEndReached}
+      onEndReachedThreshold={Platform.OS === 'ios' ? 0 : 0.01}
+      onRefresh={onRefresh}
+      refreshing={refreshing}
+      keyExtractor={(item, index) => item['@id']}
+      renderItem={({ item }) => <ListItem item={item} itemCaptionLines={ itemCaptionLines } onPress={() => _onItemPress(item, loading, refreshing, onItemPress)} /> }
+      ItemSeparatorComponent={ItemSeparator}
+      ListFooterComponent={() => <ListFooter loading={loading} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <SectionHeaderComponent title={title} />
+      )}
+    />
+  );
+}
+
+export default DeliveryList;
