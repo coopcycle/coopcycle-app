05513115208b00087d71d9fd7dc9ce43fb2e7aef
gonzalosanto <sancruzgon@gmail.com>
2025-10-21 15:28:32 -0300

    WIP: new report incident screen

--- src/navigation/task/Report.tsx
+++ src/navigation/task/Report.tsx
@@ -1,8 +1,44 @@
+import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
 import Complete from "./Complete";
+import { EditTask } from "./Edit";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { TabBar } from "./components/TabBar";
 
 export const Report = () => {
+    const [currentTab, setCurrentTab] = useState<'edit' | 'report'>('report');
+
+    const styles = StyleSheet.create({
+      buttonTab: {
+        flex: 1,
+        margin: 4,
+        color: 'black',
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      tabBar: {
+        backgroundColor: '#D9D9D9',
+        flexDirection: 'row',
+        textAlignVertical: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      },
+    });
+
+    const handleTabChange = (tab: string) => {
+        setCurrentTab(tab as 'edit' | 'report');
+    };
+    const isReportTab = currentTab === 'report';
 
     return(
-        <Complete />
+        <VStack style={{ flex: 1}}>
+            <HStack style={ styles.tabBar }> 
+                <TabBar onPress={handleTabChange}/>
+            </HStack>
+            {isReportTab ? <Complete /> : <EditTask onSubmit={() => {}} />}
+        </VStack>
     )
 };
