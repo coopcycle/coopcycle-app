07e85063a66914a3428c7e91614053665b5d10be
Alexandre Segura <mex.zktk@gmail.com>
2025-10-28 14:45:46 +0100

    Upgrade React Native.

--- android/app/src/main/java/fr/coopcycle/MainApplication.kt
+++ android/app/src/main/java/fr/coopcycle/MainApplication.kt
@@ -9,14 +9,12 @@ import android.content.Intent
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.config.ReactFeatureFlags
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.security.ProviderInstaller
@@ -53,11 +51,7 @@ class MainApplication : MultiDexApplication(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     upgradeSecurityProvider()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
