5a16904f38054aab62a4d4c4df1b21d011a953c8
Alexandre Segura <mex.zktk@gmail.com>
2025-10-31 07:14:56 +0100

    Use react-native-gesture handler Swipeable.

--- src/navigation/task/components/CompleteButton.tsx
+++ src/navigation/task/components/CompleteButton.tsx
@@ -2,29 +2,68 @@ import { Box } from '@/components/ui/box';
 import { HStack } from '@/components/ui/hstack';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
+import { Pressable } from '@/components/ui/pressable';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { SwipeRow } from 'react-native-swipe-list-view';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Reanimated, {
+  SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 import { greenColor, redColor, yellowColor } from '../../../styles/common';
 import { DoneIcon, FailedIcon } from '../styles/common';
 
-const LeftButton = ({ width }) => (
-  <View
-    style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width }}>
-    <Icon as={DoneIcon} size={24} style={{ color: '#fff' }} />
-  </View>
-);
+const swipeActionWidth = 90;
 
-const RightButton = ({ width }) => (
-  <View
-    style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width }}>
-    <Icon as={FailedIcon} size={24} style={{ color: '#fff' }} />
-  </View>
-);
+function LeftAction({ prog, drag, onPress }) {
+
+  const styleAnimation = useAnimatedStyle(() => {
+
+    return {
+      transform: [{ translateX: drag.value - swipeActionWidth }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <Pressable
+        className="items-center justify-center"
+        style={{ flex: 1, width: swipeActionWidth, backgroundColor: greenColor }}
+        testID="task:completeSuccessButton"
+        onPress={onPress}>
+        <Icon as={DoneIcon} size={24} style={{ color: '#fff' }} />
+      </Pressable>
+    </Reanimated.View>
+  );
+}
+
+function RightAction({ prog, drag, onPress }) {
+
+  const styleAnimation = useAnimatedStyle(() => {
+
+    return {
+      transform: [{ translateX: drag.value + swipeActionWidth }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <Pressable
+        className="items-center justify-center"
+        style={{ flex: 1, width: swipeActionWidth, backgroundColor: yellowColor }}
+        testID="task:completeFailureButton"
+        onPress={onPress}
+        >
+        <Icon as={FailedIcon} size={24} style={{ color: '#fff' }} />
+      </Pressable>
+    </Reanimated.View>
+  );
+}
 
 const CompleteButton = React.forwardRef((props, ref) => {
+
   const { task, onPressSuccess, onPressFailure, t } = props;
 
   const { width } = Dimensions.get('window');
@@ -68,47 +107,23 @@ const CompleteButton = React.forwardRef((props, ref) => {
           {t('SWIPE_TO_END')}
         </Text>
       </HStack>
-      <SwipeRow
-        leftOpenValue={buttonWidth}
-        stopLeftSwipe={buttonWidth + 25}
-        rightOpenValue={buttonWidth * -1}
-        stopRightSwipe={(buttonWidth + 25) * -1}
-        ref={ref}>
-        <View style={styles.rowBack}>
-          <TouchableOpacity
-            testID="task:completeSuccessButton"
-            style={{
-              flex: 1,
-              alignItems: 'flex-start',
-              justifyContent: 'center',
-              backgroundColor: greenColor,
-              width: buttonWidth,
-            }}
-            onPress={onPressSuccess}>
-            <LeftButton width={buttonWidth} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            testID="task:completeFailureButton"
-            style={{
-              flex: 1,
-              alignItems: 'flex-end',
-              justifyContent: 'center',
-              backgroundColor: yellowColor,
-              width: buttonWidth,
-            }}
-            onPress={onPressFailure}>
-            <RightButton width={buttonWidth} />
-          </TouchableOpacity>
-        </View>
-        <Box
-          className="bg-secondary-600"
-          style={{ padding: 28, width }}
-          testID="task:completeButton">
-          <Text style={{ fontSize: 20, textAlign: 'center', color: '#fff' }}>
-            {t('COMPLETE_TASK')}
-          </Text>
+      <Swipeable
+        ref={ref}
+        containerStyle={styles.swipeable}
+        friction={2}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderLeftActions={(prog, drag) => {
+          return (<LeftAction prog={prog} drag={drag} onPress={onPressSuccess} />);
+        }}
+        renderRightActions={(prog, drag) => {
+          return (<RightAction prog={prog} drag={drag} onPress={onPressFailure} />);
+        }}
+      >
+        <Box testID="task:completeButton" className="flex-1 w-full justify-center items-center bg-secondary-600" style={{ width }}>
+          <Text size="lg">{t('COMPLETE_TASK')}</Text>
         </Box>
-      </SwipeRow>
+      </Swipeable>
     </View>
   );
 });
@@ -127,6 +142,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  swipeable: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
