72b59b0dcb6773fb7306465c73d8be1fd305e799
Alexandre Segura <mex.zktk@gmail.com>
2025-11-13 21:49:46 +0100

    Fix React Navigation for Jest.

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
     '^.+\\.(js|jsx|ts)$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!@react-native|react-native|react-navigation|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native|coopcycle-frontend-js)'
+    'node_modules/(?!@react-native|react-native|@react-navigation|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native|coopcycle-frontend-js)'
   ]
 };
