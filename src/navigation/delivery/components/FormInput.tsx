0292752d34c2d35200cc322069d0cc9fef18463e
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-12-10 12:33:38 -0800

    (FormInput) add type-hints

--- src/navigation/delivery/components/FormInput.tsx
+++ src/navigation/delivery/components/FormInput.tsx
@@ -1,13 +1,13 @@
+import { ComponentPropsWithoutRef } from 'react';
 import { Input, InputField } from '@/components/ui/input';
 
-export default function FormInput({ style, ...props }) {
-
+export default function FormInput({
+  style,
+  ...props
+}: ComponentPropsWithoutRef<typeof InputField>) {
   return (
     <Input>
-      <InputField
-        {...props}
-        style={ style }
-      />
+      <InputField {...props} style={style} />
     </Input>
   );
 }
