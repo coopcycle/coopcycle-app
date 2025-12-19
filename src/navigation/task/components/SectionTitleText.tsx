9de892630580fd26de7a8ce3540f2b6937df3748
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-12-18 17:49:05 -0800

    use theme token

--- src/navigation/task/components/SectionTitleText.tsx
+++ src/navigation/task/components/SectionTitleText.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from '@/components/ui/text';
-import { useBaseTextColor } from '@/src/styles/theme';
 
 type Props = {
   text: string;
 };
 
 export const SectionTitleText = ({ text }: Props) => {
-  const textColor = useBaseTextColor();
-
-  return <Text style={[styles.text, { color: textColor }]}>{text}</Text>;
+  return (
+    <Text className="text-typography-950" style={styles.text}>
+      {text}
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({
