2cf2c490e820a5840c078cb82e893bce1624d7f0
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-12-23 18:10:14 -0800

    add(e2e test): report an incident with suggestion as a messenger

---
+++ e2e/courier/success__report_incident_with_suggestion.spec.js
@@ -0,0 +1,62 @@
+import {
+  authenticateWithCredentials,
+  describeif,
+  loadFixturesAndConnect,
+  tapById,
+  tapByText,
+  waitToBeVisible,
+} from '@/e2e/support/commands';
+import { scrollToElement, typeTextQuick } from '../support/commands';
+import { swipeLeftTaskAndTap } from './utils';
+
+const USER_JANE = 'jane';
+
+describeif(device.getPlatform() === 'android')('Courier - Task List', () => {
+  beforeEach(async () => {
+    await loadFixturesAndConnect('courier.yml', true);
+    await authenticateWithCredentials(USER_JANE, '12345678');
+
+    try {
+      // dismiss BACKGROUND_PERMISSION_DISCLOSURE alert
+      await tapByText('I accept');
+    } catch {}
+
+    try {
+      // dismiss HMS Core alert
+      await tapByText('OK');
+    } catch {}
+
+    await waitToBeVisible('messengerTabMap');
+    await waitToBeVisible('messengerTabList');
+
+    await tapById('messengerTabList');
+  });
+
+  it('should report an incident with suggested changes', async () => {
+    // Tap Report incident button
+    await swipeLeftTaskAndTap(0);
+
+    // To open select
+    await tapById('failure-reason-select-trigger');
+
+    // To select any option
+    await tapById('failure-reason-option-1');
+
+    // Fills description input
+    await typeTextQuick('notes-input', 'Some text to describe any incident');
+
+    // Tap Edit Tab
+    await tapById('editTabButton');
+
+    //TODO: add some suggestion
+
+    // Scroll to submit button
+    await scrollToElement('scrollView:edit', 'task:finishButton-edit');
+
+    // Tap Submit Button
+    await tapById('task:finishButton-edit');
+
+    // Verify task #2 has status "INCIDENT"
+    await waitToBeVisible('taskListItemIcon:INCIDENT:2');
+  });
+});
