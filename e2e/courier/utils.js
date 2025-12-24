6767d3b8209d8cc54bb1d515ced35de56f2c1464
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-12-23 17:24:13 -0800

    add(e2e test): report an incident as a messenger

---
+++ e2e/courier/utils.js
@@ -0,0 +1,19 @@
+import { swipeLeft, swipeRight, tapById } from '../support/commands';
+
+export async function swipeLeftTask(index = 0) {
+  await swipeLeft(`courierTaskList:task:${index}`);
+}
+
+export async function swipeLeftTaskAndTap(index = 0) {
+  await swipeLeft(`courierTaskList:task:${index}`);
+  await tapById(`courierTaskList:task:${index}:right`);
+}
+
+export async function swipeRightTask(index = 0) {
+  await swipeRight(`courierTaskList:task:${index}`);
+}
+
+export async function swipeRightTaskAndTap(index = 0) {
+  await swipeRight(`courierTaskList:task:${index}`);
+  await tapById(`courierTaskList:task:${index}:left`);
+}
