8474d9523d0455d667637d76cd1952975eac6cae
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-12-29 17:18:13 -0800

    add a checkbox to create an order to be paid in cash on delivery

--- src/navigation/delivery/NewDeliveryPrice.tsx
+++ src/navigation/delivery/NewDeliveryPrice.tsx
@@ -1,8 +1,16 @@
-import { Formik } from 'formik';
+import { Formik, FormikErrors } from 'formik';
 import { StyleSheet, View } from 'react-native';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  Checkbox,
+  CheckboxIcon,
+  CheckboxIndicator,
+  CheckboxLabel,
+} from '@/components/ui/checkbox';
+import { CheckIcon } from '@/components/ui/icon';
+import { Divider } from '@/components/ui/divider';
+import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -11,21 +19,45 @@ import { getPrice } from '../../redux/Delivery/actions';
 import {
   selectPrice,
   selectPriceExcludingTax,
+  selectStore,
 } from '../../redux/Delivery/selectors';
 import { useDeliveryCallback } from './contexts/DeliveryCallbackContext';
+import { useGetStorePaymentMethodsQuery } from '../../redux/api/slice';
 import FormInput from './components/FormInput';
 import ModalFormWrapper from './ModalFormWrapper';
+import { CashOnDeliveryDisclaimer } from '../checkout/components/CashOnDeliveryDisclaimer';
+import { OrderPayload, PostDeliveryBody } from '@/src/redux/api/types';
+import { useAppDispatch } from '@/src/redux/store';
+
+type NewDeliveryPriceFormValues = {
+  manualPriceVariantName?: string;
+  manualPriceVariantTotal?: string;
+  paymentMethod?: string;
+};
 
 export default function NewDeliveryPrice({ route }) {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { t } = useTranslation();
-  const delivery = route.params?.delivery;
+  const delivery = route.params?.delivery as PostDeliveryBody;
 
   const price = useSelector(selectPrice);
   const priceExcludingTax = useSelector(selectPriceExcludingTax);
+  const store = useSelector(selectStore);
   const [isManualPriceEnabled, setIsManualPriceEnabled] = useState(false);
 
   const { deliveryCallback, allowManualPrice = false } = useDeliveryCallback();
+
+  const { data: paymentMethods } = useGetStorePaymentMethodsQuery(
+    store?.['@id'],
+    {
+      skip: !store?.['@id'],
+    },
+  );
+
+  const isCashOnDeliveryAvailable =
+    paymentMethods?.methods?.some(
+      method => method.type === 'cash_on_delivery',
+    ) ?? false;
 
   useEffect(() => {
     dispatch(getPrice(delivery));
@@ -37,13 +69,17 @@ export default function NewDeliveryPrice({ route }) {
     setFieldValue('manualPriceVariantTotal', '');
   };
 
-  const parsePrice = value => {
+  const parsePrice = (value?: string) => {
+    if (!value) {
+      return null;
+    }
+
     const parsed = parseFloat(value);
     return isNaN(parsed) ? null : parseInt(parsed * 100, 10);
   };
 
-  const validate = values => {
-    const errors = {};
+  const validate = (values: NewDeliveryPriceFormValues) => {
+    const errors = {} as FormikErrors<NewDeliveryPriceFormValues>;
 
     if (isManualPriceEnabled && !parsePrice(values.manualPriceVariantTotal)) {
       errors.manualPriceVariantTotal = t('MANUAL_PRICE_VARIANT_TOTAL_ERROR');
@@ -52,20 +88,35 @@ export default function NewDeliveryPrice({ route }) {
     return errors;
   };
 
-  const submit = values => {
+  const submit = (values: NewDeliveryPriceFormValues) => {
     if ((price === null || priceExcludingTax === null) && !isManualPriceEnabled)
       return;
 
-    const data = Object.assign({}, values);
-    if (isManualPriceEnabled)
-      data.manualPriceVariantTotal = parsePrice(values.manualPriceVariantTotal);
+    const data = Object.assign({}, delivery);
+
+    const orderPayload: OrderPayload = {};
+
+    if (isManualPriceEnabled) {
+      orderPayload.arbitraryPrice = {
+        variantName: values.manualPriceVariantName,
+        variantPrice: parsePrice(values.manualPriceVariantTotal),
+      };
+    }
+
+    if (values.paymentMethod) {
+      orderPayload.paymentMethod = values.paymentMethod;
+    }
+
+    if (Object.keys(orderPayload).length > 0) {
+      data.order = orderPayload;
+    }
 
     dispatch(createDelivery(data, deliveryCallback));
   };
 
   return (
     <Formik
-      initialValues={delivery}
+      initialValues={{} as NewDeliveryPriceFormValues}
       onSubmit={submit}
       validate={validate}
       validateOnBlur={false}
@@ -147,6 +198,35 @@ export default function NewDeliveryPrice({ route }) {
                 )}
               </ButtonText>
             </Button>
+          ) : null}
+          {isCashOnDeliveryAvailable ? (
+            <>
+              <Divider className="my-4" />
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>{t('PAYMENT_FORM_TITLE')}</Text>
+                <Checkbox
+                  value="cash_on_delivery"
+                  isChecked={values.paymentMethod === 'cash_on_delivery'}
+                  onChange={checked => {
+                    setFieldValue(
+                      'paymentMethod',
+                      checked ? 'cash_on_delivery' : undefined,
+                    );
+                  }}
+                  testID="cash-on-delivery-checkbox"
+                >
+                  <CheckboxIndicator>
+                    <CheckboxIcon as={CheckIcon} />
+                  </CheckboxIndicator>
+                  <CheckboxLabel>{t('PM_CASH')}</CheckboxLabel>
+                </Checkbox>
+                {values.paymentMethod === 'cash_on_delivery' && (
+                  <View className="mt-4">
+                    <CashOnDeliveryDisclaimer />
+                  </View>
+                )}
+              </View>
+            </>
           ) : null}
         </ModalFormWrapper>
       )}
