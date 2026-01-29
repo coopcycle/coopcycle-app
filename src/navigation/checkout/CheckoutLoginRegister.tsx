2391d388a5b039a2e1cd7462d05e1067a1615b75
Alexandre Segura <mex.zktk@gmail.com>
2026-01-29 15:03:46 +0100

    Add safe area on checkout login/register screen.

--- src/navigation/checkout/CheckoutLoginRegister.tsx
+++ src/navigation/checkout/CheckoutLoginRegister.tsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import { Keyboard, View } from 'react-native';
 import { connect } from 'react-redux';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import AuthenticateContainer from '../../components/AuthenticateContainer';
 import AuthenticateForm from '../../components/AuthenticateForm';
@@ -47,47 +48,49 @@ class LoginRegister extends Component {
     const { isKeyboardVisible } = this.state;
     const { guestCheckoutEnabled } = this.props;
     return (
-      <AuthenticateContainer>
-        {!isKeyboardVisible && guestCheckoutEnabled && (
-          <>
-            <View
-              style={{
-                paddingTop: 20,
-                paddingHorizontal: 40,
-                width: '100%',
-              }}>
-              <Button
-                testID="guestCheckoutButton"
-                action="success"
-                onPress={() => this.props.guestModeOn()}>
-                <ButtonText>{this.props.t('CHECKOUT_AS_GUEST')}</ButtonText>
-              </Button>
-            </View>
+      <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+        <AuthenticateContainer>
+          {!isKeyboardVisible && guestCheckoutEnabled && (
+            <>
+              <View
+                style={{
+                  paddingTop: 20,
+                  paddingHorizontal: 40,
+                  width: '100%',
+                }}>
+                <Button
+                  testID="guestCheckoutButton"
+                  action="success"
+                  onPress={() => this.props.guestModeOn()}>
+                  <ButtonText>{this.props.t('CHECKOUT_AS_GUEST')}</ButtonText>
+                </Button>
+              </View>
 
-            <Text mt="4">{this.props.t('OR')}</Text>
-          </>
-        )}
-        {!isKeyboardVisible && (
-          <View style={{ padding: 20 }}>
-            <Text style={{ textAlign: 'center' }} note>
-              {this.props.t('CHECKOUT_LOGIN_DISCLAIMER')}
-            </Text>
-          </View>
-        )}
-        <AuthenticateForm
-          onLogin={(email, password) =>
-            this.props.login(email, password, false)
-          }
-          onRegister={data => this.props.register(data)}
-          onForgotPassword={() => {
-            this.props.forgotPassword();
-            this.props.navigation.navigate('CheckoutForgotPassword', {
-              checkEmailRouteName: 'CheckoutResetPasswordCheckEmail',
-              resumeCheckoutAfterActivation: true,
-            });
-          }}
-        />
-      </AuthenticateContainer>
+              <Text mt="4">{this.props.t('OR')}</Text>
+            </>
+          )}
+          {!isKeyboardVisible && (
+            <View style={{ padding: 20 }}>
+              <Text style={{ textAlign: 'center' }} note>
+                {this.props.t('CHECKOUT_LOGIN_DISCLAIMER')}
+              </Text>
+            </View>
+          )}
+          <AuthenticateForm
+            onLogin={(email, password) =>
+              this.props.login(email, password, false)
+            }
+            onRegister={data => this.props.register(data)}
+            onForgotPassword={() => {
+              this.props.forgotPassword();
+              this.props.navigation.navigate('CheckoutForgotPassword', {
+                checkEmailRouteName: 'CheckoutResetPasswordCheckEmail',
+                resumeCheckoutAfterActivation: true,
+              });
+            }}
+          />
+        </AuthenticateContainer>
+      </SafeAreaView>
     );
   }
 }
