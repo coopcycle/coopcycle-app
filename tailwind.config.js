b997d7f6633c8ae61e2f1b996f382cb92239860d
Alexandre Segura <mex.zktk@gmail.com>
2026-03-06 13:57:31 +0100

    Remove useless dirs from Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './app/**/*.{html,js,jsx,ts,tsx,mdx}',
     './components/**/*.{html,js,jsx,ts,tsx,mdx}',
-    './utils/**/*.{html,js,jsx,ts,tsx,mdx}',
-    './*.{html,js,jsx,ts,tsx,mdx}',
     './src/**/*.{html,js,jsx,ts,tsx,mdx}',
   ],
   presets: [require('nativewind/preset')],
