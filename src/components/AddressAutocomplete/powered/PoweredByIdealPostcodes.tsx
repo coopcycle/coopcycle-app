2b282b5e7122c4b982e3682178bd018453e9a3ff
Alexandre Segura <mex.zktk@gmail.com>
2026-08-19 16:23:47 +0200

    Fix Ideal Postcodes logo in dark mode.

--- src/components/AddressAutocomplete/powered/PoweredByIdealPostcodes.tsx
+++ src/components/AddressAutocomplete/powered/PoweredByIdealPostcodes.tsx
@@ -1,10 +1,16 @@
 import { Image, View } from 'react-native';
 
+import { useColorModeValue } from '../../../styles/theme';
+
 export default function PoweredByIdealPostcodes({ style }) {
+  // the logo is a black shape with an alpha channel, so it can be tinted
+  const tintColor = useColorModeValue('#000000', '#FFFFFF');
+
   return (
     <View style={style}>
       <Image
         resizeMode="contain"
+        tintColor={tintColor}
         source={require('../../../../assets/images/ideal_postcodes.png')}
       />
     </View>
